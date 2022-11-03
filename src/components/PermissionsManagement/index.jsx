b2aae7c8edbaf4b9cc92d737588dec4ba933dc09
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-11-03 14:30:32 +0200

    added select team member group

--- src/components/PermissionsManagement/index.jsx
+++ src/components/PermissionsManagement/index.jsx
@@ -5,9 +5,10 @@ import { useQuery } from '@tanstack/react-query';
 import { PropTypes } from 'prop-types';
 import requestNew from '../../app/requestNew';
 import SelectMenuTeamMembers from '../selectMenu';
+import DisplayData from './components/DisplaySelectedData';
 
 const useGetFolderPermissions = (id) => {
-  const { data, status } = useQuery(['folder-permissions'], async () => requestNew({
+  const { data, status } = useQuery([`folder-permissions-${id}`], async () => requestNew({
     url: `folders/${id}/access`,
     method: 'GET',
   }));
@@ -15,33 +16,13 @@ const useGetFolderPermissions = (id) => {
   return { data: data?.data, status };
 };
 
-function DisplayData({ data }) {
-  if (!data) {
-    return <> </>;
-  }
-
-  return (
-    <div className="p-1 font-medium">
-      {data.map((i) => (
-        <p key={i.id}>
-          {i.title}
-          <span className="font-semibold text-indigo-600 pl-1">{i.value}</span>
-        </p>
-      ))}
-    </div>
-  );
-}
-
-DisplayData.propTypes = {
-  data: PropTypes.array.isRequired,
-};
-
 function PermissionsManagement({ folderId }) {
   const [showPopup, setShowPopup] = useState(false);
   const { data } = useGetFolderPermissions(folderId);
   const [selectedUser, setSelectedUser] = useState(null);
+  const [selectedGroup, setSelectedGroup] = useState(null);
 
-  const dataArr = selectedUser ? [
+  const teamMemberData = selectedUser ? [
     {
       id: 1,
       title: 'Name:',
@@ -59,26 +40,63 @@ function PermissionsManagement({ folderId }) {
     },
   ] : null;
 
+  const groupData = selectedGroup ? [
+    {
+      id: 1,
+      title: 'Name:',
+      value: selectedGroup.team_member_group.name,
+    },
+    {
+      id: 2,
+      title: 'Initials:',
+      value: selectedGroup.team_member_group.initials,
+    },
+    {
+      id: 3,
+      title: 'Access level:',
+      value: selectedGroup.access_level.name,
+    },
+  ] : null;
+
+  const hidePopup = () => {
+    setShowPopup(false);
+    setSelectedGroup(null);
+    setSelectedUser(null);
+  };
+
   return (
     <>
+      {showPopup ? <div className="fixed left-0 right-0 bottom-0 top-0 bg-black opacity-0" tabIndex={0} role="button" onClick={hidePopup} onKeyDown={() => {}}> </div> : null}
       <p onClick={() => setShowPopup(true)} className="text-right text-gray-600 underline cursor-pointer">Manage permissions</p>
       {showPopup ? (
-        <div className="absolute top-0 left-0 w-80 bg-white m-2 rounded-xl border p-3 flex flex-col z-10">
-          <button type="button" onClick={() => setShowPopup(false)} className="flex w-full justify-end">
+        <div className="absolute top-0 right-0 w-80 bg-white m-2 rounded-xl border p-3 flex flex-col z-10">
+          <button type="button" onClick={hidePopup} className="flex w-full justify-end">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
-          {data ? <SelectMenuTeamMembers teamMembers={data.folder_team_members} selectedUser={selectedUser} setSelectedUser={setSelectedUser} /> : null}
-          {selectedUser ? (
-            <div className="border rounded-xl p-2 mt-2 font-medium">
-              <DisplayData data={dataArr} />
-              <div className="flex justify-between content-center text-sm mt-3">
-                <button className="border p-2 rounded-xl text-gray-600 hover:border-indigo-600 hover:text-indigo-600 transition-all duration-300" type="button">Change access</button>
-                <button className="border p-2 rounded-xl text-gray-600 hover:border-indigo-600 hover:text-indigo-600 transition-all duration-300" type="button">Remove access</button>
+          <div className="flex flex-col gap-3">
+            {data ? <SelectMenuTeamMembers teamMembers={data.folder_team_members} selectedData={selectedUser} setSelectedData={setSelectedUser} type="user" title="Select team member:" /> : null}
+            {selectedUser ? (
+              <div className="border rounded-xl p-2 mt-2 font-medium">
+                <DisplayData data={teamMemberData} />
+                <div className="flex justify-between content-center text-sm mt-3">
+                  <button className="border p-2 rounded-xl text-gray-600 hover:border-indigo-600 hover:text-indigo-600 transition-all duration-300" type="button">Change access</button>
+                  <button className="border p-2 rounded-xl text-gray-600 hover:border-indigo-600 hover:text-indigo-600 transition-all duration-300" type="button">Remove access</button>
+                </div>
               </div>
-            </div>
-          ) : null}
+            ) : null}
+            {data ? <SelectMenuTeamMembers teamMembers={data.folder_team_member_groups} selectedData={selectedGroup} setSelectedData={setSelectedGroup} type="group" title="Select team member group:" /> : null}
+            {selectedGroup ? (
+              <div className="border rounded-xl p-2 mt-2 font-medium">
+                <DisplayData data={groupData} />
+                <div className="flex justify-between content-center text-sm mt-3">
+                  <button className="border p-2 rounded-xl text-gray-600 hover:border-indigo-600 hover:text-indigo-600 transition-all duration-300" type="button">Change access</button>
+                  <button className="border p-2 rounded-xl text-gray-600 hover:border-indigo-600 hover:text-indigo-600 transition-all duration-300" type="button">Remove access</button>
+                </div>
+              </div>
+            ) : null}
+          </div>
         </div>
       ) : null}
     </>
