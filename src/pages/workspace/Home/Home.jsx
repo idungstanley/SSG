c90d7e4ac7fadcafde2cecdc9ce3b47f73d4e2e5
diamond-nicholas <diamondnicholas154@gmail.com>
2022-11-14 17:03:02 +0000

    restructure render

--- src/pages/workspace/Home/Home.jsx
+++ src/pages/workspace/Home/Home.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 function Home() {
-  console.log(1);
-  return <div className="items-end bg-gray-600">THis is home</div>;
+  return <div className="">THis is home</div>;
 }
 
 export default Home;
