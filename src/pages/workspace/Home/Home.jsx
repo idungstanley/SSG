cad56f35b20b97b47ae55651ec0ac1ba6234bd4c
diamond-nicholas <diamondnicholas154@gmail.com>
2022-11-15 11:55:26 +0000

    modified workspace nav

--- src/pages/workspace/Home/Home.jsx
+++ src/pages/workspace/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Nav from '../nav/Nav';
 
 function Home() {
-  return <div className="">THis is home</div>;
+  return (
+    <>
+      <Nav navName="Home" buttonLabel="Calendar" Assigned="Agenda" />
+      <p>This is home</p>
+    </>
+  );
 }
 
 export default Home;
--- src/pages/workspace/notification/Notification.jsx
+++ src/pages/workspace/notification/Notification.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Nav from '../nav/Nav';
 
 function Notification() {
-  return <div>Notification</div>;
+  return (
+    <>
+      <Nav
+        navName="Notification"
+        newd="New"
+        Cleared="Clear"
+        buttonLabel="All"
+        Assigned="Assigned to me"
+      />
+      <p>This is notification page</p>
+    </>
+  );
 }
 
 export default Notification;
