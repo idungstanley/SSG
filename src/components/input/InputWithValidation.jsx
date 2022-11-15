4f40e262d12fb0e5493c14773c61761682e848a3
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-11-15 16:24:29 +0200

    added new input type

--- src/components/input/InputWithValidation.jsx
+++ src/components/input/InputWithValidation.jsx
@@ -8,6 +8,9 @@ const icons = [
   {
     path: 'M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75',
   },
+  {
+    path: 'M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z',
+  },
 ];
 
 export default function InputWithValidation({
@@ -20,8 +23,12 @@ export default function InputWithValidation({
   message,
   isFocused,
 }) {
-  const label = placeholder.split(' ').at(-1);
-  console.log(message);
+  let label = placeholder.split(' ').at(-1);
+
+  if (label === 'passwordConfirmation') {
+    label = 'Confirm password';
+  }
+
   return (
     <div className="relative">
       <label
@@ -44,7 +51,13 @@ export default function InputWithValidation({
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d={label === 'email' ? icons[1].path : icons[0].path}
+            d={
+              label === 'email'
+                ? icons[1].path
+                : (label === 'password' || label === 'Confirm password')
+                  ? icons[0].path
+                  : icons[2].path
+            }
           />
         </svg>
       </div>
@@ -52,10 +65,16 @@ export default function InputWithValidation({
       <input
         className={`appearance-none block w-full px-3 py-2 pl-8 border ${
           message ? 'border-red-700' : 'border-gray-300'
-        } rounded-md shadow-sm placeholder-gray-500 focus:outline-none focus:${message ? 'ring-red-700' : 'ring-gray-300'} focus:${message ? 'border-red-700' : 'border-gray-300'} sm:text-sm`}
+        } rounded-md shadow-sm placeholder-gray-500 focus:outline-none focus:${
+          message ? 'ring-red-700' : 'ring-gray-300'
+        } focus:${message ? 'border-red-700' : 'border-gray-300'} sm:text-sm`}
         id={id}
         type={type}
-        placeholder={placeholder}
+        placeholder={
+          placeholder === 'Enter passwordConfirmation'
+            ? 'Confirm password'
+            : placeholder
+        }
         value={value}
         onChange={onChange}
         onBlur={onBlur}
