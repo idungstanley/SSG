3e53afd4b410a70718b5c59ccd2807fd478c941a
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-11-16 13:39:22 +0200

    fixed file preview for docks and images

--- src/common/FilePreview.jsx
+++ src/common/FilePreview.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 
+const docks = ['docx', 'doc'];
+const images = ['jpg', 'png'];
+
 function FilePreview({ fileData, fileExtension }) {
   const docs = [
     {
@@ -9,12 +12,17 @@ function FilePreview({ fileData, fileExtension }) {
       fileData,
     },
   ];
+
   return (
     <div className="react-doc-viewer-wrapper h-full w-full overflow-y-scroll flex-1">
-      {fileExtension === 'bin' ? (
-        <p className="text-center mt-2 text-red-500">Unsupported file extension</p>
-      ) : (
+      {docks.includes(fileExtension) ? (
         <DocViewer pluginRenderers={DocViewerRenderers} documents={docs} />
+      ) : images.includes(fileExtension) ? (
+        <img src={fileData} alt="img" />
+      ) : (
+        <p className="text-center mt-2 text-red-500">
+          Unsupported file extension
+        </p>
       )}
     </div>
   );
