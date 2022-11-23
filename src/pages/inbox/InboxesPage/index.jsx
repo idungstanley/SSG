2b546267ca140c3758f0fcbfc50b231f015c18bc
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-11-23 20:20:19 +0200

    minor fixes

--- src/pages/inbox/InboxesPage/index.jsx
+++ src/pages/inbox/InboxesPage/index.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-// import { useDispatch } from 'react-redux';
 import Header from './components/Header';
 import Table from './components/Table';
 import PinnedInboxes from './components/PinnedInboxes';
 import CreateInboxSlideOver from './components/CreateInboxSlideOver';
-// import { setCreateInboxSlideOverVisibility } from '../../../features/general/slideOver/slideOverSlice';
-// import { useGetHiddenInboxes } from '../../../features/inbox/inboxesService';
-// import { Spinner } from '../../../common';
-// import FullScreenMessage from '../../shared/components/FullScreenMessage';
 
 function InboxDashboard() {
-  // const dispatch = useDispatch();
-
-  // const { status, data } = useGetHiddenInboxes();
-
   return (
     <>
       <div className="h-full flex flex-1 flex-col w-full bg-white overflow-hidden">
@@ -21,33 +12,6 @@ function InboxDashboard() {
         <div className="flex-1 h-full overflow-y-scroll">
           <PinnedInboxes />
           <Table />
-
-          {/* {status === 'loading' && (
-            <div className="mx-auto w-6 mt-10 justify-center">
-              <Spinner size={22} color="#0F70B7" />
-            </div>
-          )} */}
-          {/* {status === 'success' ? (
-            data?.data?.inboxes.length === 0 ? (
-              <div className="flex flex-1 h-full bg-white">
-                <div className="m-auto">
-                  <FullScreenMessage
-                    title="You have no inboxes yet"
-                    description="Get started by creating a new inbox"
-                    ctaText="Create inbox"
-                    ctaOnClick={() => dispatch(setCreateInboxSlideOverVisibility(true))}
-                    showCta
-                  />
-                </div>
-              </div>
-            ) : (
-            )
-          ) : status === 'error' ? (
-            <FullScreenMessage
-              title="Oops, an error occurred :("
-              description="Please try again later."
-            />
-          ) : null} */}
         </div>
       </div>
       <CreateInboxSlideOver />
