2fcee362f7278c224edcdf24f7bca1018611b2ca
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-11-29 16:40:16 +0200

    removed unused requests and login in code

--- src/pages/inbox/InboxPage/components/InboxFile/index.jsx
+++ src/pages/inbox/InboxPage/components/InboxFile/index.jsx
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Toolbar from './Toolbar';
 import Main from './Main';
-import { useGetActiveInboxes, useInboxes } from '../../../../../features/inbox/inboxesService';
 import { useGetInboxFile } from '../../../../../features/inbox/inboxService';
 
 function InboxFile() {
@@ -14,28 +13,12 @@ function InboxFile() {
 
   const { status, data: inboxFile } = useGetInboxFile(selectedInboxFileId);
 
-  // TODO: check if its can be removed
-  const { data: dataActiveWithPinned } = useGetActiveInboxes();
-  const { data: dataHidden } = useInboxes('hidden');
-  const { data: dataArchive } = useInboxes('archived');
-
-  const active = dataActiveWithPinned?.data.inboxes;
-  const hidden = dataHidden?.data.inboxes;
-  const archive = dataArchive?.data.inboxes;
-
-  const inbox = active?.find((i) => i.id === inboxId)
-    || hidden?.find((i) => i.id === inboxId)
-    || archive?.find((i) => i.id === inboxId);
-
-  return status === 'success'
-    && inbox
-    && inboxFile
-    && inbox?.id === inboxFile?.inbox_id ? (
-      <div className="h-full flex flex-col w-full">
-        <Toolbar />
-        <Main />
-      </div>
-    ) : null;
+  return status === 'success' && inboxFile?.inbox_id === inboxId ? (
+    <div className="h-full flex flex-col w-full">
+      <Toolbar />
+      <Main />
+    </div>
+  ) : null;
 }
 
 export default InboxFile;
