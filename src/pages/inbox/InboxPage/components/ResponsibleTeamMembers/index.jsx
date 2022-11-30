c8fc2200fb72b943e835c9b3511de616a3495932
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-11-30 21:44:57 +0200

    added modal for responsible team members

--- src/pages/inbox/InboxPage/components/ResponsibleTeamMembers/index.jsx
+++ src/pages/inbox/InboxPage/components/ResponsibleTeamMembers/index.jsx
@@ -1,32 +1,46 @@
 import { UserIcon } from '@heroicons/react/solid';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Button } from '../../../../../components';
 import { useGetInboxFile } from '../../../../../features/inbox/inboxService';
+import Modal from './Modal';
 
 export default function ResponsibleTeamMembers() {
+  // TODO: add modal with selection team member dropdown, list all members and removing members
+  // * seelct team members from reusable component
+  // * list all items as list blacklist emails
+
+  const [showModal, setShowModal] = useState(false);
   const selectedInboxFileId = useSelector(
     (state) => state.inbox.selectedInboxFileId,
   );
   const { data: inboxFile } = useGetInboxFile(selectedInboxFileId);
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    setShowModal(!showModal);
+  };
 
   return inboxFile ? (
-    <Button
-      buttonStyle="white"
-      label="Responsible members"
-      onClick={handleClick}
-      icon={
-        <UserIcon className="mr-2.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-      }
-      iconPosition="center"
-      disabled={false}
-      roundedLeft={false}
-      roundedRight={false}
-      borderRight={false}
-      ringOnFocus
-      width="w-52"
-    />
+    <>
+      {showModal ? <Modal setShowModal={setShowModal} /> : null}
+      <Button
+        buttonStyle="white"
+        label="Responsible members"
+        onClick={handleClick}
+        icon={(
+          <UserIcon
+            className="mr-2.5 h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
+        )}
+        iconPosition="center"
+        disabled={false}
+        roundedLeft={false}
+        roundedRight={false}
+        borderRight={false}
+        ringOnFocus
+        width="w-52"
+      />
+    </>
   ) : null;
 }
