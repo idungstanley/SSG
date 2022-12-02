af27765fd090e680b6d7052947ee6b406fb6232f
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-12-02 14:39:39 +0200

    removed unused UI

--- src/components/selectMenu/index.jsx
+++ src/components/selectMenu/index.jsx
@@ -13,20 +13,35 @@ export default function SelectMenuTeamMembers({
   title,
 }) {
   if (!teamMembers.length) {
-    return <> </>;
+    return null;
   }
 
   return (
     <Listbox value={selectedData} onChange={setSelectedData}>
       {({ open }) => (
         <div>
-          <Listbox.Label className="block text-sm font-medium text-gray-700">{title}</Listbox.Label>
+          <Listbox.Label className="block text-sm font-medium text-gray-700">
+            {title}
+          </Listbox.Label>
           <div className="relative mt-1">
             <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
-              <span className="block truncate h-5">{selectedData ? selectedData.user : null}</span>
+              <span className="block truncate h-5">
+                {selectedData ? selectedData.name : null}
+              </span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+                  />
                 </svg>
               </span>
             </Listbox.Button>
@@ -42,25 +57,21 @@ export default function SelectMenuTeamMembers({
                 {teamMembers.map((person) => (
                   <Listbox.Option
                     key={person.id}
-                    className={({ active }) => classNames(active ? 'text-white bg-indigo-600' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9')}
+                    className={({ active }) => classNames(
+                      active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                      'relative cursor-default select-none py-2 pl-3 pr-9',
+                    )}
                     value={person}
                   >
-                    {({ selectedId, active }) => (
-                      <>
-                        <span className={classNames(selectedId ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                          {person.user}
-                        </span>
-
-                        {selectedData ? (
-                          <span
-                            className={classNames(active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4')}
-                          >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                              <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
-                            </svg>
-                          </span>
-                        ) : null}
-                      </>
+                    {({ selectedId }) => (
+                      <span
+                        className={classNames(
+                          selectedId ? 'font-semibold' : 'font-normal',
+                          'block truncate',
+                        )}
+                      >
+                        {person.name}
+                      </span>
                     )}
                   </Listbox.Option>
                 ))}
