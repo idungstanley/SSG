fe2075dd623a274f96f25b0668063e351224b4cb
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-12-08 19:04:13 +0200

    added grid view

---
+++ src/pages/explorer/ExplorerPage/components/ListItems/Grid.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { OutputDateTime } from '../../../../../app/helpers';
+import { FileIcon } from '../../../../../common';
+
+export default function Grid({
+  checkbox,
+  checked,
+  toggleAll,
+  sortedItems,
+  selectedItems,
+  handleChangeItem,
+  handleClick,
+}) {
+  const { selectedItemId } = useSelector((state) => state.explorer);
+
+  return (
+    <>
+      <div className="w-full h-10 border-b">
+        <div className="relative w-12 px-6 sm:w-16 sm:px-8">
+          <input
+            type="checkbox"
+            className="absolute cursor-pointer left-4 top-1/2 mt-3 h-4 w-4 rounded border-gray-300 text-indigo-600 ring-0 focus:ring-0 sm:left-6"
+            ref={checkbox}
+            checked={checked}
+            onChange={toggleAll}
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 p-4">
+        {sortedItems.map((item) => (
+          <button
+            type="button"
+            key={item.id}
+            onClick={(e) => handleClick(e, item.id, item.item_type)}
+            className={`relative flex items-center cursor-pointer space-x-3 rounded-lg border border-gray-300 bg-white px-3 py-5 shadow-sm rind-0 focus:ring-0 hover:border-gray-400 ${
+              selectedItems.includes(item.id) ? 'bg-gray-50' : null
+            } ${selectedItemId === item.id ? 'bg-indigo-100' : null}`}
+          >
+            <div className="relative w-12 px-6 sm:w-16 sm:px-8">
+              {selectedItems.includes(item.id) && (
+                <div className="absolute inset-y-0 left-0 w-0.5 bg-indigo-600" />
+              )}
+              <input
+                type="checkbox"
+                className="absolute left-4 top-1/2 -mt-2 h-4 cursor-pointer w-4 rounded border-gray-300 text-indigo-600 ring-0 focus:ring-0 sm:left-6"
+                value={item.id}
+                checked={selectedItems.includes(item.id)}
+                onChange={(e) => handleChangeItem(e, item.id, item.item_type)}
+              />
+            </div>
+            <div className="flex-shrink-0">
+              <FileIcon extensionKey={item.icon} size={14} />
+            </div>
+            <div className="min-w-0 flex-1 pl-5">
+              <p
+                className={`text-sm font-medium text-left text-gray-900 ${
+                  selectedItems.includes(item.id)
+                    ? 'text-indigo-600'
+                    : 'text-gray-900'
+                }`}
+              >
+                {item.name}
+              </p>
+              <p className="truncate text-sm text-left text-gray-500">
+                {OutputDateTime(item.created_at)}
+              </p>
+            </div>
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
+
+Grid.propTypes = {
+  checkbox: PropTypes.object.isRequired,
+  checked: PropTypes.bool.isRequired,
+  toggleAll: PropTypes.func.isRequired,
+  sortedItems: PropTypes.array.isRequired,
+  selectedItems: PropTypes.array.isRequired,
+  handleChangeItem: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
---
+++ src/pages/explorer/ExplorerPage/components/ListItems/index.jsx
@@ -0,0 +1,187 @@
+/* eslint-disable react/jsx-no-bind */
+import React, {
+  useLayoutEffect, useMemo, useRef, useState,
+} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { Spinner } from '../../../../../common';
+import { useGetExplorerFilesAndFolders } from '../../../../../features/explorer/explorerService';
+import {
+  setSelectedFiles,
+  setSelectedFolders,
+  resetSelectedFilesAndFolders,
+  setSelectedItem,
+  setShowUploadModal,
+} from '../../../../../features/explorer/explorerSlice';
+import FullScreenMessage from '../../../../shared/components/FullScreenMessage';
+import { sortItems } from '../Toolbar/SortingItems';
+
+export default function ExplorerTable() {
+  const dispatch = useDispatch();
+  const { folderId } = useParams();
+  const checkbox = useRef();
+  const [checked, setChecked] = useState(false);
+  const [indeterminate, setIndeterminate] = useState(false);
+  const {
+    selectedFileIds,
+    selectedFolderIds,
+    selectedSorting,
+  } = useSelector((state) => state.explorer);
+  const selectedItems = [...selectedFileIds, ...selectedFolderIds];
+
+  const { data, status } = useGetExplorerFilesAndFolders(folderId);
+
+  const items = [];
+
+  data?.data.folders.map((i) => items.push({
+    icon: 'folder',
+    name: i.name,
+    created_at: i.created_at,
+    size: '-',
+    item_type: 'folder',
+    id: i.id,
+    updated_at: i.updated_at,
+  }));
+
+  data?.data.files.map((i) => items.push({
+    icon: i.file_format.key,
+    name: i.display_name,
+    created_at: i.created_at,
+    size: i.size,
+    item_type: 'file',
+    id: i.id,
+    updated_at: i.updated_at,
+  }));
+
+  useLayoutEffect(() => {
+    const isIndeterminate = selectedItems.length > 0 && selectedItems.length < items?.length;
+
+    if (
+      selectedItems.length === items.length
+      && +selectedItems.length + +items.length > 0
+    ) {
+      setChecked(selectedItems.length === items.length);
+    }
+    setIndeterminate(isIndeterminate);
+    if (checkbox.current) {
+      checkbox.current.indeterminate = isIndeterminate;
+    }
+  }, [selectedItems]);
+
+  function toggleAll() {
+    if (checked || indeterminate) {
+      dispatch(resetSelectedFilesAndFolders());
+    } else {
+      dispatch(
+        setSelectedFiles([
+          ...items.filter((i) => i.item_type === 'file').map((i) => i.id),
+        ]),
+      );
+      dispatch(
+        setSelectedFolders([
+          ...items.filter((i) => i.item_type === 'folder').map((i) => i.id),
+        ]),
+      );
+    }
+
+    setChecked(!checked && !indeterminate);
+    setIndeterminate(false);
+  }
+
+  const handleClick = (e, itemId, type) => {
+    if (selectedItems.length && !e.target.value) {
+      dispatch(resetSelectedFilesAndFolders());
+    }
+
+    if (!e.target.value) {
+      dispatch(
+        setSelectedItem({
+          selectedItemId: itemId,
+          selectedItemType: type,
+        }),
+      );
+      dispatch(
+        type === 'file'
+          ? setSelectedFiles([itemId])
+          : setSelectedFolders([itemId]),
+      );
+    }
+  };
+
+  const handleChangeItem = (e, itemId, type) => {
+    if (!e.target.checked) {
+      dispatch(
+        type === 'file'
+          ? setSelectedFiles([...selectedFileIds.filter((i) => i !== itemId)])
+          : setSelectedFolders([
+            ...selectedFolderIds.filter((i) => i !== itemId),
+          ]),
+      );
+    } else {
+      dispatch(
+        type === 'file'
+          ? setSelectedFiles([...selectedFileIds, itemId])
+          : setSelectedFolders([...selectedFolderIds, itemId]),
+      );
+    }
+  };
+
+  if (status === 'error') {
+    return (
+      <FullScreenMessage
+        title="Oops, an error occurred :("
+        description="Please try again later."
+      />
+    );
+  }
+
+  if (status === 'loading') {
+    return (
+      <div className="mx-auto w-6 mt-10 justify-center">
+        <Spinner size={22} color="#0F70B7" />
+      </div>
+    );
+  }
+
+  const sortedItems = useMemo(
+    () => [
+      ...sortItems(
+        items?.filter((i) => i.item_type === 'folder'),
+        selectedSorting.id,
+      ),
+      ...sortItems(
+        items?.filter((i) => i.item_type === 'file'),
+        selectedSorting.id,
+      ),
+    ],
+    [data, selectedSorting],
+  );
+
+  return !items.length ? (
+    <FullScreenMessage
+      title="No files or folders in your explorer"
+      description="Upload one to start working"
+      ctaText="Upload"
+      ctaOnClick={() => dispatch(setShowUploadModal(true))}
+      showCta
+    />
+  ) : (
+    <div className="flex flex-col px-3 md:px-0">
+      <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+          <div className="relative overflow-hidden">
+            {/* <Table
+              checkbox={checkbox}
+              checked={checked}
+              toggleAll={toggleAll}
+              sortedItems={sortedItems}
+              selectedItems={selectedItems}
+              handleChangeItem={handleChangeItem}
+              handleClick={handleClick}
+            /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
