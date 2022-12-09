b4126c116033527246a32d676dc82f179ad086dc
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-12-09 14:05:27 +0200

    added debounce hook for search

--- src/pages/inbox/InboxPage/components/LeftSidebar/TableWthSelection/index.jsx
+++ src/pages/inbox/InboxPage/components/LeftSidebar/TableWthSelection/index.jsx
@@ -78,6 +78,7 @@ export default function TableWithSelection() {
           inboxFileIndex: index,
         }),
       );
+      setSelectedFiles([fileId]);
     }
   };
 
--- src/pages/search/SearchPage/index.jsx
+++ src/pages/search/SearchPage/index.jsx
@@ -6,17 +6,25 @@ import Toolbar from '../components/header/Toolbar';
 import FilterResults from '../components/header/FilterResults';
 import { useSearchEverything } from '../../../features/search/searchService';
 import { EmptyStateSimple } from '../../../components';
+import { useDebounce } from '../../../hooks';
 
 export default function SearchPage() {
-  const selectedItemId = useSelector((state) => state.search.selectedItemId);
-  const selectedItemType = useSelector((state) => state.search.selectedItemType);
+  const {
+    selectedItemId,
+    selectedItemType,
+    searchQuery,
+    resultsType,
+    searchFileContents,
+  } = useSelector((state) => state.search);
 
-  const searchQuery = useSelector((state) => state.search.searchQuery);
-  const resultsType = useSelector((state) => state.search.resultsType);
-  const searchFileContents = useSelector((state) => state.search.searchFileContents);
+  const debouncedValue = useDebounce(searchQuery, 300);
+  console.log(debouncedValue);
 
-  const { data, status } = useSearchEverything(searchQuery, resultsType, searchFileContents);
-  console.log(data);
+  const { data, status } = useSearchEverything(
+    searchQuery,
+    resultsType,
+    searchFileContents,
+  );
 
   return (
     <div className="h-full flex flex-col w-full">
@@ -24,7 +32,6 @@ export default function SearchPage() {
       <FilterResults />
 
       <div className="flex flex-row overflow-hidden bg-gray-50 h-full">
-
         <div className="flex-1 overflow-y-scroll bg-gray-50">
           {searchQuery.length < 2 && (
             <div className="flex flex-1 h-full bg-white">
@@ -37,26 +44,36 @@ export default function SearchPage() {
             </div>
           )}
 
-          {((status === 'success' && resultsType === 'files' && data.data.files.length === 0 && searchQuery.length >= 2) || (status === 'success' && resultsType === 'folders' && data.data.folders.length === 0 && searchQuery.length >= 2)) && (
-            <div className="flex flex-1 h-full bg-white">
-              <div className="m-auto">
-                <EmptyStateSimple
-                  title="No search results"
-                  description="We couldn't find any matching results"
-                />
+          {((status === 'success'
+            && resultsType === 'files'
+            && data.data.files.length === 0
+            && searchQuery.length >= 2)
+            || (status === 'success'
+              && resultsType === 'folders'
+              && data.data.folders.length === 0
+              && searchQuery.length >= 2)) && (
+              <div className="flex flex-1 h-full bg-white">
+                <div className="m-auto">
+                  <EmptyStateSimple
+                    title="No search results"
+                    description="We couldn't find any matching results"
+                  />
+                </div>
               </div>
-            </div>
           )}
 
-          {((status === 'success' && resultsType === 'files' && data.data.files.length !== 0 && searchQuery.length >= 2) || (status === 'success' && resultsType === 'folders' && data.data.folders.length !== 0 && searchQuery.length >= 2)) && (
-            <SearchResults />
-          )}
+          {((status === 'success'
+            && resultsType === 'files'
+            && data.data.files.length !== 0
+            && searchQuery.length >= 2)
+            || (status === 'success'
+              && resultsType === 'folders'
+              && data.data.folders.length !== 0
+              && searchQuery.length >= 2)) && <SearchResults />}
         </div>
 
         {/* Details sidebar (separate files and folders) */}
-        {selectedItemId !== null && selectedItemType !== null && (
-          <Preview />
-        )}
+        {selectedItemId !== null && selectedItemType !== null && <Preview />}
       </div>
     </div>
   );
