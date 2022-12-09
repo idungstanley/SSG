633224c8b3aa530dea2525b71cb2ad0a0f9089f1
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-12-09 17:42:33 +0200

    added rew search request for searching all items

--- src/features/search/searchService.js
+++ src/features/search/searchService.js
@@ -1,8 +1,70 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import requestNew from '../../app/requestNew';
 
+export const useSearch = (query, searchFileContents, enabled) => {
+  // const queryClient = useQueryClient();
+
+  const explorer = useQuery(
+    ['search_explorer', { query, searchFileContents }],
+    async () => requestNew({
+      url: '/search',
+      method: 'GET',
+      params: {
+        content_search: searchFileContents,
+        query,
+      },
+    }),
+    {
+      enabled: !!enabled && query.length > 2,
+      // onSuccess: (data) => {
+      //   data.data.files?.map((file) => queryClient.setQueryData(['search_everything_file', file.id], file));
+      //   data.data.folders?.map((folder) => queryClient.setQueryData(
+      //     ['search_everything_folder', folder.id],
+      //     folder,
+      //   ));
+      // },
+    },
+  );
+
+  const inbox = useQuery(
+    ['search_inbox', { query, searchFileContents }],
+    async () => requestNew({
+      url: '/search/inbox-files',
+      method: 'GET',
+      params: {
+        query,
+      },
+    }),
+    {
+      enabled: !!enabled && query.length > 2,
+      // onSuccess: (data) => {
+      //   if (resultsType === 'files') {
+      //     data.data.files.map((file) => queryClient.setQueryData(['search_everything_file', file.id], file));
+      //   } else if (resultsType === 'folders') {
+      //     data.data.folders.map((folder) => queryClient.setQueryData(
+      //       ['search_everything_folder', folder.id],
+      //       folder,
+      //     ));
+      //   }
+    },
+  );
+
+  return {
+    files: explorer?.data?.data.files,
+    folders: explorer?.data?.data.folders,
+    inbox: inbox?.data?.data.inbox_files,
+    explorerStatus: explorer?.status,
+    inboxStatus: inbox?.status,
+  };
+};
+
 // Search everything
-export const useSearchEverything = (query, resultsType, searchFileContents, enabled) => {
+export const useSearchEverything = (
+  query,
+  resultsType,
+  searchFileContents,
+  enabled,
+) => {
   const queryClient = useQueryClient();
 
   const url = resultsType === 'files' ? '/search/files' : '/search/folders';
@@ -23,7 +85,10 @@ export const useSearchEverything = (query, resultsType, searchFileContents, enab
         if (resultsType === 'files') {
           data.data.files.map((file) => queryClient.setQueryData(['search_everything_file', file.id], file));
         } else if (resultsType === 'folders') {
-          data.data.folders.map((folder) => queryClient.setQueryData(['search_everything_folder', folder.id], folder));
+          data.data.folders.map((folder) => queryClient.setQueryData(
+            ['search_everything_folder', folder.id],
+            folder,
+          ));
         }
       },
     },
