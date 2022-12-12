d16b2ea770162c3db7078796c2aaae5cf2faf734
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-12-12 13:07:01 +0200

    added better inbox pdf preview

--- src/common/FilePreview.jsx
+++ src/common/FilePreview.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
+import FileViewer from 'react-file-viewer';
 import PropTypes from 'prop-types';
-import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
+// import DocViewer, { DocViewerRenderers } from '@cyntler/react-doc-viewer';
 import FullScreenMessage from '../pages/shared/components/FullScreenMessage';
 
 const docks = ['docx', 'doc', 'pdf'];
 const images = ['jpg', 'png'];
 
 function FilePreview({ fileData, fileExtension }) {
-  const docs = [
-    {
-      fileType: fileExtension,
-      fileData,
-    },
-  ];
+  // const docs = [
+  //   {
+  //     fileType: fileExtension,
+  //     fileData,
+  //   },
+  // ];
+
+  const onError = (e) => {
+    // eslint-disable-next-line no-console
+    console.error('error in file viewer:', e);
+  };
 
   return (
     <div className="react-doc-viewer-wrapper h-full w-full overflow-y-scroll flex-1">
       {images.includes(fileExtension) ? (
         <img src={fileData} alt="img" />
       ) : docks.includes(fileExtension) ? (
-        <DocViewer
-          pluginRenderers={DocViewerRenderers}
-          documents={docs}
-          config={{
-            header: {
-              disableHeader: false,
-              disableFileName: false,
-              retainURLParams: false,
-            },
-          }}
+        // <DocViewer
+        //   pluginRenderers={DocViewerRenderers}
+        //   documents={docs}
+        //   config={{
+        //     header: {
+        //       disableHeader: false,
+        //       disableFileName: false,
+        //       retainURLParams: false,
+        //     },
+        //   }}
+        // />
+        <FileViewer
+          fileType={fileExtension}
+          filePath={fileData}
+          onError={onError}
         />
       ) : (
         <FullScreenMessage
