03f7d1171dd64e5d3129b613f7e84019aaf2405b
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-12-13 18:27:34 +0200

    added new responsible sidebar, header, outlet

--- src/pages/workspace/Index.jsx
+++ src/pages/workspace/Index.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from './sidebar/Sidebar';
+import Header from './sidebar/Header';
+import NewSidebar from './sidebar/NewSidebar';
+// import Sidebar from './sidebar/Sidebar';
+
+// function Index() {
+//   return (
+//     <main className="flex w-full overflow-y-scroll">
+//       <section className="w-2/12">
+//         <Sidebar />
+//       </section>
+//       <section className="w-10/12">
+//         <Outlet />
+//       </section>
+//     </main>
+//   );
+// }
 
 function Index() {
   return (
-    <main className="flex w-full overflow-y-scroll">
-      <section className="w-2/12">
+    <div>
+      <NewSidebar />
+      <div className="flex flex-1 flex-col md:pl-64">
+        <Header />
+        <main className="flex-1">
+          <div className="py-6">
+            <div className="mx-auto px-4 sm:px-6 md:px-8">
+              {/* Replace with your content */}
+              <div className="py-4">
+                <Outlet />
+                {/* <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" /> */}
+              </div>
+              {/* /End replace */}
+            </div>
+          </div>
+        </main>
+      </div>
+
+      {/* <section className="w-2/12">
         <Sidebar />
-      </section>
-      <section className="w-10/12">
-        <Outlet />
-      </section>
-    </main>
+      </section> */}
+    </div>
   );
 }
 
