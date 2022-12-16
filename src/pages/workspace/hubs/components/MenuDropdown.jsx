cf3ab700b68c23ab127ee06c5509ae060b3dfdc7
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-12-16 18:10:53 +0200

    added reusable dropdown for sidebar items

--- src/pages/workspace/hubs/components/MenuDropdown.jsx
+++ src/pages/workspace/hubs/components/MenuDropdown.jsx
@@ -1,16 +1,7 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import {
-  BgColorsOutlined,
-  EllipsisOutlined,
-  LinkOutlined,
-  PlusOutlined,
-  StarOutlined,
-} from '@ant-design/icons';
-import {
   ArchiveIcon,
-  ChevronRightIcon,
   CogIcon,
   DocumentDuplicateIcon,
   EyeOffIcon,
@@ -18,22 +9,130 @@ import {
   ShareIcon,
   SparklesIcon,
   TrashIcon,
+  StarIcon,
+  PlusIcon,
+  LinkIcon,
+  DotsVerticalIcon,
+  ColorSwatchIcon,
 } from '@heroicons/react/outline';
-import { Link } from 'react-router-dom';
-// import PlusDropDown from './PlusDropDown';
+// import { useSelector } from 'react-redux';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 function MenuDropdown() {
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(' ');
-  }
+  // const { currentItemId, type } = useSelector((state) => state.workspace);
+
+  // ! actions here (create, delete, rename)
+
+  const itemsList = [
+    {
+      id: 1,
+      title: 'Create new',
+      onClick: () => {},
+      icon: (
+        <PlusIcon className="h-7 w-5 pt-2 text-gray-700" aria-hidden="true" />
+      ),
+      isVisible: true,
+    },
+    {
+      id: 2,
+      title: 'Rename',
+      onClick: () => {},
+      icon: <PencilIcon className="h-4 w-4" aria-hidden="true" />,
+      isVisible: true,
+    },
+    {
+      id: 3,
+      title: 'Color & Avatar',
+      onClick: () => {},
+      icon: (
+        <ColorSwatchIcon
+          className="h-7 w-5 pt-2 text-gray-700"
+          aria-hidden="true"
+        />
+      ),
+      isVisible: true,
+    },
+    {
+      id: 4,
+      title: 'Copy link',
+      onClick: () => {},
+      icon: <LinkIcon className="h-4 w-4" aria-hidden="true" />,
+      isVisible: true,
+    },
+    {
+      id: 5,
+      title: 'Duplicate',
+      onClick: () => {},
+      icon: <DocumentDuplicateIcon className="h-4 w-4" aria-hidden="true" />,
+      isVisible: true,
+    },
+    {
+      id: 6,
+      title: 'Add to favorites',
+      onClick: () => {},
+      icon: <StarIcon className="h-4 w-4" aria-hidden="true" />,
+      isVisible: true,
+    },
+    {
+      id: 7,
+      title: 'Hide in sidebar',
+      onClick: () => {},
+      icon: <EyeOffIcon className="h-4 w-4" aria-hidden="true" />,
+      isVisible: true,
+    },
+    {
+      id: 8,
+      title: 'Templates',
+      onClick: () => {},
+      icon: (
+        <SparklesIcon
+          className="h-6 w-5 pt-2 text-gray-700"
+          aria-hidden="true"
+        />
+      ),
+      isVisible: true,
+    },
+    {
+      id: 9,
+      title: 'More settings',
+      onClick: () => {},
+      icon: (
+        <CogIcon className="h-6 w-5 pt-2 text-gray-700" aria-hidden="true" />
+      ),
+      isVisible: true,
+    },
+    {
+      id: 10,
+      title: 'Sharing & Permission',
+      onClick: () => {},
+      icon: <ShareIcon className="h-4 w-4" aria-hidden="true" />,
+      isVisible: true,
+    },
+    {
+      id: 11,
+      title: 'Archive',
+      onClick: () => {},
+      icon: <ArchiveIcon className="h-4 w-4" aria-hidden="true" />,
+      isVisible: true,
+    },
+    {
+      id: 12,
+      title: 'Delete',
+      onClick: () => {},
+      icon: <TrashIcon className="h-4 w-4" aria-hidden="true" />,
+      isVisible: true,
+    },
+  ];
 
   return (
-    <Menu as="div" className="">
-      <div>
-        <Menu.Button className="text-gray-400 mt-4 flex text-sm">
-          <EllipsisOutlined className="h-6 w-6" aria-hidden="true" />
-        </Menu.Button>
-      </div>
+    <Menu as="div">
+      <Menu.Button className="text-gray-400 flex text-sm">
+        <DotsVerticalIcon className="h-5 w-5" aria-hidden="true" />
+      </Menu.Button>
+
       <Transition
         as={Fragment}
         enter="transition ease-out duration-100"
@@ -44,213 +143,21 @@ function MenuDropdown() {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="origin-top-right absolute bottom-20 left-50 z-10 -mt-28 w-56 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center justify-between space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <div className="flex items-center space-x-1">
-                  <PlusOutlined
-                    className="h-7 w-5 pt-2 text-gray-700"
-                    aria-hidden="true"
-                  />
-                  <p>Create new</p>
+          {itemsList.map((item) => (item.isVisible ? (
+            <Menu.Item key={item.id} onClick={item.onClick}>
+              {({ active }) => (
+                <div
+                  className={classNames(
+                    active ? 'bg-gray-100' : '',
+                    'flex items-center space-x-2 px-4 py-2 text-sm text-red-700 text-left',
+                  )}
+                >
+                  <TrashIcon className="h-4 w-4" aria-hidden="true" />
+                  <p>{item.title}</p>
                 </div>
-                <ChevronRightIcon
-                  className="h-5 w-5 pt-2 text-gray-700"
-                  aria-hidden="true"
-                />
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <PencilIcon className="h-4 w-4" aria-hidden="true" />
-                <p>Rename</p>
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center justify-between space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <div className="flex items-center space-x-1">
-                  <BgColorsOutlined
-                    className="h-7 w-5 pt-2 text-gray-700"
-                    aria-hidden="true"
-                  />
-                  <p>Color & Avatar</p>
-                </div>
-                <ChevronRightIcon
-                  className="h-5 w-5 pt-2 text-gray-700"
-                  aria-hidden="true"
-                />
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <LinkOutlined className="h-4 w-4" aria-hidden="true" />
-                <p>Copy link</p>
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <DocumentDuplicateIcon className="h-4 w-4" aria-hidden="true" />
-                <p>Duplicate</p>
-              </Link>
-            )}
-          </Menu.Item>
-          <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <StarOutlined className="h-4 w-4" aria-hidden="true" />
-                <p>Add to favorites</p>
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <EyeOffIcon className="h-4 w-4" aria-hidden="true" />
-                <p>Hide in my sidebar</p>
-              </Link>
-            )}
-          </Menu.Item>
-          <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center justify-between space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <div className="flex items-center space-x-1">
-                  <SparklesIcon
-                    className="h-6 w-5 pt-2 text-gray-700"
-                    aria-hidden="true"
-                  />
-                  <p>Templates</p>
-                </div>
-                <ChevronRightIcon
-                  className="h-5 w-5 pt-2 text-gray-700"
-                  aria-hidden="true"
-                />
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center justify-between space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <div className="flex items-center space-x-1">
-                  <CogIcon
-                    className="h-6 w-5 pt-2 text-gray-700"
-                    aria-hidden="true"
-                  />
-                  <p>More settings</p>
-                </div>
-                <ChevronRightIcon
-                  className="h-5 w-5 pt-2 text-gray-700"
-                  aria-hidden="true"
-                />
-              </Link>
-            )}
-          </Menu.Item>
-          <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <ShareIcon className="h-4 w-4" aria-hidden="true" />
-                <p>Sharing & Permission</p>
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center space-x-2 px-4 py-2 text-sm text-gray-700 text-left',
-                )}
-              >
-                <ArchiveIcon className="h-4 w-4" aria-hidden="true" />
-                <p>Archive</p>
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
-                to="#"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'flex items-center space-x-2 px-4 py-2 text-sm text-red-700 text-left',
-                )}
-              >
-                <TrashIcon className="h-4 w-4" aria-hidden="true" />
-                <p>Delete</p>
-              </Link>
-            )}
-          </Menu.Item>
+              )}
+            </Menu.Item>
+          ) : null))}
         </Menu.Items>
       </Transition>
     </Menu>
