8b215ef64547d5bf96a144789b7cc56d91edc918
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-12-21 19:59:46 +0200

    added new layout for shared data

--- src/pages/shared/index.jsx
+++ src/pages/shared/index.jsx
@@ -1,54 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import {
-  useGetFile,
-  useGetFolder,
-  useGetSharedFilesAndFolders,
-} from '../../features/shared/sharedService';
-import { Spinner } from '../../common';
 import SharedTable from './components/SharedTable';
-import FullScreenMessage from '../../components/CenterMessage/FullScreenMessage';
-import ItemPreviewSidebar from '../../components/ItemPreviewSidebar';
+import Toolbar from './components/Toolbar';
 
 export default function SharedPage() {
-  const { data, filesStatus, foldersStatus } = useGetSharedFilesAndFolders();
-
-  const { selectedItemId, selectedItemType } = useSelector((state) => state.shared);
-
-  const { data: item } = selectedItemType === 'file'
-    ? useGetFile(selectedItemId)
-    : useGetFolder(selectedItemId);
-
   return (
     <div className="h-full flex flex-col w-full">
-      <div className="flex flex-row overflow-hidden h-full">
-        <div className="flex-1 overflow-y-scroll">
-          {filesStatus === 'loading' || foldersStatus === 'loading' ? (
-            <div className="mx-auto w-6 mt-10 justify-center">
-              <Spinner size={22} color="#0F70B7" />
-            </div>
-          ) : filesStatus === 'error' || foldersStatus === 'error' ? (
-            <FullScreenMessage
-              title="Oops, an error occurred :("
-              description="Please try again later."
-            />
-          ) : data && (data.folders.length === 0 || data.files.length === 0) ? (
-            <div className="flex flex-1 h-full bg-white">
-              <div className="m-auto">
-                <FullScreenMessage
-                  title="No shared files or folders"
-                  description="Ask someone to share a folder or file with you"
-                />
-              </div>
-            </div>
-          ) : (
-            <SharedTable data={data} />
-          )}
-        </div>
-
-        {selectedItemId ? (
-          <ItemPreviewSidebar item={item} type={selectedItemType} />
-        ) : null}
+      <div className="w-full">
+        <Toolbar />
+      </div>
+      <div className="relative flex-1 overflow-y-scroll overflow-x-none bg-white align-middle min-w-full overflow-hidden h-full">
+        <SharedTable />
       </div>
     </div>
   );
