78483d76d662ca19b712f78ed2ed2db3e5144650
devransom-github <sundaystanley56@gmail.com>
2022-12-26 21:13:01 +0100

    Updated changes on txs

--- src/components/avatar/StatusDot.tsx
+++ src/components/avatar/StatusDot.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
+interface statusType {
+  on: string
+  colour: string
+  size: number
+  ringSize: number
+  top: boolean
+}
 
 function StatusDot({
   on, colour, size, ringSize, top,
-}) {
+}: statusType) {
   return (
-    <span className="inline-block relative">
+    <span className="relative inline-block">
       {on}
       <span
         className={`absolute ${
@@ -22,14 +29,6 @@ StatusDot.defaultProps = {
   colour: '#4ADE80',
   ringSize: 2,
   top: true,
-};
-
-StatusDot.propTypes = {
-  on: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
-  colour: PropTypes.string,
-  size: PropTypes.number,
-  ringSize: PropTypes.number,
-  top: PropTypes.bool,
 };
 
 export default StatusDot;
