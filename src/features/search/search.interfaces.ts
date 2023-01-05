7260f12c3646d7c1253f2b2688e4bfb79f5d0433
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-01-05 20:45:28 +0200

    added better types for search

---
+++ src/features/search/search.interfaces.ts
@@ -0,0 +1,45 @@
+interface IFile {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  size: number;
+  file_format: {
+    extension: string;
+  };
+  display_name: string;
+  ancestor_path: null | string;
+}
+
+interface IFolder {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  ancestor_path: null | string;
+  name: string;
+}
+
+interface IInboxFile {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  inbox_file_source: {
+    size: number;
+    file_format: {
+      extension: string;
+    };
+    display_name: string;
+  };
+}
+
+export interface IExplorerSearchRes {
+  data: {
+    files: IFile[];
+    folders: IFolder[];
+  };
+}
+
+export interface IInboxSearchRes {
+  data: {
+    inbox_files: IInboxFile[];
+  };
+}
