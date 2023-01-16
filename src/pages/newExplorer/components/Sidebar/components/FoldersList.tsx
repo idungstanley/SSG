08aa8a42ea20c2fe697dbdce84ec849ed806bd3e
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-01-16 19:13:50 +0200

    added invalidation queries for move

--- src/pages/newExplorer/components/Sidebar/components/FoldersList.tsx
+++ src/pages/newExplorer/components/Sidebar/components/FoldersList.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useGetExplorerFolder } from '../../../../../features/explorer/explorerService';
 import { useNavigate, useParams } from 'react-router-dom';
 import FolderItem from './FolderItem';
@@ -7,6 +7,10 @@ import {
   setSelectedFolderId,
   setSelectedItem,
 } from '../../../../../features/explorer/explorerSlice';
+import { DndContext, DragEndEvent, useDroppable } from '@dnd-kit/core';
+import { useMoveExplorerItems } from '../../../../../features/explorer/explorerActionsService';
+import { classNames } from '../../../../../utils';
+import { resetSelectedItem } from '../../../../../features/search/searchSlice';
 
 interface FoldersListProps {
   folders: {
@@ -27,6 +31,14 @@ export default function FoldersList({
   const { folderId } = useParams();
   const { selectedFolderId } = useAppSelector((state) => state.explorer);
   const { data: sub } = useGetExplorerFolder(folderId);
+
+  // parent id for invalidate active query
+  const [parentOfDraggable, setParentOfDraggable] = useState<{
+    parentId: string;
+    overId: string;
+  } | null>(null);
+
+  const { mutate: onMove } = useMoveExplorerItems(parentOfDraggable);
 
   const handleClickFolder = (folderId: string, parentId: string | null) => {
     const isActiveFolder = selectedFolderId === folderId;
@@ -63,7 +75,7 @@ export default function FoldersList({
       : selectedFolder?.ancestors?.length) || 0;
 
   return (
-    <>
+    <DndContext onDragEnd={handleDragEnd}>
       {folders.map((rootFolder) => (
         <div key={rootFolder.id}>
           {/* root folders list */}
@@ -144,6 +156,52 @@ export default function FoldersList({
           ) : null}
         </div>
       ))}
-    </>
+      <DragOverRoot />
+    </DndContext>
+  );
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { over, active } = event;
+
+    const overId = over?.id;
+    const activeId = active.id;
+
+    if (overId && activeId) {
+      if (overId !== activeId) {
+        if (activeId === selectedFolderId) {
+          dispatch(resetSelectedItem());
+          navigate('/new-explorer');
+        }
+
+        setParentOfDraggable({
+          parentId: active.data.current?.parentId as string,
+          overId: overId === 'root' ? '' : (overId as string),
+        });
+        onMove({
+          targetFolderId: overId === 'root' ? '' : (overId as string),
+          folderIds: [activeId as string],
+        });
+      }
+    }
+  }
+}
+
+function DragOverRoot() {
+  const { isOver, setNodeRef: droppableRef } = useDroppable({
+    id: 'root',
+    data: { parentId: null },
+  });
+
+  return (
+    <div
+      className={classNames(
+        'mt-2 w-full border text-center py-1 px-1 hover:bg-gray-100',
+
+        isOver ? 'bg-primary-100' : ''
+      )}
+      ref={droppableRef}
+    >
+      Drag here to move to root folder
+    </div>
   );
 }
