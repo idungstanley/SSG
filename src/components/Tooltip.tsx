0b80ef9c84dcacb3473e5c99e678527cee9d8831
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-01-19 16:55:13 +0200

    improved styles

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,7 +1,4 @@
-import React, {
-  ReactNode,
-  //  useRef
-} from 'react';
+import React, { ReactNode } from 'react';
 
 interface ToolTipProps {
   children: ReactNode;
@@ -9,31 +6,9 @@ interface ToolTipProps {
 }
 
 export default function ToolTip({ children, tooltip }: ToolTipProps) {
-  // const tooltipRef = useRef<HTMLSpanElement>(null);
-  // const container = useRef<HTMLDivElement>(null);
-
   return (
     <div data-tooltip={tooltip} className="tooltip">
       {children}
     </div>
-    // <div
-    //   ref={container}
-    //   onMouseEnter={({ clientX }) => {
-    //     if (!tooltipRef.current || !container.current) return;
-    //     const { left } = container.current.getBoundingClientRect();
-
-    //     tooltipRef.current.style.left = clientX - left + 'px';
-    //   }}
-    //   className="group relative inline-block"
-    // >
-    //   {children}
-
-    //   <span
-    //     ref={tooltipRef}
-    //     className="absolute z-20 top-full whitespace-nowrap opacity-0 transition mt-2 group-hover:opacity-100 bg-gray-700 text-white p-2 rounded-xl"
-    //   >
-    //     {tooltip}
-    //   </span>
-    // </div>
   );
 }
