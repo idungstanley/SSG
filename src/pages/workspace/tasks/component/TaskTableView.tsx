c1b203391348d88e117e6a3ed625eed98191e1db
Omachi John <omachi.john@yahoo.com>
2023-01-23 21:09:28 +0100

    Render assignees jsx

--- src/pages/workspace/tasks/component/TaskTableView.tsx
+++ src/pages/workspace/tasks/component/TaskTableView.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useAppSelector } from '../../../../app/hooks';
-import MaterialTable from 'material-table';
-import { ThemeProvider, createTheme } from '@mui/material';
-import { BiExport } from 'react-icons/bi';
-import { BiHide } from 'react-icons/bi';
-import { BsSearch } from 'react-icons/bs';
-import { AiOutlineFilter } from 'react-icons/ai';
-import { FaSort } from 'react-icons/fa';
+import React, { useEffect, useState } from "react";
+import { useAppSelector } from "../../../../app/hooks";
+import MaterialTable from "material-table";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { BiExport } from "react-icons/bi";
+import { BiHide } from "react-icons/bi";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineFilter } from "react-icons/ai";
+import { FaSort } from "react-icons/fa";
+import { AvatarWithInitials } from "../../../../components";
 
 function TaskTableView() {
   const defaultMaterialTheme = createTheme();
@@ -35,16 +36,43 @@ function TaskTableView() {
 
   const dynamicColum: any = [];
 
-  // console.log("columnHead", columnHead);
+  const groupAssignee = (data) => {
+    return data?.map((newData) => (
+      <>
+        <span key={newData.id} className="flex-1">
+          <AvatarWithInitials
+            initials={newData.initials}
+            backgroundColour={newData.colour}
+            height="h-5"
+            width="w-5"
+          />
+        </span>
+      </>
+    ));
+  };
 
   columnHead[0].map((column) => {
     const singleColumn = {
       title:
-        column.split('_').join(' ').toUpperCase() == 'NAME'
-          ? 'TASKS'
-          : column.split('_').join(' ').toUpperCase(),
+        column.split("_").join(" ").toUpperCase() == "NAME"
+          ? "TASKS"
+          : column.split("_").join(" ").toUpperCase(),
       field: column,
       emptyValue: () => <p>-</p>,
+
+      render:
+        column == "assignees"
+          ? (newData) => (
+              <span key={newData.id} className="flex-1">
+                <AvatarWithInitials
+                  initials={newData.initials}
+                  backgroundColour={newData.colour}
+                  height="h-5"
+                  width="w-5"
+                />
+              </span>
+            )
+          : null,
     };
     dynamicColum.push(singleColumn);
   });
@@ -59,7 +87,7 @@ function TaskTableView() {
             columns={dynamicColum}
             data={editable ?? []}
             options={{
-              searchFieldAlignment: 'right',
+              searchFieldAlignment: "right",
               // filtering: true,
               exportButton: true,
               selection: true,
@@ -68,10 +96,11 @@ function TaskTableView() {
               columnResizable: false,
               columnsButton: true,
               headerStyle: {
-                fontSize: '10px',
+                fontSize: "10px",
               },
-              rowStyle: { fontSize: '10px' },
-              maxBodyHeight: '300px',
+              rowStyle: { fontSize: "10px" },
+              maxBodyHeight: "300px",
+              // doubleHorizontalScroll: false,
             }}
             icons={icons}
           />
