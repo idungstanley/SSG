1f3286f8a74ce9788c2c717b39a19f65a82808f5
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-01-23 17:49:34 +0200

    added better styles, fixed wrong folder info

--- src/components/Chat/components/ChatList.tsx
+++ src/components/Chat/components/ChatList.tsx
@@ -31,43 +31,46 @@ export default function ChatsList({ selectChat }: ChatsListProps) {
   ) : !data?.length ? (
     <FullScreenMessage title="No chats." description="Create one." />
   ) : (
-    <ul role="list" className="divide-y divide-gray-200 flex flex-wrap">
-      {data?.map((chat) => (
-        <li
-          key={chat.id}
-          className="grid grid-cols-sidebarItem items-center justify-between w-full hover:bg-gray-100 p-2"
-        >
-          {/* avatar */}
-          <span
-            onClick={() => selectChat(chat.id)}
-            className="flex items-center cursor-pointer justify-center p-1 rounded-full w-12 h-12 bg-indigo-600 text-white pb-1 uppercase font-medium text-xl"
+    <>
+      <h2 className="text-lg font-medium text-gray-900 border-b p-2">Chats</h2>
+      <ul role="list" className="divide-y divide-gray-200 flex flex-wrap">
+        {data?.map((chat) => (
+          <li
+            key={chat.id}
+            className="grid grid-cols-sidebarItem items-center justify-between w-full hover:bg-gray-100 p-2"
           >
-            {chat.name.slice(0, 2)}
-          </span>
-
-          {/* chat name and last message */}
-          <div
-            onClick={() => selectChat(chat.id)}
-            className="ml-3 cursor-pointer"
-          >
-            <p className="font-medium text-gray-900">{chat.name}</p>
-            <p className="text-sm text-gray-500">message</p>
-          </div>
-
-          {/* action buttons */}
-          <div className="flex items-center gap-2">
-            <span className="flex bg-indigo-600 text-xs text-white w-6 h-6 justify-center items-center rounded-full">
-              {chat.new_messages_count}
+            {/* avatar */}
+            <span
+              onClick={() => selectChat(chat.id)}
+              className="flex items-center cursor-pointer justify-center p-1 rounded-full w-12 h-12 bg-indigo-600 text-white pb-1 uppercase font-medium text-xl"
+            >
+              {chat.name.slice(0, 2)}
             </span>
-            <TrashIcon
-              onClick={() => handleDelete(chat.id)}
-              className="h-5 w-5 cursor-pointer"
-              aria-hidden="true"
-              id="trashIcon"
-            />
-          </div>
-        </li>
-      ))}
-    </ul>
+
+            {/* chat name and last message */}
+            <div
+              onClick={() => selectChat(chat.id)}
+              className="ml-3 cursor-pointer"
+            >
+              <p className="font-medium text-gray-900">{chat.name}</p>
+              <p className="text-sm text-gray-500">message</p>
+            </div>
+
+            {/* action buttons */}
+            <div className="flex items-center gap-2">
+              <span className="flex bg-indigo-600 text-xs text-white w-6 h-6 justify-center items-center rounded-full">
+                {chat.new_messages_count}
+              </span>
+              <TrashIcon
+                onClick={() => handleDelete(chat.id)}
+                className="h-5 w-5 cursor-pointer"
+                aria-hidden="true"
+                id="trashIcon"
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
