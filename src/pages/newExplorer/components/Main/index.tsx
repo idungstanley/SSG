941182778f20f189c0f15b4516a3bb955174838d
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-01-27 18:56:07 +0200

    added dynamic grid styles for pilot

--- src/pages/newExplorer/components/Main/index.tsx
+++ src/pages/newExplorer/components/Main/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import {
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+} from '@heroicons/react/24/outline';
+import React, { useState } from 'react';
 import { useAppSelector } from '../../../../app/hooks';
 import { classNames } from '../../../../utils';
+import Pilot from '../Pilot';
 
 import FilePreview from './components/FilePreview';
 import FilesListWithToolbar from './components/FilesListWithToolbar';
@@ -9,17 +14,58 @@ export default function Main() {
   const { settings } = useAppSelector((state) => state.account);
   const { showPreview } = settings;
 
-  const { fastPreview } = useAppSelector((state) => state.explorer);
+  const [showExpandedPilot, setShowExpandedPilot] = useState(false);
+
+  const { selectedFileId, selectedFolderId, fastPreview } = useAppSelector(
+    (state) => state.explorer
+  );
+
+  const showPilot = !!selectedFolderId || !!selectedFileId;
 
   // show only if preview toggle or fast preview is enabled
-  const show = showPreview || fastPreview.show;
+  const showFilePreview = showPreview || fastPreview.show;
+
+  const gridCols = showFilePreview
+    ? showPilot
+      ? showExpandedPilot
+        ? 'grid-cols-3'
+        : 'grid-cols-frFrAuto'
+      : 'grid-cols-2'
+    : showPilot
+    ? showExpandedPilot
+      ? 'grid-cols-2'
+      : 'grid-cols-frAuto'
+    : 'grid-cols-1';
 
   return (
-    <div className={classNames('border-t', show ? 'grid grid-cols-2' : '')}>
+    <div className={classNames('border-t grid', gridCols)}>
       <FilesListWithToolbar />
 
       {/* file preview */}
-      {show ? <FilePreview /> : null}
+      {showFilePreview ? <FilePreview /> : null}
+
+      {showPilot ? (
+        <div
+          className={classNames(
+            showPilot ? 'space-y-2' : '',
+            'p-2 border-l h-full'
+          )}
+        >
+          <button
+            type="button"
+            onClick={() => setShowExpandedPilot((prev) => !prev)}
+            className="text-gray-500"
+          >
+            {showPilot ? (
+              <ChevronDoubleRightIcon className="w-5 h-5" />
+            ) : (
+              <ChevronDoubleLeftIcon className="w-5 h-5" />
+            )}
+          </button>
+
+          {showExpandedPilot ? <Pilot /> : null}
+        </div>
+      ) : null}
     </div>
   );
 }
--- src/pages/newExplorer/components/Pilot/components/Nav.tsx
+++ src/pages/newExplorer/components/Pilot/components/Nav.tsx
@@ -12,37 +12,33 @@ import { classNames } from '../../../../../utils';
 const tabs = [
   {
     id: 0,
-    label: 'Information',
-    icon: (
-      <InformationCircleIcon className="w-6 h-6 text-gray-300 cursor-pointer" />
-    ),
+    label: 'Details',
+    icon: <InformationCircleIcon className="w-5 h-5" />,
   },
   {
     id: 1,
-    label: 'History',
-    icon: <ClockIcon className="w-6 h-6 text-gray-300 cursor-pointer" />,
+    label: 'Logs',
+    icon: <ClockIcon className="w-5 h-5" />,
   },
   {
     id: 2,
     label: 'Permissions',
-    icon: <LockClosedIcon className="w-6 h-6 text-gray-300 cursor-pointer" />,
+    icon: <LockClosedIcon className="w-5 h-5" />,
   },
   {
     id: 3,
-    label: 'Communication',
-    icon: <SignalIcon className="w-6 h-6 text-gray-300 cursor-pointer" />,
+    label: 'Comments',
+    icon: <ChatBubbleLeftEllipsisIcon className="w-5 h-5" />,
   },
   {
     id: 4,
     label: 'Watchers',
-    icon: <EyeIcon className="w-6 h-6 text-gray-300 cursor-pointer" />,
+    icon: <EyeIcon className="w-5 h-5" />,
   },
   {
     id: 5,
-    label: 'Comments',
-    icon: (
-      <ChatBubbleLeftEllipsisIcon className="w-6 h-6 text-gray-300 cursor-pointer" />
-    ),
+    label: 'Communication',
+    icon: <SignalIcon className="w-5 h-5" />,
   },
 ];
 
@@ -58,7 +54,7 @@ export default function Nav({ activeTabId, setActiveTabId }: NavProps) {
 
   return (
     <nav
-      className="flex flex-wrap w-full gap-4 pb-5 border-b"
+      className="flex flex-wrap gap-1 w-full pb-5 border-b"
       aria-label="Tabs"
     >
       {tabs.map((tab) => (
@@ -67,12 +63,13 @@ export default function Nav({ activeTabId, setActiveTabId }: NavProps) {
           onClick={() => handleClick(tab.id)}
           className={classNames(
             tab.id === activeTabId
-              ? 'bg-indigo-100 text-indigo-700'
+              ? 'bg-gray-100 text-gray-700'
               : 'text-gray-500 hover:text-gray-700 hover:bg-gray-50',
-            'px-3 py-2 font-medium text-sm rounded-md cursor-pointer flex flex-grow justify-center border transition'
+            'flex cursor-pointer text-sm flex-grow items-center gap-2 px-3 py-2 fon justify-center border transition'
           )}
           aria-current={tab.id === activeTabId ? 'page' : undefined}
         >
+          {tab.icon}
           {tab.label}
         </span>
       ))}
