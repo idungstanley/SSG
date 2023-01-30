2d08cbf3f332134c9a3ea3f1ebe03320061f39b8
Omachi John <omachi.john@yahoo.com>
2023-01-30 12:37:54 +0100

    create task on hub level

--- src/pages/workspace/hubs/components/renderHubs/items/itemsListData/ItemsListsData.tsx
+++ src/pages/workspace/hubs/components/renderHubs/items/itemsListData/ItemsListsData.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { useAppDispatch, useAppSelector } from "../../../../../../../app/hooks";
 import { getTaskListService } from "../../../../../../../features/task/taskService";
+import { setAddNewTaskItem } from "../../../../../../../features/task/taskSlice";
+import AddNewItem from "../../../../../tasks/component/taskColumn/AddNewItem";
 import TaskData from "../../../../../tasks/component/taskData/TaskData";
 
 interface ItemsListsDataProps {
@@ -7,6 +10,9 @@ interface ItemsListsDataProps {
 }
 export default function ItemsListsData({ listId }: ItemsListsDataProps) {
   const { data } = getTaskListService({ listId });
+
+  const { addNewTaskItem } = useAppSelector((state) => state.task);
+  const dispatch = useAppDispatch();
 
   return (
     <section>
@@ -19,6 +25,17 @@ export default function ItemsListsData({ listId }: ItemsListsDataProps) {
             </div>
           );
         })}
+
+        {/* {addNewTaskItem && <AddNewItem listId={listId} />}
+        <div
+          className=""
+          id="newItem"
+          onClick={() => dispatch(setAddNewTaskItem(!addNewTaskItem))}
+        >
+          <p className="pl-2 text-xs  w-20 mt-1 cursor-pointer ml-10 font-semibold text-gray-400">
+            + New Task
+          </p>
+        </div> */}
       </div>
     </section>
   );
