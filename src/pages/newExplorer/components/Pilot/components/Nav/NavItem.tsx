ed9a0e3e745974836fd35a0c2b4e25e85c619cd5
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-01-31 21:30:14 +0200

    destructured nav component

---
+++ src/pages/newExplorer/components/Pilot/components/Nav/NavItem.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { classNames } from '../../../../../../utils';
+import { MdDragIndicator } from 'react-icons/md';
+import { useSortable } from '@dnd-kit/sortable';
+
+interface NavItemProps {
+  id: number;
+  label: string;
+  icon: JSX.Element;
+  activeTabId: number;
+  setActiveTabId: (i: number) => void;
+}
+
+export default function NavItem({
+  id,
+  label,
+  icon,
+  activeTabId,
+  setActiveTabId,
+}: NavItemProps) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id,
+  });
+
+  const style = {
+    transform: transform
+      ? `translate(${transform.x}px, ${transform.y}px)`
+      : undefined,
+    transition,
+    backgroundColor: isDragging ? '#f3f4f6' : undefined, // ? bg for draggable, can be replaced by any style
+    zIndex: isDragging ? 1 : undefined, // important for overlay
+  };
+
+  const handleClick = (tabId: number) => {
+    setActiveTabId(tabId);
+  };
+
+  return (
+    <div
+      style={style}
+      onClick={() => handleClick(id)}
+      className={classNames(
+        id === activeTabId
+          ? 'bg-gray-100 text-gray-700'
+          : 'text-gray-500 hover:text-gray-700 hover:bg-gray-50',
+        'group flex cursor-pointer text-sm items-center gap-2 px-3 py-2 pr-7 border justify-between'
+      )}
+      aria-current={id === activeTabId ? 'page' : undefined}
+    >
+      {/* drag area */}
+      <span
+        ref={setNodeRef}
+        {...attributes}
+        {...listeners}
+        className="opacity-0 group-hover:opacity-100"
+      >
+        <MdDragIndicator aria-hidden="true" className="w-4 h-4 cursor-move" />
+      </span>
+
+      {/* main content */}
+      <div className="flex gap-2 truncate">
+        {icon}
+        <p title={label}>{label}</p>
+      </div>
+      <div />
+    </div>
+  );
+}
--- src/pages/newExplorer/components/Pilot/components/Nav/index.tsx
+++ src/pages/newExplorer/components/Pilot/components/Nav/index.tsx
@@ -7,8 +7,6 @@ import {
   LockClosedIcon,
   SignalIcon,
 } from '@heroicons/react/24/outline';
-import { classNames } from '../../../../../utils';
-import { MdDragIndicator } from 'react-icons/md';
 import {
   closestCenter,
   DndContext,
@@ -23,8 +21,8 @@ import {
   rectSortingStrategy,
   SortableContext,
   sortableKeyboardCoordinates,
-  useSortable,
 } from '@dnd-kit/sortable';
+import NavItem from './NavItem';
 
 const tabs = [
   {
@@ -127,76 +125,5 @@ export default function Nav({ activeTabId, setActiveTabId }: NavProps) {
         </SortableContext>
       </nav>
     </DndContext>
-  );
-}
-
-interface NavItemProps {
-  id: number;
-  label: string;
-  icon: JSX.Element;
-  activeTabId: number;
-  setActiveTabId: (i: number) => void;
-}
-
-function NavItem({
-  id,
-  label,
-  icon,
-  activeTabId,
-  setActiveTabId,
-}: NavItemProps) {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-    isDragging,
-  } = useSortable({
-    id,
-  });
-
-  const style = {
-    transform: transform
-      ? `translate(${transform.x}px, ${transform.y}px)`
-      : undefined,
-    transition,
-    backgroundColor: isDragging ? '#f3f4f6' : undefined,
-    zIndex: isDragging ? 1 : undefined, // important for overlay
-  };
-
-  const handleClick = (tabId: number) => {
-    setActiveTabId(tabId);
-  };
-
-  return (
-    <div
-      style={style}
-      onClick={() => handleClick(id)}
-      className={classNames(
-        id === activeTabId
-          ? 'bg-gray-100 text-gray-700'
-          : 'text-gray-500 hover:text-gray-700 hover:bg-gray-50',
-        'group flex cursor-pointer text-sm items-center gap-2 px-3 py-2 pr-7 border justify-between'
-      )}
-      aria-current={id === activeTabId ? 'page' : undefined}
-    >
-      {/* drag area */}
-      <span
-        ref={setNodeRef}
-        {...attributes}
-        {...listeners}
-        className="opacity-0 group-hover:opacity-100"
-      >
-        <MdDragIndicator aria-hidden="true" className="w-4 h-4 cursor-move" />
-      </span>
-
-      {/* main content */}
-      <div className="flex gap-2 truncate">
-        {icon}
-        <p title={label}>{label}</p>
-      </div>
-      <div />
-    </div>
   );
 }
