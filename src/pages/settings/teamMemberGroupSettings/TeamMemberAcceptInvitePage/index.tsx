f7107385895c20b7014310db31e897a3ed8ac3a9
diamond-nicholas <diamondnicholas154@gmail.com>
2023-02-13 12:26:17 +0000

    updage invite logic

--- src/pages/settings/teamMemberGroupSettings/TeamMemberAcceptInvitePage/index.tsx
+++ src/pages/settings/teamMemberGroupSettings/TeamMemberAcceptInvitePage/index.tsx
@@ -3,8 +3,8 @@ import { useParams } from 'react-router-dom';
 import { useAcceptTeamMemberInvite } from '../../../../features/settings/teamMemberInvites/teamMemberInviteService';
 import RegisterPage from '../../../workspace/createWorkspace/auth/RegisterPage/index';
 import NotFoundPage from '../../../NotFoundPage';
-import AcceptInvite from './AcceptInvite';
 
+import { Button } from '../../../../components';
 export default function TeamMemberAcceptInvite() {
   const { inviteCode } = useParams();
 
@@ -14,13 +14,35 @@ export default function TeamMemberAcceptInvite() {
     }
   }, [inviteCode]);
 
-  const { data } = useAcceptTeamMemberInvite();
-  const user = data?.data.user;
+  const { status, data } = useAcceptTeamMemberInvite();
+
+  const handleAcceptInvite = () => {
+    if (status == 'success') {
+      localStorage.setItem('user', JSON.stringify(data?.data.user));
+    } else if (data?.message == 'Unauthenticated') {
+      <RegisterPage />;
+    }
+    window.location.href = '/workspace';
+  };
 
   return data?.message == 'Unauthenticated' ? (
     <RegisterPage />
   ) : inviteCode ? (
-    <AcceptInvite user={user} />
+    <main className="flex min-h-full flex-col bg-white mx-auto w-full max-w-7xl flex-grow justify-center px-4 sm:px-6 lg:px-8 text-center">
+      <p className="mt-2 text-base text-gray-500">
+        Click on the button to proceed into workspace
+      </p>
+      <div className="mt-6">
+        <Button
+          buttonStyle="primary"
+          onClick={handleAcceptInvite}
+          label={'Accept'}
+          padding="py-2 px-4"
+          height="h-10"
+          width="w-40"
+        />
+      </div>
+    </main>
   ) : (
     <NotFoundPage />
   );
