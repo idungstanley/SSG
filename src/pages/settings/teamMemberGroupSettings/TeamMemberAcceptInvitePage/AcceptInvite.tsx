0342ee15e810e4a9ee35c7411193bc7c54c3225e
diamond-nicholas <diamondnicholas154@gmail.com>
2023-02-13 11:51:52 +0000

    update invite logic

--- src/pages/settings/teamMemberGroupSettings/TeamMemberAcceptInvitePage/AcceptInvite.tsx
+++ src/pages/settings/teamMemberGroupSettings/TeamMemberAcceptInvitePage/AcceptInvite.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Button } from '../../../../components';
 
-export default function AcceptInvite() {
+interface AcceptInviteProps {
+  user: {
+    avatar_path: string | null;
+    colour: string;
+    default_workspace_id: string | null;
+    email: string;
+    initials: string;
+    name: string;
+    timezone: string;
+  };
+}
+
+export default function AcceptInvite({ user }: AcceptInviteProps) {
   const handleAcceptInvite = () => {
+    localStorage.setItem('user', JSON.stringify(user));
     window.location.href = '/workspace';
   };
   return (
     <main className="flex min-h-full flex-col bg-white mx-auto w-full max-w-7xl flex-grow justify-center px-4 sm:px-6 lg:px-8 text-center">
-      <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+      <p className="mt-2 text-base text-gray-500">
         Click on the button to proceed into workspace
-      </h1>
-      {/* <p className="mt-2 text-base text-gray-500">
-        Sorry, we couldn’t find the page you’re looking for.
-      </p> */}
+      </p>
       <div className="mt-6">
         <Button
           buttonStyle="primary"
