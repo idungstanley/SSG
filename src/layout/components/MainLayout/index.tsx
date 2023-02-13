2acc8c8f050cb5b139da3a9a98ee017f7315c2c0
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-02-13 18:22:50 +0200

    added progress bar in layout

--- src/layout/components/MainLayout/index.tsx
+++ src/layout/components/MainLayout/index.tsx
@@ -1,35 +1,15 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { useIsFetching, useIsMutating } from '@tanstack/react-query';
-import LoadingBar, { LoadingBarRef } from 'react-top-loading-bar';
+
 import TopMenu from './TopMenu';
 import MyWorkspacesSlideOver from '../../../pages/account/components/MyWorkspacesSlideOver';
 import Chat from '../../../components/Chat';
+import ProgressBar from './ProgressBar';
 
 function MainLayout() {
-  const progressBarRef = useRef<LoadingBarRef>(null);
-
-  const isFetching = useIsFetching();
-  const isMutating = useIsMutating();
-
-  useEffect(() => {
-    if (isFetching !== 0 || isMutating !== 0) {
-      progressBarRef.current?.staticStart();
-      progressBarRef.current?.continuousStart(65, 500);
-    } else {
-      progressBarRef.current?.complete();
-    }
-  }, [isFetching, isMutating]);
-
   return (
     <div className="h-full flex flex-col">
-      <LoadingBar
-        color="#EF4444"
-        height={2.5}
-        waitingTime={100}
-        loaderSpeed={250}
-        ref={progressBarRef}
-      />
+      <ProgressBar />
       <TopMenu />
       <div className="flex h-full">
         <div className="flex-1 w-full h-full overflow-hidden">
