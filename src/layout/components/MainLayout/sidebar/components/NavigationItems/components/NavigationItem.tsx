2432c2d1fdc9b5ba017e24de68e488b6a5cd8794
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-02-14 16:09:58 +0200

    added decomposition for navigation items

---
+++ src/layout/components/MainLayout/sidebar/components/NavigationItems/components/NavigationItem.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useAppSelector } from '../../../../../../../app/hooks';
+import { classNames } from '../../../../../../../utils';
+
+interface NavigationItemProps {
+  item: {
+    name: string;
+    href: string;
+    alwaysShow: boolean;
+    source?: string;
+    icon?: JSX.Element;
+  };
+  isVisible: boolean;
+}
+
+export default function NavigationItem({
+  item,
+  isVisible,
+}: NavigationItemProps) {
+  const { pathname } = useLocation();
+  const { showSidebar } = useAppSelector((state) => state.account);
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <Link
+      to={item.href}
+      className={classNames(
+        pathname === item.href
+          ? 'bg-green-100 hover:bg-green-200'
+          : 'hover:bg-gray-100',
+        !showSidebar ? 'justify-center' : 'gap-2 items-center',
+        'relative flex cursor-pointer p-2 w-full hover:text-gray-500 '
+      )}
+    >
+      {item.href === pathname ? (
+        <span className="absolute rounded-r-lg top-0 bottom-0 left-0 w-1 bg-green-500 " />
+      ) : null}
+
+      {item.icon || (
+        <img className="w-5 h-5" src={item.source} alt={item.name} />
+      )}
+      {showSidebar ? <p className="text-xs truncate">{item.name}</p> : null}
+    </Link>
+  );
+}
---
+++ src/layout/components/MainLayout/sidebar/components/NavigationItems/index.tsx
@@ -0,0 +1,104 @@
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  BellIcon,
+  Squares2X2Icon,
+  CalendarIcon,
+  UserGroupIcon,
+} from '@heroicons/react/24/outline';
+import React, { useState } from 'react';
+import favoriteIcon from '../../../../../../assets/branding/Favourite-icon.svg';
+import groupIcon from '../../../../../../assets/branding/Group.png';
+import homeIcon from '../../../../../../assets/branding/Home-icon.svg';
+import { classNames } from '../../../../../../utils';
+import { useAppSelector } from '../../../../../../app/hooks';
+import NavigationItem from './components/NavigationItem';
+
+const navigation = [
+  {
+    name: 'Home',
+    href: '/',
+    source: homeIcon,
+    alwaysShow: true,
+  },
+  {
+    name: 'Notifications',
+    href: '/notification',
+    icon: <BellIcon className="w-5 h-5" aria-hidden="true" />,
+    alwaysShow: true,
+  },
+  {
+    name: 'Calendar',
+    href: '/calendar',
+    icon: <CalendarIcon className="w-5 h-5" aria-hidden="true" />,
+    alwaysShow: false,
+  },
+  {
+    name: 'Community',
+    href: '/community',
+    icon: <UserGroupIcon className="w-5 h-5" aria-hidden="true" />,
+    alwaysShow: false,
+  },
+  {
+    name: 'Goals',
+    href: '/goals',
+    source: groupIcon,
+    alwaysShow: false,
+  },
+  {
+    name: 'Dashboards',
+    href: '/dashboard',
+    icon: <Squares2X2Icon className="w-5 h-5" aria-hidden="true" />,
+    alwaysShow: false,
+  },
+  {
+    name: 'Favorites',
+    href: '/favorites',
+    source: favoriteIcon,
+    alwaysShow: false,
+  },
+];
+
+const showLessOrMore = [
+  {
+    name: 'Show Less',
+    icon: <ArrowUpIcon className="w-5 h-5" aria-hidden="true" />,
+  },
+  {
+    name: 'Show More',
+    icon: <ArrowDownIcon className="w-5 h-5" aria-hidden="true" />,
+  },
+];
+
+export default function NavigationItems() {
+  const [showMore, setShowMore] = useState(false);
+  const { showSidebar } = useAppSelector((state) => state.account);
+
+  return (
+    <nav className="flex flex-col mt-1">
+      {navigation.map((item) => (
+        <NavigationItem
+          key={item.href}
+          item={item}
+          isVisible={item.alwaysShow || showMore}
+        />
+      ))}
+
+      {/* show less or more button */}
+      <div
+        onClick={() => setShowMore((prev) => !prev)}
+        className={classNames(
+          !showSidebar ? 'justify-center' : 'gap-2 items-center',
+          'flex cursor-pointer gap-2 items-center p-2 w-full hover:text-gray-500 hover:bg-gray-100'
+        )}
+      >
+        {showLessOrMore[showMore ? 0 : 1].icon}
+        {showSidebar ? (
+          <p className="text-xs truncate">
+            {showLessOrMore[showMore ? 0 : 1].name}
+          </p>
+        ) : null}
+      </div>
+    </nav>
+  );
+}
