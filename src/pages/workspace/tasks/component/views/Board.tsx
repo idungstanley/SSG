68872d082d44ca3c826343b86d83f1940f1087fd
nathaniel <na.diamond@simpsgroup.co.uk>
2023-02-16 18:04:21 +0100

    add card body

--- src/pages/workspace/tasks/component/views/Board.tsx
+++ src/pages/workspace/tasks/component/views/Board.tsx
@@ -6,7 +6,7 @@ function Board() {
   console.log(myTaskData);
 
   return (
-    <div className=" m-auto fgoverflow h-screen ">
+    <div className=" m-auto fgoverflow ">
       <div className="flex  gap-5  ">
         {myTaskData.map((column) => (
           <div
@@ -14,13 +14,13 @@ function Board() {
             className=" rounded shadow-md  "
             style={{ width: "500px" }}
           >
-            <div className="bg-gray-400 w-56 h-1 rounded-t-sm "></div>
-            <h2 className="  text-xs uppercase bg-white h-10 w-56 pt-3 px-2 py-1 rounded font-bold">
+            <div className="bg-gray-400 w-64 h-1 rounded-t-sm "></div>
+            <div className="  text-xs uppercase bg-white h-10 w-64 pt-3 px-2 py-1 rounded font-bold">
               {column.status}
-            </h2>
-            <h2 className="  text-xs uppercase bg-white w-56 h-10 mt-3 px-2 py-1 rounded font-bold">
+            </div>
+            <div className="  text-xs uppercase bg-white h-48 w-64 h-10 mt-3 px-2 py-1 rounded font-bold">
               {column.name}
-            </h2>
+            </div>
           </div>
         ))}
       </div>
