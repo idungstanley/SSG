60a9cf79de7273c1749c66dd6f00da7324de25e2
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-03-02 11:56:08 +0100

    Add update to email

---
+++ src/pages/workspace/email/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FaRoute } from "react-icons/fa";
+import { useAppSelector } from "../../../app/hooks";
+import PlaceItem from "../../../layout/components/MainLayout/Sidebar/components/PlaceItem";
+import { cl } from "../../../utils";
+
+export default function RoutePlanner() {
+  const { showSidebar } = useAppSelector((state) => state.account);
+
+  return (
+    <div>
+      <PlaceItem label="EMAIL" icon={<FaRoute className="w-4 h-4" />} />
+      <div className={cl("mb-2", !showSidebar && "overflow-x-hidden w-12")}>
+        EMAIL{" "}
+      </div>
+    </div>
+  );
+}
