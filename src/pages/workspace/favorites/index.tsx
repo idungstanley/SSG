e74bb138e999fe85026b8b2dda60a03988dab18e
alhajideen <salifunurudeen834@gmail.com>
2023-03-07 07:48:37 +0100

    fix extended bar heading for favorites

--- src/pages/workspace/favorites/index.tsx
+++ src/pages/workspace/favorites/index.tsx
@@ -20,41 +20,18 @@ function Favorites() {
 
   return (
     <>
-      <div>
-        <li className="bg-gray-200 focus:flex flex w-full pl-5 py-5 items-center relative list-none gap-2">
-          <div className="flex justify-center w-full">
-            <div className="flex items-center">
-              <span className="flex justify-between items-center w-full h-6 mx-1">
-                <AiFillStar />
-              </span>
-              <span className="block font-semibold text-xs w-full cursor-pointer uppercase leading-3 tracking-wider text-black font-black mx-1">
-                FAVORITES
-              </span>
-            </div>
+      <div className="bg-gray-200 focus:flex flex w-full py-5 items-center relative list-none gap-2">
+        <div className="flex justify-center w-full">
+          <div className="flex items-center">
+            <span className="flex justify-between items-center w-full h-6 mx-1">
+              <AiFillStar />
+            </span>
+            <span className="block font-semibold text-xs w-full cursor-pointer uppercase leading-3 tracking-wider text-black font-black mx-1">
+              FAVORITES
+            </span>
           </div>
-        </li>
+        </div>
       </div>
-      {/* <div className="flex justify-between mx-2">
-        {typesArr.map((type) => {
-          return (
-            <div
-              key={type.id}
-              className="flex content-center justify-between  items-center"
-            >
-              <AvatarWithInitials
-                initials={""}
-                height="h-2"
-                width="w-2"
-                backgroundColour={type.color}
-                roundedStyle="rounded"
-              />
-              <h6 className="mx-1" style={{ fontSize: "8px" }}>
-                {type.type}
-              </h6>
-            </div>
-          );
-        })}
-      </div> */}
       <div className={cl("mb-2", !showSidebar && "overflow-x-hidden w-12")}>
         {data?.data.favorites.map(
           (fav: {
