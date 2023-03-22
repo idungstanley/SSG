f79798833611caf0179ec81525babde831a53008
diamond-nicholas <diamondnicholas154@gmail.com>
2023-03-22 10:10:51 +0000

    update infinite scroll for data

--- src/pages/workspace/hubs/components/renderHubs/RenderHubs.tsx
+++ src/pages/workspace/hubs/components/renderHubs/RenderHubs.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import React, { useMemo, useState, useEffect, useRef } from 'react';
 import { useAppSelector } from '../../../../../app/hooks';
 import ListNav from '../../../lists/components/renderlist/ListNav';
@@ -24,7 +21,7 @@ function RenderHubs() {
   const [TaskDataGroupings, setTaskDataGroupings] = useState<TaskDataGroupingsProps | unknown>({});
   const { activeItemName } = useAppSelector((state) => state.workspace);
   const { listView, tableView, boardView } = useAppSelector((state) => state.task);
-  const containerRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const retrievedObject = localStorage.getItem('hubDetailsStorage');
   const hubdetail: HubDetailTypes = JSON.parse(retrievedObject as string) as HubDetailTypes;
@@ -74,13 +71,13 @@ function RenderHubs() {
 
   useEffect(() => {
     const container = containerRef.current;
-    container.addEventListener('scroll', handleScroll);
-    return () => container.removeEventListener('scroll', handleScroll);
+    container?.addEventListener('scroll', handleScroll);
+    return () => container?.removeEventListener('scroll', handleScroll);
   }, [containerRef, fetchNextPage, hasNextPage]);
 
-  function handleScroll(event) {
-    const container = event.target;
-    const scrollDifference = container.scrollHeight - container.scrollTop - container.clientHeight;
+  function handleScroll(event: UIEvent | Event) {
+    const container = event.target as HTMLElement;
+    const scrollDifference = container?.scrollHeight - container.scrollTop - container.clientHeight;
     const range = 1;
 
     if (scrollDifference <= range && scrollDifference >= -range && hasNextPage) {
@@ -107,7 +104,11 @@ function RenderHubs() {
       >
         {listView && (
           <div className="pr-1 pt-0.5 w-full h-full">
-            <div className="w-full overflow-auto" style={{ minHeight: '0', maxHeight: '90vh' }} ref={containerRef}>
+            <div
+              className="w-full overflow-auto mb-10"
+              style={{ minHeight: '0', maxHeight: '90vh' }}
+              ref={containerRef}
+            >
               <div className="w-full">
                 <ListFilter />
               </div>
@@ -137,7 +138,7 @@ function RenderHubs() {
         )}
         {tableView && (
           <div className="pr-1 pt-0.5 w-full h-full">
-            <div className="w-full" style={{ minHeight: '0', maxHeight: '90vh' }}>
+            <div className="w-full overflow-auto" style={{ minHeight: '0', maxHeight: '90vh' }}>
               {tableView && (
                 <TaskTableTemplateData
                   filteredTaskData={
@@ -157,7 +158,7 @@ function RenderHubs() {
         )}
         {boardView && (
           <div className="pr-1 pt-0.5 w-full h-full">
-            <div className="w-full" style={{ minHeight: '0', maxHeight: '90vh' }}>
+            <div className="w-full overflow-auto" style={{ minHeight: '0', maxHeight: '90vh' }}>
               {boardView && <TaskBoardTemplate unFilteredTaskData={unFilteredTaskData2 as ITaskFullList[]} />}
             </div>
           </div>
