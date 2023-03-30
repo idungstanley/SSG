355f907b68cfbc8d6662338cc77b7ccf94f652a3
manlikenonso1 <cezeigbo4@gmail.com>
2023-03-30 20:06:07 +0100

    update on overlap

--- src/pages/workspace/tasks/assignTask/GroupAssignee.tsx
+++ src/pages/workspace/tasks/assignTask/GroupAssignee.tsx
@@ -7,13 +7,16 @@ function GroupAssignee({ data }: { data: [{ id: string; initials: string; colour
 
   return (
     <>
-      {data && data?.length >= 3 ? (
+      {data && data?.length >= 2 ? (
         <div className="flex items-center justify-center">
           {data
-            ?.slice(0, 2)
+            ?.slice(0, 3)
             .map((newData: { id: React.Key | null | undefined; initials: string; colour: string | undefined }) => (
               <div key={newData.id} className="">
-                <span key={newData.id} className="flex items-center gap-1 justify center">
+                <span
+                  key={newData.id}
+                  className="flex items-center justify-center -ml-2.5 border-white border-2 rounded-full"
+                >
                   <AvatarWithInitials
                     initials={newData.initials}
                     backgroundColour={newData.colour}
@@ -24,14 +27,14 @@ function GroupAssignee({ data }: { data: [{ id: string; initials: string; colour
               </div>
             ))}
           <span>
-            {(data as [{ id: string; initials: string; colour: string }])?.length - 2 !== 0 ? (
-              <span>+{(data as [{ id: string; initials: string; colour: string }])?.length - 2}</span>
+            {(data as [{ id: string; initials: string; colour: string }])?.length - 3 !== 0 ? (
+              <span>+{(data as [{ id: string; initials: string; colour: string }])?.length - 3}</span>
             ) : null}
           </span>
         </div>
       ) : (
         data?.map((newData) => (
-          <div key={newData.id} className="flex w-3">
+          <div key={newData.id} className="flex z-10">
             <span key={newData.id}>
               <AvatarWithInitials
                 initials={newData.initials}
