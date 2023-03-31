2dc38cd5cbbea0ccbd0096d3c802f65992001f9c
manlikenonso1 <cezeigbo4@gmail.com>
2023-03-31 09:36:35 +0100

    update logic for assignee count

--- src/pages/workspace/tasks/assignTask/GroupAssignee.tsx
+++ src/pages/workspace/tasks/assignTask/GroupAssignee.tsx
@@ -7,34 +7,42 @@ function GroupAssignee({ data }: { data: [{ id: string; initials: string; colour
 
   return (
     <>
-      {data && data?.length >= 2 ? (
+      {data && data?.length ? (
         <div className="flex items-center justify-center">
           {data
             ?.slice(0, 3)
-            .map((newData: { id: React.Key | null | undefined; initials: string; colour: string | undefined }) => (
-              <div key={newData.id} className="">
-                <span
+            .map(
+              (
+                newData: { id: React.Key | null | undefined; initials: string; colour: string | undefined },
+                index: number
+              ) => (
+                <div
                   key={newData.id}
-                  className="flex items-center justify-center -ml-3 border-white border-2 rounded-full"
+                  className={`scaleBigger ${index === 0 ? ' z-30  ' : ''} ${index === 1 ? 'z-20 ' : 'z-10'} `}
                 >
-                  <AvatarWithInitials
-                    initials={newData.initials}
-                    backgroundColour={newData.colour}
-                    height={`${CompactView || CompactViewWrap ? 'h-4' : 'h-8'}`}
-                    width={`${CompactView || CompactViewWrap ? 'w-4' : 'w-8'}`}
-                  />
-                </span>
-              </div>
-            ))}
+                  <span
+                    key={newData.id}
+                    className="flex items-center justify-center -ml-3.5 border-white border-2  rounded-full hover:bg-blue-500"
+                  >
+                    <AvatarWithInitials
+                      initials={newData.initials}
+                      backgroundColour={newData.colour}
+                      height={`${CompactView || CompactViewWrap ? 'h-4' : 'h-8'}`}
+                      width={`${CompactView || CompactViewWrap ? 'w-4' : 'w-8'}`}
+                    />
+                  </span>
+                </div>
+              )
+            )}
           <span>
-            {(data as [{ id: string; initials: string; colour: string }])?.length - 3 !== 0 ? (
-              <span>+{(data as [{ id: string; initials: string; colour: string }])?.length - 3}</span>
+            {(data as [{ id: string; initials: string; colour: string }])?.length - 2 !== 0 ? (
+              <span>+{(data as [{ id: string; initials: string; colour: string }])?.length - 2}</span>
             ) : null}
           </span>
         </div>
       ) : (
         data?.map((newData) => (
-          <div key={newData.id} className="flex z-10">
+          <div key={newData.id} className="flex">
             <span key={newData.id}>
               <AvatarWithInitials
                 initials={newData.initials}
