dcb7e22b6a5961ede013c668f04234f8ffdd6d45
Omachi John <omachi.john@yahoo.com>
2023-04-11 22:26:44 +0100

    improve assignee x icon

--- src/pages/workspace/tasks/assignTask/GroupAssignee.tsx
+++ src/pages/workspace/tasks/assignTask/GroupAssignee.tsx
@@ -4,7 +4,13 @@ import { AvatarWithInitials } from '../../../../components';
 
 function GroupAssignee({ data }: { data: [{ id: string; initials: string; colour: string }] | undefined }) {
   const { CompactView, CompactViewWrap } = useAppSelector((state) => state.task);
-  const [displayed, setDisplayed] = useState(false);
+  const [displayed, setDisplayed] = useState<{
+    show: boolean;
+    index: null | number;
+  }>({
+    show: false,
+    index: null
+  });
 
   return (
     <>
@@ -23,8 +29,22 @@ function GroupAssignee({ data }: { data: [{ id: string; initials: string; colour
                     index === 2 ? 'z-20' : 'z-10'
                   }  `}
                 >
-                  <span key={newData.id} className=" flex items-center  -ml-2.5  border-2  rounded-full  ">
-                    <div onMouseEnter={() => setDisplayed(true)} onMouseLeave={() => setDisplayed(false)}>
+                  <span key={newData.id} className=" flex items-center  -ml-2.5  border-2  rounded-full ">
+                    <div
+                      onMouseEnter={() =>
+                        setDisplayed({
+                          show: true,
+                          index
+                        })
+                      }
+                      onMouseLeave={() =>
+                        setDisplayed({
+                          show: false,
+                          index
+                        })
+                      }
+                      className="relative"
+                    >
                       <AvatarWithInitials
                         initials={newData.initials}
                         backgroundColour={newData.colour}
@@ -35,9 +55,9 @@ function GroupAssignee({ data }: { data: [{ id: string; initials: string; colour
                           CompactView || CompactViewWrap ? 'CompactWithInitialsW' : 'ComfortableWithInitialsW'
                         }`}
                       />
-                      {displayed && (
+                      {displayed.show && index == displayed?.index && (
                         <button
-                          className="absolute top-0 right-0 border h-3 w-3 rounded-full    bg-gray-500  text-white "
+                          className="absolute top-0 right-0 border h-3 w-3 rounded-full bg-gray-500  text-white "
                           style={{
                             fontSize: '6px'
                           }}
@@ -69,15 +89,31 @@ function GroupAssignee({ data }: { data: [{ id: string; initials: string; colour
               index === 2 ? 'z-20' : 'z-10'
             } `}
           >
-            <span key={newData.id} className="flex items-center justify-center -ml-2.5  border-2  rounded-full">
-              <div onMouseEnter={() => setDisplayed(true)} onMouseLeave={() => setDisplayed(false)}>
+            <span
+              key={newData.id}
+              className="flex items-center justify-center -ml-2.5  border-2  rounded-full relative"
+            >
+              <div
+                onMouseEnter={() =>
+                  setDisplayed({
+                    show: true,
+                    index
+                  })
+                }
+                onMouseLeave={() =>
+                  setDisplayed({
+                    show: false,
+                    index
+                  })
+                }
+              >
                 <AvatarWithInitials
                   initials={newData.initials}
                   backgroundColour={newData.colour}
                   height={`${CompactView || CompactViewWrap ? 'CompactWithInitialsH' : 'ComfortableWithInitialsH'}`}
                   width={`${CompactView || CompactViewWrap ? 'CompactWithInitialsW' : 'ComfortableWithInitialsW'}`}
                 />
-                {displayed && (
+                {displayed.show && index == displayed?.index && (
                   <button
                     className="absolute top-0 right-0  border h-3 w-3 rounded-full border   bg-gray-500  text-white "
                     style={{
