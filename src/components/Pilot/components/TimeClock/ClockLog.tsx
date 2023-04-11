2f41004122e80ce56d5246363cb79202dd4fae66
aceDavon <makojidavid@gmail.com>
2023-04-11 17:22:17 +0100

    Add toggle table implementation for the timelogs

--- src/components/Pilot/components/TimeClock/ClockLog.tsx
+++ src/components/Pilot/components/TimeClock/ClockLog.tsx
@@ -1,7 +1,16 @@
+import { FiPlusCircle } from 'react-icons/fi';
 import { useAppSelector } from '../../../../app/hooks';
 import { GetTimeEntriesService } from '../../../../features/task/taskService';
 import EntryList, { entriesProps } from '../../../../pages/workspace/tasks/timeclock/entryLists/EntryList';
 import NoEntriesFound from './NoEntries';
+
+export const headArr = [
+  { title: 'assignee', id: 1, hidden: false },
+  { title: 'duration', id: 2, hidden: false },
+  { title: 'start date', id: 3, hidden: false },
+  { title: 'end date', id: 4, hidden: false },
+  { title: 'description', id: 5, hidden: true }
+];
 
 export default function ClockLog() {
   const { activeItemId, activeItemType } = useAppSelector((state) => state.workspace);
@@ -17,11 +26,17 @@ export default function ClockLog() {
       } else {
         return (
           <>
-            <div className="flex items-center text-xs font-extralight border-b pb-2 border-gray-400">
-              <span className="w-20">Duration</span>
-              <span className="w-12">Start Date</span>
-              <span className="w-12">End Date</span>
-              <span className="w-12">Assignee</span>
+            <div className="flex items-center text-xs font-extralight border-b pb-2 border-gray-400 space-x-6">
+              {headArr.map((col) => {
+                return (
+                  !col.hidden && (
+                    <span key={col.id} className="w-12 cursor-default">
+                      {col.title}
+                    </span>
+                  )
+                );
+              })}
+              <FiPlusCircle className="AddColumnDropdownButton cursor-pointer font-black h-4 w-4" />
             </div>
             {getTaskEntries?.data?.time_entries?.map((entries: entriesProps) => (
               <EntryList entries={entries} key={entries.id} />
