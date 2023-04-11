f60047da338a1cdf77541987f7128d91a08833e7
Omachi John <omachi.john@yahoo.com>
2023-04-11 11:31:10 +0100

    perfect screen recording implementation

--- src/components/Pilot/components/RecordScreen/Recording.tsx
+++ src/components/Pilot/components/RecordScreen/Recording.tsx
@@ -1,27 +1,71 @@
-import React from 'react';
-import { IoStopCircleOutline } from 'react-icons/io5';
-import { HiOutlinePlayCircle } from 'react-icons/hi2';
-import { MdOutlinePauseCircle } from 'react-icons/md';
-import ToolTip from '../../../Tooltip';
+import React, { useState } from 'react';
 
 export default function Recording() {
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const [recorder, setRecorder] = useState<MediaRecorder | null>(null);
+  const [isRecording, setIsRecording] = useState(false);
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+
+  const startRecording = async () => {
+    const displayStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
+    setStream(displayStream);
+    const mediaRecorder = new MediaRecorder(displayStream);
+    setRecorder(mediaRecorder);
+    const chunks: BlobPart[] = [];
+    mediaRecorder.ondataavailable = (e: BlobEvent) => {
+      chunks.push(e.data);
+    };
+    mediaRecorder.onstop = () => {
+      const blob = new Blob(chunks, { type: 'video/mp4' });
+      const videoUrl = URL.createObjectURL(blob);
+      setVideoUrl(videoUrl);
+    };
+    mediaRecorder.start();
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    if (recorder?.state === 'recording') {
+      recorder.stop();
+      stream?.getTracks().forEach((track) => track.stop());
+      setIsRecording(false);
+    }
+  };
+
   return (
-    <div className="screenRecording">
-      <div id="startRecord">
-        <ToolTip tooltip="Start Screen Recording">
-          <HiOutlinePlayCircle className="h-6 w-6" />
-        </ToolTip>
-      </div>
-      <div id="pauseRecording">
-        <ToolTip tooltip="Pause Screen Recording">
-          <MdOutlinePauseCircle className="h-6 w-6" />
-        </ToolTip>
-      </div>
-      <div id="stopRecording">
-        <ToolTip tooltip="Stop Screen Recording">
-          <IoStopCircleOutline className="h-6 w-6" />
-        </ToolTip>
-      </div>
+    <div>
+      {isRecording ? (
+        <>
+          <video src={videoUrl!} controls />
+          <button onClick={stopRecording}>Stop Recording</button>
+        </>
+      ) : (
+        <button onClick={startRecording}>Start Recording</button>
+      )}
     </div>
   );
 }
+
+// export default Recording;
+
+// export default function Recording() {
+//   return (
+//     <div className="screenRecording">
+//       <div id="startRecord">
+//         <ToolTip tooltip="Start Screen Recording">
+//           <HiOutlinePlayCircle className="h-6 w-6" />
+//         </ToolTip>
+//       </div>
+//       <div id="pauseRecording">
+//         <ToolTip tooltip="Pause Screen Recording">
+//           <MdOutlinePauseCircle className="h-6 w-6" />
+//         </ToolTip>
+//       </div>
+//       <div id="stopRecording">
+//         <ToolTip tooltip="Stop Screen Recording">
+//           <IoStopCircleOutline className="h-6 w-6" />
+//         </ToolTip>
+//       </div>
+//     </div>
+//   );
+// }
