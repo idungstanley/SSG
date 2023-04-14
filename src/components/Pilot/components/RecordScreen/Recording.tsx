b78ce69461ff25785a3419c53656a13bc987744e
Omachi John <omachi.john@yahoo.com>
2023-04-14 17:14:35 +0100

    update borders on editable content on hover for task

--- src/components/Pilot/components/RecordScreen/Recording.tsx
+++ src/components/Pilot/components/RecordScreen/Recording.tsx
@@ -1,10 +1,61 @@
 import React, { useState } from 'react';
 
+// export default function Recording() {
+//   const [stream, setStream] = useState<MediaStream | null>(null);
+//   const [recorder, setRecorder] = useState<MediaRecorder | null>(null);
+//   const [isRecording, setIsRecording] = useState(false);
+//   const [videoUrl, setVideoUrl] = useState<string | undefined>(undefined);
+
+//   const startRecording = async () => {
+//     const displayStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
+//     setStream(displayStream);
+//     const mediaRecorder = new MediaRecorder(displayStream);
+//     setRecorder(mediaRecorder);
+//     const chunks: BlobPart[] = [];
+//     mediaRecorder.ondataavailable = (e: BlobEvent) => {
+//       chunks.push(e.data);
+//     };
+//     mediaRecorder.onstop = () => {
+//       const blob = new Blob(chunks, { type: 'video/mp4' });
+//       const videoUrl = URL.createObjectURL(blob);
+//       setVideoUrl(videoUrl);
+//     };
+//     mediaRecorder.start();
+//     setIsRecording(true);
+//   };
+
+//   const stopRecording = () => {
+//     if (recorder?.state === 'recording') {
+//       recorder.stop();
+//       stream?.getTracks().forEach((track) => track.stop());
+//       setIsRecording(false);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       {isRecording ? (
+//         <>
+//           <video src={videoUrl} controls />
+//           <button onClick={stopRecording} className="screenRecording"></button>
+//         </>
+//       ) : (
+//         <>
+//           <div className="screenRecording">
+//             {videoUrl && <video src={videoUrl} controls />}
+//             <button onClick={startRecording}>Start Recording</button>
+//           </div>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+
 export default function Recording() {
   const [stream, setStream] = useState<MediaStream | null>(null);
   const [recorder, setRecorder] = useState<MediaRecorder | null>(null);
   const [isRecording, setIsRecording] = useState(false);
-  const [videoUrl, setVideoUrl] = useState<string | undefined>(undefined);
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
 
   const startRecording = async () => {
     const displayStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
@@ -36,16 +87,11 @@ export default function Recording() {
     <div>
       {isRecording ? (
         <>
-          <video src={videoUrl} controls />
-          <button onClick={stopRecording} className="screenRecording"></button>
+          <video src={videoUrl!} controls />
+          <button onClick={stopRecording}>Stop Recording</button>
         </>
       ) : (
-        <>
-          <div className="screenRecording">
-            {videoUrl && <video src={videoUrl} controls />}
-            <button onClick={startRecording}>Start Recording</button>
-          </div>
-        </>
+        <button onClick={startRecording}>Start Recording</button>
       )}
     </div>
   );
