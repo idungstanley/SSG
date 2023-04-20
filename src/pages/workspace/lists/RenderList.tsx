2ce5ca861c9db93b06bf374b70ae564c89044472
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-04-20 16:05:50 +0100

    Add fix for listName not showing on the pilot

--- src/pages/workspace/lists/RenderList.tsx
+++ src/pages/workspace/lists/RenderList.tsx
@@ -20,6 +20,8 @@ import TaskCalenderTemplate from '../tasks/component/views/hubLevel/TaskCalender
 import NoTaskFound from '../tasks/component/taskData/NoTaskFound';
 import FilterByAssigneesSliderOver from './components/renderlist/filters/FilterByAssigneesSliderOver';
 import { ITaskFullList } from '../../../features/task/interface.tasks';
+import { UseGetListDetails } from '../../../features/list/listService';
+import { setActiveEntityName, setActiveItem } from '../../../features/workspace/workspaceSlice';
 
 function RenderList() {
   const dispatch = useDispatch();
@@ -50,6 +52,15 @@ function RenderList() {
     hasNextPage,
     fetchNextPage
   } = getTaskListService({ listId, assigneeUserId: filterTaskByAssigneeIds });
+  const listType = 'list';
+  const { data: listData } = UseGetListDetails({ activeItemId: listId, activeItemType: listType });
+  const listName = listData?.data.list.name;
+  useEffect(() => {
+    if (listId) {
+      dispatch(setActiveItem({ activeItemId: listId, activeItemType: listType, activeItemName: listName }));
+      dispatch(setActiveEntityName(listName));
+    }
+  }, [listId, listData]);
 
   const paginatedTaskData = useMemo(
     () => listDetailsData?.pages.flatMap((page) => page?.data.tasks),
