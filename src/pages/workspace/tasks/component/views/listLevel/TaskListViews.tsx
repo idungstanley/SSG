dafaea37231fe45e078a6b49de94072f7a68b3d3
aceDavon <makojidavid@gmail.com>
2023-04-25 15:51:38 +0100

    Extract The sortModal component
    Extract the sortModal component to a stand alone component and export
    for use accross the application

--- src/pages/workspace/tasks/component/views/listLevel/TaskListViews.tsx
+++ src/pages/workspace/tasks/component/views/listLevel/TaskListViews.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FiPlusCircle } from 'react-icons/fi';
 import { useAppDispatch, useAppSelector } from '../../../../../../app/hooks';
 import AddColumnDropdown from '../../../dropdown/AddColumnDropdown';
 import { getTaskColumns, setCloseTaskListView, setSortArray } from '../../../../../../features/task/taskSlice';
 import '../../views/view.css';
 import '../../taskData/task.css';
-import { IoIosArrowDropdown, IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
+import { IoIosArrowDropdown } from 'react-icons/io';
 import { columnsHead, listColumnProps } from '../ListColumns';
 import { MdDragIndicator } from 'react-icons/md';
 import { FaSort } from 'react-icons/fa';
 import { useList } from '../../../../../../features/list/listService';
 import CreateDropdownFieldModal from '../../../dropdown/CreateDropdownFieldModal';
-import { CiFilter } from 'react-icons/ci';
 import { GiCancel } from 'react-icons/gi';
+import SortModal from '../../../../../../components/SortModal/SortModal';
 // import { useQueryClient } from '@tanstack/react-query';
 
 const unique = (arr: listColumnProps[]) => [...new Set(arr)];
@@ -43,6 +43,7 @@ export default function TaskListViews({
 
   const { data } = useList(listId);
   const [sortArr, setSortArr] = useState<string[]>([]);
+  const [querySwitch, setQuerySwitch] = useState<boolean>(false);
 
   const handleSort = (header: string, id: string) => {
     setheaderId(id);
@@ -50,8 +51,13 @@ export default function TaskListViews({
     setSortArr((prev) => [...prev, header]);
     setSortAbleArr((prev) => [...prev, { dir: 'asc', field: header == 'Assignees' ? 'assignee' : header }]);
     setShowSortModal(!showSortModal);
-    dispatch(setSortArray(sortAbleArr));
+    setQuerySwitch(!querySwitch);
   };
+
+  if (querySwitch) {
+    dispatch(setSortArray(sortAbleArr));
+    setQuerySwitch(!querySwitch);
+  }
 
   const handleRemoveFilter = (title: string): void => {
     setSortArr((prev) => prev.filter((el) => el !== title));
@@ -324,83 +330,6 @@ export default function TaskListViews({
             ) : null}
           </span>
         </span>
-      </div>
-    </div>
-  );
-}
-
-type SortModalProps = {
-  headers: string[];
-  toggleModal: React.Dispatch<React.SetStateAction<boolean>>;
-  arr: {
-    sortAbleArr: Array<SortOption>;
-    setSortAbleArr: React.Dispatch<React.SetStateAction<SortOption[]>>;
-  };
-  handleSortFn: (header: string, id: string) => void;
-};
-
-function SortModal({ headers, toggleModal, arr }: SortModalProps) {
-  const modalRef = useRef<HTMLDivElement>(null);
-  // const [queryState] = useState<boolean>(false);
-  // const dispatch = useAppDispatch();
-  const { sortAbleArr, setSortAbleArr } = arr;
-  // const queryClient = useQueryClient();
-
-  const handleClick = (title: string) => {
-    setSortAbleArr(
-      sortAbleArr.map((sortOption) => {
-        if (sortOption.field === title) {
-          const newDir = sortOption.dir === 'asc' ? 'desc' : 'asc';
-          return { ...sortOption, dir: newDir };
-        }
-        return sortOption;
-      })
-    );
-  };
-
-  // queryState && dispatch(setSortArr(sortAbleArr));
-
-  // useEffect(() => {
-  //   setQueryState(!queryState);
-  //   queryClient.invalidateQueries(['task']);
-  //   return setQueryState(!queryState);
-  // }, [sortAbleArr]);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-        toggleModal(false);
-      }
-    };
-
-    document.addEventListener('click', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, [toggleModal]);
-
-  return (
-    <div className="fixed z-50 bg-white mt-20 shadow-lg" ref={modalRef}>
-      <div className="flex flex-col space-y-2 w-44 px-1">
-        <div className="flex flex-col space-y-1 border-b-2 justify-start px-2">
-          {headers.map((title: string, index: number) => (
-            <div
-              key={index}
-              className="flex justify-between items-center font-semibold text-xs capitalize py-2 group cursor-pointer"
-              style={{ color: '#78828d' }}
-            >
-              {title}
-              <div className="flex items-center space-x-1">
-                <CiFilter className="opacity-0 transition duration-200 group-hover:opacity-100 text-gray-100 bg-gray-400 rounded-full cursor-pointer text-sm h-3 w-3 " />
-                <div className=" flex flex-col justify-center items-center w-6 h-6">
-                  <IoMdArrowDropup className="text-gray-400" />
-                  <IoMdArrowDropdown className="text-gray-400" onClick={() => handleClick(title)} />
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
     </div>
   );
