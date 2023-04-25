18b110e9fb168bde12d56d0c296ce5c9433b24dc
diamond-nicholas <diamondnicholas154@gmail.com>
2023-04-25 17:09:27 +0100

    update notifications settings

--- src/pages/settings/NotificationSettings/components/Table.tsx
+++ src/pages/settings/NotificationSettings/components/Table.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from 'react';
 import { GetNotificationSettingsService } from '../../../../features/general/notification/notificationService';
-import TBodyData from './TBody';
 import THeadData from './THead';
 
 export default function NotificaitonTable() {
@@ -17,22 +16,25 @@ export default function NotificaitonTable() {
               {data[category]?.types &&
                 Object.keys(data[category].types).map((cat: string) => (
                   <Fragment key={cat}>
-                    {Object.keys(data[category].types[cat]).map((body: string) => {
-                      const typeObj = data[category].types[cat][body];
-                      console.log(typeObj); // Check the typeObj structure
-                      return (
-                        <TBodyData
-                          notificationBody={{
-                            name: typeObj?.name ?? 'Unknown Name',
-                            default_email: typeObj?.default_email ?? false,
-                            default_site: typeObj?.default_site ?? false,
-                            is_email: typeObj?.is_email ?? false,
-                            is_site: typeObj?.is_site ?? false
-                          }}
-                          key={key}
-                        />
-                      );
-                    })}
+                    {/* {console.log(data[category].types[cat]?.category)} */}
+                    <tr>
+                      <td scope="col" className="pl-10  py-3.5 text-left text-sm font-semibold text-gray-900">
+                        {data[category].types[cat]?.name}
+                      </td>
+                      {/* {Object.keys(data[category].types[cat]?.types).map((values) */}
+                      <td scope="col" className="pl-14 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        <input checked type="checkbox" />
+                      </td>
+                      <td scope="col" className="pl-14 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        <input type="checkbox" />
+                      </td>
+                      <td scope="col" className="pl-14 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        <input type="checkbox" />
+                      </td>
+                      <td scope="col" className="pl-14 px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        <input type="checkbox" />
+                      </td>
+                    </tr>
                   </Fragment>
                 ))}
             </tbody>
