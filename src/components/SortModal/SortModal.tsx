f0335c4fa7e64b79e7e51b93ec4833bc723dd091
aceDavon <makojidavid@gmail.com>
2023-04-26 22:15:33 +0100

    Refactor sort modal to fit recommended UI

--- src/components/SortModal/SortModal.tsx
+++ src/components/SortModal/SortModal.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { SortOption } from '../../pages/workspace/tasks/component/views/listLevel/TaskListViews';
 import { CiFilter } from 'react-icons/ci';
 import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
+import { AiFillCaretLeft, AiFillCaretRight, AiOutlineFileSearch } from 'react-icons/ai';
 
 type SortModalProps = {
   headers: string[];
@@ -16,6 +17,9 @@ type SortModalProps = {
 export default function SortModal({ headers, toggleModal, arr }: SortModalProps) {
   const modalRef = useRef<HTMLDivElement>(null);
   const { sortAbleArr, setSortAbleArr } = arr;
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [sortDropDown, setSortDropDown] = useState<boolean>(false);
+  const [filterDropDown, setFilterDropDown] = useState<boolean>(false);
 
   const handleClick = (title: string) => {
     setSortAbleArr(
@@ -27,6 +31,14 @@ export default function SortModal({ headers, toggleModal, arr }: SortModalProps)
         return sortOption;
       })
     );
+  };
+
+  const switchBtns = (field: string) => {
+    if (field === 'sortBtn') {
+      setSortDropDown((prev) => !prev);
+    } else if (field === 'filterBtn') {
+      setFilterDropDown((prev) => !prev);
+    }
   };
 
   useEffect(() => {
@@ -44,18 +56,23 @@ export default function SortModal({ headers, toggleModal, arr }: SortModalProps)
   }, [toggleModal]);
 
   return (
-    <div className="fixed z-50 bg-white mt-20 shadow-lg" ref={modalRef}>
-      <div className="flex flex-col space-y-2 w-44 px-1">
-        <div className="flex flex-col space-y-1 border-b-2 justify-start px-2">
+    <div className="fixed z-50 bg-white shadow-lg" ref={modalRef}>
+      <div className="flex flex-col bg-white w-80 absolute top-2 px-1 rounded-md shadow-2xl" style={{ height: '65vh' }}>
+        <span className="text-xs text-center mt-4">Sorted columns</span>
+        <div className="flex flex-col space-y-1 border-b-2 justify-start mx-4 capitalize pb-4">
           {headers.map((title: string, index: number) => (
             <div
               key={index}
-              className="flex justify-between items-center font-semibold text-xs capitalize py-2 group cursor-pointer"
+              className="flex justify-between items-center font-semibold text-xs capitalize py-2 pl-1 space-y-2 my-1 group cursor-pointer alt-task rounded-md"
               style={{ color: '#78828d' }}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
             >
               {title}
               <div className="flex items-center space-x-1">
-                <CiFilter className="opacity-0 transition duration-200 group-hover:opacity-100 text-gray-100 bg-gray-400 rounded-full cursor-pointer text-sm h-3 w-3 " />
+                {index === hoveredIndex && (
+                  <CiFilter className="opacity-0 transition duration-200 group-hover:opacity-100 text-gray-100 bg-gray-400 rounded-full cursor-pointer text-sm h-3 w-3 " />
+                )}
                 <div className=" flex flex-col justify-center items-center w-6 h-6">
                   <IoMdArrowDropup className="text-gray-400" />
                   <IoMdArrowDropdown className="text-gray-400" onClick={() => handleClick(title)} />
@@ -63,6 +80,46 @@ export default function SortModal({ headers, toggleModal, arr }: SortModalProps)
               </div>
             </div>
           ))}
+          {/* sort implementation */}
+          <div className="flex justify-between pr-1">
+            <span>sort by color</span>
+            {!sortDropDown ? (
+              <AiFillCaretRight className="text-gray-400 font-bold h-3 w-3" onClick={() => switchBtns('sortBtn')} />
+            ) : (
+              <AiFillCaretLeft className="text-gray-400 font-bold h-3 w-3" onClick={() => switchBtns('sortBtn')} />
+            )}
+          </div>
+        </div>
+        {/* filter implementation */}
+        <div className="flex flex-col space-y-6 mx-4 capitalize">
+          <div className="relative">
+            <input type="text" className="w-full border outline-none rounded-md" />
+            <AiOutlineFileSearch className="absolute right-2 top-2 w-6 h-6" />
+          </div>
+          <div className="flex justify-between pr-1">
+            <span>filter by color</span>
+            {!filterDropDown ? (
+              <AiFillCaretRight className="text-gray-400 font-bold h-3 w-3" onClick={() => switchBtns('filterBtn')} />
+            ) : (
+              <AiFillCaretLeft className="text-gray-400 font-bold h-3 w-3" onClick={() => switchBtns('filterBtn')} />
+            )}
+          </div>
+          <div className="flex justify-between pr-1">
+            <span>filter by condition</span>
+            {!filterDropDown ? (
+              <AiFillCaretRight className="text-gray-400 font-bold h-3 w-3" onClick={() => switchBtns('filterBtn')} />
+            ) : (
+              <AiFillCaretLeft className="text-gray-400 font-bold h-3 w-3" onClick={() => switchBtns('filterBtn')} />
+            )}
+          </div>
+          <div className="flex justify-between pr-1">
+            <span>filter by values</span>
+            {!filterDropDown ? (
+              <AiFillCaretRight className="text-gray-400 font-bold h-3 w-3" onClick={() => switchBtns('filterBtn')} />
+            ) : (
+              <AiFillCaretLeft className="text-gray-400 font-bold h-3 w-3" onClick={() => switchBtns('filterBtn')} />
+            )}
+          </div>
         </div>
       </div>
     </div>
