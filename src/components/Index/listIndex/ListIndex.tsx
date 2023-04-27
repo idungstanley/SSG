4d04d6a6586325a2fb77cb42c0405013d466ab27
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-04-27 17:06:22 +0100

    Fix error

--- src/components/Index/listIndex/ListIndex.tsx
+++ src/components/Index/listIndex/ListIndex.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
-// import { useGetHubWallet } from '../../../features/hubs/hubService';
-// import MenuDropdown from '../../Dropdown/MenuDropdown';
-// import { useAppSelector } from '../../../app/hooks';
-// import ListItem from '../../tasks/ListItem';
-import { getListService } from '../../../features/list/listService';
+import { useGetHubWallet } from '../../../features/hubs/hubService';
+import MenuDropdown from '../../Dropdown/MenuDropdown';
+import { useAppSelector } from '../../../app/hooks';
+import ListItem from '../../tasks/ListItem';
 
 interface ListIndexProps {
   showHubList: boolean;
-  getCurrentHubId: string | null | undefined;
+  getCurrentHubId: string | null;
   paddingLeft?: string;
 }
 
 function ListIndex({ showHubList, getCurrentHubId, paddingLeft = '26' }: ListIndexProps) {
-  const { data } = getListService({ getCurrentHubId });
-  console.log(getCurrentHubId);
-  console.log(data);
-  console.log(paddingLeft);
-  // const { showMenuDropdown } = useAppSelector((state) => state.hub);
+  const { data } = useGetHubWallet(getCurrentHubId);
+  const { showMenuDropdown } = useAppSelector((state) => state.hub);
 
   return (
     <div id="createWallet" className={`${showHubList ? 'block' : 'hidden'}`}>
-      {/* {data?.data?.lists &&
+      {data?.data?.lists &&
         data?.data?.lists.map((list) => (
           <div key={list.id}>
             <ListItem paddingLeft={paddingLeft} list={list} />
             {showMenuDropdown === list.id ? <MenuDropdown /> : null}
           </div>
-        ))} */}
+        ))}
     </div>
   );
 }
