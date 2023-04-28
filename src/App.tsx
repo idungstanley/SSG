e3aa617a47c32ec534e4937ef8be06ad8532c8e2
alhajideen <salifunurudeen834@gmail.com>
2023-04-28 11:31:23 +0100

    fix the routes changing on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,31 @@ import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import routes from './routes';
 import Prompt from './common/Prompt';
-// import { selectCurrentUser } from './features/auth/authSlice';
+import { selectCurrentUser } from './features/auth/authSlice';
 // import './styles/index.css';
 // import { setOnlineStatus } from './features/community/communityService';
-import { useGetSelf } from './features/settings/user/userSettingsServices';
-import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from './app/hooks';
-import { setUserData, setUserInfo } from './features/settings/user/userSettingsSlice';
+// import { useGetSelf } from './features/settings/user/userSettingsServices';
+// import { useEffect } from 'react';
+import { useAppSelector } from './app/hooks';
+// import { setUserData, setUserInfo } from './features/settings/user/userSettingsSlice';
 
 function App() {
-  // const user = useSelector(selectCurrentUser);
+  const user = useAppSelector(selectCurrentUser);
   // setOnlineStatus();
-  const dispatch = useAppDispatch();
-  const { userData } = useAppSelector((state) => state.userSetting);
 
-  const { data, status } = useGetSelf();
+  // const { data, status } = useGetSelf();
 
-  useEffect(() => {
-    dispatch(setUserData(data?.data.user));
-    dispatch(setUserInfo({ ...data?.data.user }));
-  }, [data]);
+  // useEffect(() => {
+  //   dispatch(setUserData(data?.data.user));
+  //   dispatch(setUserInfo({ ...data?.data.user }));
+  // }, [data]);
 
-  return status == 'success' ? (
+  return (
     <>
-      <RouterProvider router={routes(userData)} />
+      <RouterProvider router={routes(user)} />
       <Toaster position="bottom-left" />
       <Prompt />
     </>
-  ) : null;
+  );
 }
 export default App;
