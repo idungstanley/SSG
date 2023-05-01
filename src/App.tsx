8cd374f7ce1ece998f1654905c4162cb107c24cc
alhajideen <salifunurudeen834@gmail.com>
2023-05-01 14:40:07 +0100

    add a service to get logged in user data on load

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,25 @@ import Prompt from './common/Prompt';
 import { selectCurrentUser } from './features/auth/authSlice';
 // import './styles/index.css';
 // import { setOnlineStatus } from './features/community/communityService';
-// import { useGetSelf } from './features/settings/user/userSettingsServices';
-// import { useEffect } from 'react';
-import { useAppSelector } from './app/hooks';
-// import { setUserData, setUserInfo } from './features/settings/user/userSettingsSlice';
+import { useGetSelf } from './features/settings/user/userSettingsServices';
+import { useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from './app/hooks';
+import { setUserData, setUserInfo } from './features/settings/user/userSettingsSlice';
 
 function App() {
   const user = useAppSelector(selectCurrentUser);
+  // const { accessToken } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
   // setOnlineStatus();
 
-  // const { data, status } = useGetSelf();
+  const { data, status } = useGetSelf();
 
-  // useEffect(() => {
-  //   dispatch(setUserData(data?.data.user));
-  //   dispatch(setUserInfo({ ...data?.data.user }));
-  // }, [data]);
+  useEffect(() => {
+    if (status === 'success') {
+      dispatch(setUserData(data?.data.user));
+      dispatch(setUserInfo({ ...data?.data.user }));
+    }
+  }, [data]);
 
   return (
     <>
