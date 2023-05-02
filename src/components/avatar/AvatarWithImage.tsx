81ac61cecc529d29b80d23ea0295869842219bda
manlikenonso1 <cezeigbo4@gmail.com>
2023-05-02 14:03:05 +0100

    added avatars to assignee

--- src/components/avatar/AvatarWithImage.tsx
+++ src/components/avatar/AvatarWithImage.tsx
@@ -15,24 +15,16 @@ function AvatarWithImage({
 }: AvatarWithInitialsProps) {
   return (
     <div className="relative">
-      <img
-        className={`inline-flex items-center justify-center  ${height} ${width} ${
-          roundedStyle === 'circular' && 'rounded-full'
-        } ${roundedStyle === 'rounded' && 'rounded'}`}
-        src={image_path}
-      />
+      <span className={`${height} ${width} ${roundedStyle} flex items-center justify-center `}>
+        <img
+          className={` ${height} ${width} ${roundedStyle === 'circular' && 'rounded-full'} ${
+            roundedStyle === 'rounded' && 'rounded'
+          }`}
+          src={image_path}
+        />
+      </span>
     </div>
   );
 }
-
-// <div className="relative ">
-//   <span
-//     className={`inline-flex  items-center justify-center ${height} ${width} ${
-//       roundedStyle === 'circular' && 'rounded-full'
-//     } ${roundedStyle === 'rounded' && 'rounded'}`}
-//   >
-//     <img className="w-full h-full rounded-full cursor-pointer" src={image_path} alt="avatar" />
-//   </span>
-// </div>
 
 export default AvatarWithImage;
