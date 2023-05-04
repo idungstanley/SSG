c4b03428ecd2add6b1024606d910587a27fd1810
alhajideen <salifunurudeen834@gmail.com>
2023-05-04 19:56:58 +0100

    add the icon and buttons

--- src/pages/settings/WorkspaceSettings/index.tsx
+++ src/pages/settings/WorkspaceSettings/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SimpleSectionHeading } from '../../../components';
+import { AvatarWithInitials } from '../../../components';
 import notificationFrame from '../../../assets/branding/notificationFrame.png';
 
 function WorkspaceSettings() {
@@ -8,9 +8,24 @@ function WorkspaceSettings() {
       <div
         className="mt-5 flex items-center rounded-t-md pl-5"
         style={{ backgroundImage: `url(${notificationFrame})`, height: '122px' }}
-      >
-        <SimpleSectionHeading title="" description="" />
-      </div>
+      ></div>
+      <section className="-mt-12 w-11/12 m-auto flex justify-between items-end">
+        <div className="flex items-end">
+          <AvatarWithInitials initials={'NS'} backgroundColour={'red'} height="h-24" width="w-24" />
+          <h3 className="font-medium" style={{ fontSize: '15px' }}>
+            ELASTIC WORKSPACE
+          </h3>
+        </div>
+        <div>
+          <button className="p-1 rounded text-sm border border-gray-500 mx-2 w-16 h-8">Cancel</button>
+          <button
+            className="p-1 rounded text-sm border border-gray-500 mx-2 text-white w-16 h-8"
+            style={{ backgroundColor: '#BF00FF' }}
+          >
+            Save
+          </button>
+        </div>
+      </section>
     </main>
   );
 }
