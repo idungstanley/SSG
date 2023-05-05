08ebe62ece0cd6fdb087ced2df7a31829f7cea59
alhajideen <salifunurudeen834@gmail.com>
2023-05-05 16:48:42 +0100

    make the workspaces to align properly on the list

--- src/pages/settings/WorkspaceSettings/index.tsx
+++ src/pages/settings/WorkspaceSettings/index.tsx
@@ -26,77 +26,44 @@ function WorkspaceSettings() {
           </button>
         </div>
       </section>
-      <table className="table-auto border-collapse  border border-slate-400 mt-10 m-auto" style={{ width: '1250px' }}>
+      <table className="table-auto border-collapse border border-slate-400 mt-10 m-auto" style={{ width: '1250px' }}>
         <thead className="bg-gray-200" style={{ height: '60px' }}>
-          <tr className="border border-slate-300 ">
-            <th className="font-medium text-black   " style={{ fontSize: '15px' }}>
+          <tr className="border border-slate-300">
+            <th className="font-medium text-black text-center py-3" style={{ fontSize: '15px' }}>
               WORKSPACE
             </th>
-            <th className="font-medium text-black" style={{ fontSize: '15px' }}>
+            <th className="font-medium text-black text-center py-3" style={{ fontSize: '15px' }}>
               AVATAR
             </th>
-            <th className="font-medium text-black" style={{ fontSize: '15px' }}>
+            <th className="font-medium text-black text-center py-3" style={{ fontSize: '15px' }}>
               LAST TIME VISITED
             </th>
-            <th className="font-medium text-black " style={{ fontSize: '15px' }}>
+            <th className="font-medium text-black text-center py-3" style={{ fontSize: '15px' }}>
               DATE CREATED
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr className="border border-slate-300 hover:bg-fuchsia-200 " style={{ height: '60px' }}>
-            <td>ELASTIC GROUP</td>
-            <td>
+          <tr className="border border-slate-300 hover:bg-fuchsia-200 cursor-pointer">
+            <td className="text-center py-3">ELASTIC GROUP</td>
+            <td className="text-center py-3">
               <AvatarWithInitials initials={'NS'} backgroundColour={'red'} height="h-10" width="w-10" />
             </td>
-            <td> 13 HOURS AGO</td>
-            <td>10/12/2023</td>
+            <td className="text-center py-3">13 HOURS AGO</td>
+            <td className="text-center py-3">10/12/2023</td>
           </tr>
-          <tr className="hover:bg-fuchsia-200" style={{ height: '60px' }}>
-            <td>ALSO WORKSPACE</td>
-            <td>
+          <tr className="hover:bg-fuchsia-200 cursor-pointer" style={{ height: '60px' }}>
+            <td className="text-center py-3">ALSO WORKSPACE</td>
+            <td className="text-center py-3">
               <AvatarWithInitials initials={'AW'} backgroundColour={'blue'} height="h-10" width="w-10" />
             </td>
-            <td> 2 DAYS AGO</td>
-            <td>10/12/2023</td>
+            <td className="text-center py-3">2 DAYS AGO</td>
+            <td className="text-center py-3">10/12/2023</td>
           </tr>
         </tbody>
-        {/* </div> */}
-        {/* <div
-          className="flex items-end m-auto justify-between  border-2 items-center hover:bg-fuchsia-200"
-          style={{ height: '60px', width: '1250px' }}
-        > */}
-        {/* <div className="font-medium text-black p-6" style={{ fontSize: '15px' }}>
-            ELASTIC GROUP
-          </div>
-          <div className="font-medium text-black mr-8" style={{ fontSize: '15px' }}>
-            <AvatarWithInitials initials={'NS'} backgroundColour={'red'} height="h-10" width="w-10" />
-          </div>
-          <div className="font-medium text-black mr-14" style={{ fontSize: '15px' }}>
-            13 HOURS AGO
-          </div>
-          <div className="font-medium text-black p-6  mr-4" style={{ fontSize: '15px' }}>
-            10/12/2023
-          </div>
-        </div>
-        <div
-          className="flex items-end m-auto justify-between  border-2 items-center hover:bg-fuchsia-200"
-          style={{ height: '60px', width: '1250px' }}
-        >
-          <div className="font-medium text-black p-6" style={{ fontSize: '15px' }}>
-            ALSO WORKSPACE
-          </div>
-          <div className="font-medium text-black -ml-14" style={{ fontSize: '15px' }}>
-            <AvatarWithInitials initials={'AW'} backgroundColour={'blue'} height="h-10" width="w-10" />
-          </div>
-          <div className="font-medium text-black mr-10 " style={{ fontSize: '15px' }}>
-            2 DAYS AGO
-          </div>
-          <div className="font-medium text-black p-6  mr-4" style={{ fontSize: '15px' }}>
-            10/12/2023
-          </div>
-        </div> */}
       </table>
+
+      <br />
     </main>
   );
 }
