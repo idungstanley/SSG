74fd420b560d08a0e41b97149f97100518697369
alhajideen <salifunurudeen834@gmail.com>
2023-05-05 16:57:54 +0100

    remove unused styles

--- src/pages/settings/WorkspaceSettings/index.tsx
+++ src/pages/settings/WorkspaceSettings/index.tsx
@@ -26,9 +26,9 @@ function WorkspaceSettings() {
           </button>
         </div>
       </section>
-      <table className="table-auto border-collapse border border-slate-400 mt-10 m-auto" style={{ width: '1250px' }}>
-        <thead className="bg-gray-200" style={{ height: '60px' }}>
-          <tr className="border border-slate-300">
+      <table className="table-auto border-collapse border border-slate-400 mt-10 w-10/12 m-auto rounded">
+        <thead className="bg-gray-200 py-3 h-16">
+          <tr className="border border-slate-300 py-3">
             <th className="font-medium text-black text-center py-3" style={{ fontSize: '15px' }}>
               WORKSPACE
             </th>
@@ -52,7 +52,7 @@ function WorkspaceSettings() {
             <td className="text-center py-3">13 HOURS AGO</td>
             <td className="text-center py-3">10/12/2023</td>
           </tr>
-          <tr className="hover:bg-fuchsia-200 cursor-pointer" style={{ height: '60px' }}>
+          <tr className="hover:bg-fuchsia-200 cursor-pointer">
             <td className="text-center py-3">ALSO WORKSPACE</td>
             <td className="text-center py-3">
               <AvatarWithInitials initials={'AW'} backgroundColour={'blue'} height="h-10" width="w-10" />
