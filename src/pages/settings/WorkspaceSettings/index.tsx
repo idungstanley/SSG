6178d1e737289f552264d187e24bd00617732b97
manlikenonso1 <cezeigbo4@gmail.com>
2023-05-05 02:02:51 +0100

    workspace settings

--- src/pages/settings/WorkspaceSettings/index.tsx
+++ src/pages/settings/WorkspaceSettings/index.tsx
@@ -26,6 +26,59 @@ function WorkspaceSettings() {
           </button>
         </div>
       </section>
+      <div className="  m-auto shadow-xl" style={{ width: '1250px' }}>
+        <div
+          className="flex items-end m-auto justify-between mt-10 border-2 items-center bg-gray-200"
+          style={{ height: '60px', width: '1250px' }}
+        >
+          <div className="font-medium text-black p-6" style={{ fontSize: '15px' }}>
+            WORKSPACE
+          </div>
+          <div className="font-medium text-black" style={{ fontSize: '15px' }}>
+            AVATAR
+          </div>
+          <div className="font-medium text-black" style={{ fontSize: '15px' }}>
+            LAST TIME VISITED
+          </div>
+          <div className="font-medium text-black p-6" style={{ fontSize: '15px' }}>
+            DATE CREATED
+          </div>
+        </div>
+        <div
+          className="flex items-end m-auto justify-between  border-2 items-center hover:bg-fuchsia-200"
+          style={{ height: '60px', width: '1250px' }}
+        >
+          <div className="font-medium text-black p-6" style={{ fontSize: '15px' }}>
+            ELASTIC GROUP
+          </div>
+          <div className="font-medium text-black mr-8" style={{ fontSize: '15px' }}>
+            <AvatarWithInitials initials={'NS'} backgroundColour={'red'} height="h-10" width="w-10" />
+          </div>
+          <div className="font-medium text-black mr-14" style={{ fontSize: '15px' }}>
+            13 HOURS AGO
+          </div>
+          <div className="font-medium text-black p-6  mr-4" style={{ fontSize: '15px' }}>
+            10/12/2023
+          </div>
+        </div>
+        <div
+          className="flex items-end m-auto justify-between  border-2 items-center hover:bg-fuchsia-200"
+          style={{ height: '60px', width: '1250px' }}
+        >
+          <div className="font-medium text-black p-6" style={{ fontSize: '15px' }}>
+            ALSO WORKSPACE
+          </div>
+          <div className="font-medium text-black -ml-14" style={{ fontSize: '15px' }}>
+            <AvatarWithInitials initials={'AW'} backgroundColour={'blue'} height="h-10" width="w-10" />
+          </div>
+          <div className="font-medium text-black mr-10 " style={{ fontSize: '15px' }}>
+            2 DAYS AGO
+          </div>
+          <div className="font-medium text-black p-6  mr-4" style={{ fontSize: '15px' }}>
+            10/12/2023
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
