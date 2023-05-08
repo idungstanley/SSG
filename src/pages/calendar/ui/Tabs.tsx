e3727102160387ca32a24f1a2f1c18a08b4f7aa0
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-05-08 14:38:54 +0300

    added header as in design

--- src/pages/calendar/ui/Tabs.tsx
+++ src/pages/calendar/ui/Tabs.tsx
@@ -1,16 +1,25 @@
+import { AdjustmentsHorizontalIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import CalendarIcon from '../../../assets/icons/CalendarIcon';
+import WallchartIcon from '../../../assets/icons/WallchartIcon';
 import { useGetTeamMembers } from '../../../features/settings/teamMembers/teamMemberService';
 import { cl } from '../../../utils';
 import { useDaysOff } from '../lib/daysOffContext';
 import { checkIsOwner } from '../lib/userUtils';
 
 const tabs = [
-  { name: 'Wallchart', href: '/calendar' },
-  { name: 'NewWallchart', href: '/calendar/new' },
-  { name: 'Calendar', href: '/calendar/year' },
-  { name: 'Leave types', href: '/calendar/types' }
+  { name: 'Wallchart', href: '/calendar', icon: <WallchartIcon className="w-5 h-5 cursor-pointer" /> },
+  { name: 'NewWallchart', href: '/calendar/new', icon: <CalendarIcon className="w-5 h-5 cursor-pointer" /> },
+  { name: 'Multi calendar', href: '/calendar/year', icon: <CalendarIcon className="w-5 h-5 cursor-pointer" /> },
+  {
+    name: 'Leave types',
+    href: '/calendar/types',
+    icon: <AdjustmentsHorizontalIcon className="w-5 h-5 cursor-pointer" />
+  }
 ];
+
+const showInputOnTabs = [tabs[0].href, tabs[1].href, tabs[2].href];
 
 export function Tabs() {
   const { pathname } = useLocation();
@@ -22,45 +31,69 @@ export function Tabs() {
 
   const count = daysOff.filter((i) => !i.isApproved).length;
 
-  const privateTabs = isOwner ? [{ name: 'Manage', href: '/calendar/manage', count }] : [];
+  const privateTabs = isOwner
+    ? [
+        {
+          name: 'Manage',
+          href: '/calendar/manage',
+          count,
+          icon: <AdjustmentsHorizontalIcon className="w-5 h-5 cursor-pointer" />
+        }
+      ]
+    : [];
 
   return (
-    <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-      {tabs.map((tab) => (
-        <Link
-          key={tab.name}
-          to={tab.href}
-          className={cl(
-            tab.href === pathname
-              ? 'border-primary-500 text-primary-600'
-              : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-            'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium'
-          )}
-          aria-current={tab.href === pathname ? 'page' : undefined}
-        >
-          {tab.name}
-        </Link>
-      ))}
+    <div className="-mb-px p-2 flex items-center justify-between w-full">
+      {showInputOnTabs.includes(pathname) ? <Input /> : null}
 
-      {privateTabs.map((tab) => (
-        <Link
-          key={tab.name}
-          to={tab.href}
-          className={cl(
-            tab.href === pathname
-              ? 'border-primary-500 text-primary-600'
-              : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-            'relative whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium'
-          )}
-          aria-current={tab.href === pathname ? 'page' : undefined}
-        >
-          {tab.name}
+      <nav className="flex space-x-4" aria-label="Tabs">
+        {tabs.map((tab) => (
+          <Link
+            key={tab.name}
+            to={tab.href}
+            className={cl(
+              'flex gap-3 rounded-md text-gray-900 items-center border whitespace-nowrap px-3 py-2 shadow-sm text-sm font-medium',
+              tab.href === pathname ? 'border-primary-200 bg-primary-200' : 'hover:bg-gray-300'
+            )}
+            aria-current={tab.href === pathname ? 'page' : undefined}
+          >
+            {tab.icon}
+            {tab.name}
+          </Link>
+        ))}
 
-          <span className="absolute top-1 -right-2 w-5 text-xs h-5 flex items-center justify-center rounded-full bg-primary-500 text-white">
-            {tab.count}
-          </span>
-        </Link>
-      ))}
-    </nav>
+        {privateTabs.map((tab) => (
+          <Link
+            key={tab.name}
+            to={tab.href}
+            className={cl(
+              'flex gap-3 rounded-md items-center text-gray-900 border whitespace-nowrap px-3 py-2 shadow-sm text-sm font-medium relative',
+              tab.href === pathname ? 'border-primary-500 bg-primary-600' : 'hover:bg-gray-300'
+            )}
+            aria-current={tab.href === pathname ? 'page' : undefined}
+          >
+            {tab.icon}
+            {tab.name}
+
+            <span className="absolute top-1 -right-2 w-5 text-xs h-5 flex items-center justify-center rounded-full bg-primary-500 text-white">
+              {tab.count}
+            </span>
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
+}
+
+function Input() {
+  return (
+    <div className="relative">
+      <MagnifyingGlassIcon className="absolute w-5 h-5 top-2 left-2 text-gray-400" />
+      <input
+        type="text"
+        placeholder="Search users..."
+        className="block font-medium border-gray-100 text-sm placeholder:text-gray-400 text-gray-900 border rounded-md shadow-sm pl-10 p-2"
+      />
+    </div>
   );
 }
