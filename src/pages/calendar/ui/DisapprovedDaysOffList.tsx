36a5effc54905c0e706e86b111223ba8fd4e8eaf
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-05-08 15:09:38 +0300

    added message is no requests, improved disapproved count in header

--- src/pages/calendar/ui/DisapprovedDaysOffList.tsx
+++ src/pages/calendar/ui/DisapprovedDaysOffList.tsx
@@ -1,5 +1,6 @@
 import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/24/outline';
 import dayjs from 'dayjs';
+import FullScreenMessage from '../../../components/CenterMessage/FullScreenMessage';
 import {
   useApproveDayOff,
   useDisapproveDayOff,
@@ -14,34 +15,38 @@ export default function DisapprovedDaysOffList() {
   const { mutate: onApprove } = useApproveDayOff(MOCKED_HUB_ID);
   const { mutate: onDisapprove } = useDisapproveDayOff(MOCKED_HUB_ID);
 
-  return (
-    <ul className="grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
-      {disapprovedDaysOff?.map((dayOff) => (
-        <DisapprovedDayOffCard
-          userName={dayOff.team_member.user.name}
-          roleName={dayOff.team_member.role.name}
-          userEmail={dayOff.team_member.user.email}
-          userInitials={dayOff.team_member.user.initials}
-          key={dayOff.id}
-          reason={dayOff.reason}
-          startDate={dayjs(dayOff.start_date).format('MMMM D, YYYY')}
-          endDate={dayjs(dayOff.end_date).format('MMMM D, YYYY')}
-          actions={
-            <div className="grid grid-cols-2 py-1">
-              <div className="flex w-full h-full items-center justify-center">
-                <button onClick={() => onApprove(dayOff.id)} className="text-red-900 p-2">
-                  <XCircleIcon className="h-5 w-5" aria-hidden="true" />
-                </button>
+  return disapprovedDaysOff ? (
+    !disapprovedDaysOff.length ? (
+      <FullScreenMessage title="There are no requests." description="Add one." showHalFScreen />
+    ) : (
+      <ul className="grid grid-cols-1 gap-6 lg:grid-cols-2 xl:grid-cols-3">
+        {disapprovedDaysOff?.map((dayOff) => (
+          <DisapprovedDayOffCard
+            userName={dayOff.team_member.user.name}
+            roleName={dayOff.team_member.role.name}
+            userEmail={dayOff.team_member.user.email}
+            userInitials={dayOff.team_member.user.initials}
+            key={dayOff.id}
+            reason={dayOff.reason}
+            startDate={dayjs(dayOff.start_date).format('MMMM D, YYYY')}
+            endDate={dayjs(dayOff.end_date).format('MMMM D, YYYY')}
+            actions={
+              <div className="grid grid-cols-2 py-1">
+                <div className="flex w-full h-full items-center justify-center">
+                  <button onClick={() => onApprove(dayOff.id)} className="text-red-900 p-2">
+                    <XCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  </button>
+                </div>
+                <div className="flex w-full h-full items-center justify-center">
+                  <button onClick={() => onDisapprove(dayOff.id)} className="text-green-900 p-2">
+                    <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                  </button>
+                </div>
               </div>
-              <div className="flex w-full h-full items-center justify-center">
-                <button onClick={() => onDisapprove(dayOff.id)} className="text-green-900 p-2">
-                  <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
-                </button>
-              </div>
-            </div>
-          }
-        />
-      ))}
-    </ul>
-  );
+            }
+          />
+        ))}
+      </ul>
+    )
+  ) : null;
 }
--- src/pages/calendar/ui/Tabs.tsx
+++ src/pages/calendar/ui/Tabs.tsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import CalendarIcon from '../../../assets/icons/CalendarIcon';
 import WallchartIcon from '../../../assets/icons/WallchartIcon';
+import { useDisapprovedDaysOff } from '../../../features/calendar/api/daysOffApi';
 import { useGetTeamMembers } from '../../../features/settings/teamMembers/teamMemberService';
 import { cl } from '../../../utils';
-import { useDaysOff } from '../lib/daysOffContext';
 import { checkIsOwner } from '../lib/userUtils';
+import { MOCKED_HUB_ID } from './DisapprovedDaysOffList';
 
 const tabs = [
   { name: 'Wallchart', href: '/calendar', icon: <WallchartIcon className="w-5 h-5 cursor-pointer" /> },
@@ -25,11 +26,11 @@ export function Tabs() {
   const { pathname } = useLocation();
   const { data } = useGetTeamMembers({ page: 1, query: '' });
   const members = data?.data.team_members ?? [];
-  const { daysOff } = useDaysOff();
+  const { data: disapprovedDaysOff } = useDisapprovedDaysOff(MOCKED_HUB_ID);
 
   const isOwner = checkIsOwner(members);
 
-  const count = daysOff.filter((i) => !i.isApproved).length;
+  const count = disapprovedDaysOff?.filter((i) => !i.is_approved).length;
 
   const privateTabs = isOwner
     ? [
@@ -44,7 +45,7 @@ export function Tabs() {
 
   return (
     <div className="-mb-px p-2 flex items-center justify-between w-full">
-      {showInputOnTabs.includes(pathname) ? <Input /> : null}
+      {showInputOnTabs.includes(pathname) ? <Input /> : <div></div>}
 
       <nav className="flex space-x-4" aria-label="Tabs">
         {tabs.map((tab) => (
@@ -68,14 +69,14 @@ export function Tabs() {
             to={tab.href}
             className={cl(
               'flex gap-3 rounded-md items-center text-gray-900 border whitespace-nowrap px-3 py-2 shadow-sm text-sm font-medium relative',
-              tab.href === pathname ? 'border-primary-500 bg-primary-600' : 'hover:bg-gray-300'
+              tab.href === pathname ? 'border-primary-200 bg-primary-200' : 'hover:bg-gray-300'
             )}
             aria-current={tab.href === pathname ? 'page' : undefined}
           >
             {tab.icon}
             {tab.name}
 
-            <span className="absolute top-1 -right-2 w-5 text-xs h-5 flex items-center justify-center rounded-full bg-primary-500 text-white">
+            <span className="absolute -top-1 -right-2 w-5 text-xs h-5 flex items-center justify-center rounded-full bg-primary-500 text-white">
               {tab.count}
             </span>
           </Link>
