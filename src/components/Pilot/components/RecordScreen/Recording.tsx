60a76c657e6be18b80497b4dab20d1106c2e1205
aceDavon <d.onate@simpsgroup.co.uk>
2023-05-08 07:41:58 +0100

    Extract the Recording Logs to a standalone component

--- src/components/Pilot/components/RecordScreen/Recording.tsx
+++ src/components/Pilot/components/RecordScreen/Recording.tsx
@@ -1,13 +1,11 @@
 import { useQueryClient } from '@tanstack/react-query';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useReactMediaRecorder } from 'react-media-recorder';
 import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
 import { getUploadAttatchment, uploadRecording } from '../../../../features/workspace/workspaceService';
 import { setRecording } from '../../../../features/workspace/workspaceSlice';
-import moment from 'moment';
-import AvatarWithInitials from '../../../avatar/AvatarWithInitials';
 import '../../../../pages/workspace/tasks/component/views/view.css';
-import { RiPlayFill, RiStopFill } from 'react-icons/ri';
+import VideoEntries from './RecordingLogs';
 
 export interface IFormData {
   append(name: string, value: Blob, fileName?: string): void;
@@ -84,72 +82,5 @@ export default function Recording() {
         })}
       </table>
     </div>
-  );
-}
-
-interface VideoEntriesProps {
-  videoFile: {
-    id: string;
-    path: string;
-    physical_file: {
-      id: string;
-      created_at: string;
-      updated_at: string;
-    };
-  };
-}
-
-function VideoEntries({ videoFile }: VideoEntriesProps) {
-  const { created_at, updated_at } = videoFile.physical_file;
-  const duration = new Date(updated_at).getTime() - new Date(created_at).getTime();
-  const [playToggle, setPlayToggle] = useState<boolean>(false);
-  const videoRef: React.MutableRefObject<HTMLVideoElement | null> = useRef(null);
-  const handlePlayBack = () => {
-    if (videoRef.current) {
-      if (playToggle) {
-        videoRef.current.pause();
-        setPlayToggle(!playToggle);
-      } else {
-        videoRef.current.play();
-        setPlayToggle(!playToggle);
-      }
-    }
-  };
-  const toggleControls = () =>
-    videoRef.current?.hasAttribute('controls')
-      ? videoRef.current.removeAttribute('controls')
-      : videoRef.current?.setAttribute('controls', 'true');
-
-  return (
-    <tr className="flex space-x-10 border-b items-center p-2 relative">
-      <td>
-        <AvatarWithInitials initials="MD" width="w-5" height="h-5" />
-      </td>
-      <td className="relative">
-        <video
-          ref={videoRef}
-          src={videoFile.path}
-          height={60}
-          width={100}
-          tabIndex={0}
-          className="recording-tag"
-          onMouseEnter={toggleControls}
-          onMouseLeave={toggleControls}
-          onEnded={() => setPlayToggle(!playToggle)}
-        />
-        {playToggle ? (
-          <RiStopFill
-            className="absolute top-10 left-1 h-4 w-4 text-gray-200 cursor-pointer hover:text-gray-200"
-            onClick={() => handlePlayBack()}
-          />
-        ) : (
-          <RiPlayFill
-            className="absolute top-10 left-1 h-4 w-4 text-gray-200 cursor-pointer hover:text-gray-200"
-            onClick={() => handlePlayBack()}
-          />
-        )}
-      </td>
-      <td>{moment(duration).format('HH:mm:ss')}</td>
-    </tr>
   );
 }
