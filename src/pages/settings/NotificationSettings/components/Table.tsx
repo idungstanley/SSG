27ece018512067663477803d47d59f2873198b7d
alhajideen <salifunurudeen834@gmail.com>
2023-05-08 15:59:52 +0100

    Add functionality to make the notification collapse from the heading

--- src/pages/settings/NotificationSettings/components/Table.tsx
+++ src/pages/settings/NotificationSettings/components/Table.tsx
@@ -3,7 +3,13 @@ import {
   GetNotificationSettingsService,
   useChangeNotificationSettings
 } from '../../../../features/general/notification/notificationService';
-import THeadData from './THead';
+// import THeadData from './THead';
+import { MdOpenInBrowser, MdOutlineAlternateEmail } from 'react-icons/md';
+import { GoDeviceMobile } from 'react-icons/go';
+import { GrApps } from 'react-icons/gr';
+import { Disclosure } from '@headlessui/react';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
+import { cl } from '../../../../utils';
 
 export default function NotificaitonTable() {
   const { data } = GetNotificationSettingsService();
@@ -24,70 +30,114 @@ export default function NotificaitonTable() {
     });
   };
   return (
-    <div className="w-full border border-gray-300">
+    <div className="bg-white border border-gray-300 ">
       {data &&
         Object.keys(data).map((category) => (
-          <div key={category}>
-            <div className="bg-gray-200">
-              <THeadData notificationHead={category} />
-            </div>
-            <div className="bg-white p-4 w-full">
-              {data[category]?.types &&
-                Object.keys(data[category].types).map((cat: string) => (
-                  <div key={cat} className="flex justify-between items-center w-full">
-                    <div className="pl-10 py-3.5 text-left text-sm font-semibold text-gray-900 w-2/4">
-                      {data[category].types[cat]?.name}
-                    </div>
-                    <div className="flex w-2/4 justify-between">
-                      <div className="px-3 flex justify-cemter">
-                        <input
-                          checked={data[category].types[cat]?.is_email}
-                          className="h-5 w-5 cursor-pointer bg-gray-300 border-gray-500 ml-2"
-                          type="checkbox"
-                          onChange={() => {
-                            handleChange(
-                              category,
-                              data[category].types[cat]?.type,
-                              'email',
-                              data[category].types[cat]?.is_email
-                            );
-                          }}
-                        />
+          <Disclosure key={category}>
+            {({ open }) => (
+              <div className="w-full mt-2">
+                <Disclosure.Button className="w-full">
+                  <div className="bg-gray-200">
+                    <div className="flex">
+                      <div className="py-6 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 w-2/4">
+                        <div className="flex items-center space-x-2">
+                          <ChevronRightIcon className={cl(open ? 'rotate-90 transform' : '', 'w-6 h-6')} />
+                          <p className="uppercase" style={{ fontSize: '15px' }}>
+                            {category}
+                          </p>
+                        </div>
                       </div>
-                      <div className="px-3 flex justify-cemter">
-                        <input
-                          type="checkbox"
-                          className="h-5 w-5 cursor-pointer checked:bg-gray-300 border-gray-500"
-                          checked
-                        />
-                      </div>
-                      <div className="px-3 flex justify-cemter">
-                        <input
-                          type="checkbox"
-                          className="h-5 w-5 cursor-pointer checked:bg-gray-300 border-gray-500"
-                          checked={data[category].types[cat]?.is_site}
-                          onChange={() => {
-                            handleChange(
-                              category,
-                              data[category].types[cat]?.type,
-                              'site',
-                              data[category].types[cat]?.is_site
-                            );
-                          }}
-                        />
-                      </div>
-                      <div className="px-3 flex justify-cemter">
-                        <input
-                          type="checkbox"
-                          className="h-5 w-5 cursor-pointer checked:bg-gray-300 border-gray-500"
-                          checked
-                        />
+                      <div className="flex w-2/4 justify-between">
+                        <div className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                          <div className="flex items-center flex-col">
+                            <MdOutlineAlternateEmail />
+                            <p>Email</p>
+                          </div>
+                        </div>
+                        <div className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                          <div className="flex items-center flex-col">
+                            <GoDeviceMobile />
+                            <p>Mobile</p>
+                          </div>
+                        </div>
+                        <div className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                          <div className="flex items-center flex-col">
+                            <MdOpenInBrowser />
+                            <p>Browser</p>
+                          </div>
+                        </div>
+                        <div className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                          <div className="flex items-center flex-col">
+                            <GrApps />
+                            <p>In-App</p>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
-                ))}
-            </div>
-          </div>
+                </Disclosure.Button>
+                <Disclosure.Panel>
+                  {' '}
+                  <div className="bg-white p-4 w-full">
+                    {data[category]?.types &&
+                      Object.keys(data[category].types).map((cat: string) => (
+                        <div key={cat} className="flex justify-between items-center w-full">
+                          <div className="pl-10 py-3.5 text-left text-sm font-semibold text-gray-900 w-2/4">
+                            {data[category].types[cat]?.name}
+                          </div>
+                          <div className="flex w-2/4 justify-between">
+                            <div className="px-3 flex justify-cemter">
+                              <input
+                                checked={data[category].types[cat]?.is_email}
+                                className="h-5 w-5 cursor-pointer bg-gray-300 border-gray-500 ml-2"
+                                type="checkbox"
+                                onChange={() => {
+                                  handleChange(
+                                    category,
+                                    data[category].types[cat]?.type,
+                                    'email',
+                                    data[category].types[cat]?.is_email
+                                  );
+                                }}
+                              />
+                            </div>
+                            <div className="px-3 flex justify-cemter">
+                              <input
+                                type="checkbox"
+                                className="h-5 w-5 cursor-pointer checked:bg-gray-300 border-gray-500"
+                                checked
+                              />
+                            </div>
+                            <div className="px-3 flex justify-cemter">
+                              <input
+                                type="checkbox"
+                                className="h-5 w-5 cursor-pointer checked:bg-gray-300 border-gray-500"
+                                checked={data[category].types[cat]?.is_site}
+                                onChange={() => {
+                                  handleChange(
+                                    category,
+                                    data[category].types[cat]?.type,
+                                    'site',
+                                    data[category].types[cat]?.is_site
+                                  );
+                                }}
+                              />
+                            </div>
+                            <div className="px-3 flex justify-cemter">
+                              <input
+                                type="checkbox"
+                                className="h-5 w-5 cursor-pointer checked:bg-gray-300 border-gray-500"
+                                checked
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                  </div>
+                </Disclosure.Panel>
+              </div>
+            )}
+          </Disclosure>
         ))}
     </div>
   );
