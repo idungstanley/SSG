979e011a0afa12f28658388d166af1def1f6e673
aceDavon <d.onate@simpsgroup.co.uk>
2023-05-12 12:38:04 +0100

    Refactor Upload service to allow for files Upload
    Change the UploadRecording implementation to a react Query
    implementation using the useMutate to post recorded video files
    and call the mutation on stop of recording.

--- src/components/Pilot/components/RecordScreen/Recording.tsx
+++ src/components/Pilot/components/RecordScreen/Recording.tsx
@@ -1,11 +1,9 @@
-import { useQueryClient } from '@tanstack/react-query';
-import React, { useEffect } from 'react';
-import { useReactMediaRecorder } from 'react-media-recorder';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
-import { uploadRecording } from '../../../../features/workspace/workspaceService';
+import { useUploadRecording } from '../../../../features/workspace/workspaceService';
 import { setRecording } from '../../../../features/workspace/workspaceSlice';
 import '../../../../pages/workspace/tasks/component/views/view.css';
-// import VideoEntries from './RecordingLogs';
+import RecordRTC from 'recordrtc';
 
 export interface IFormData {
   append(name: string, value: Blob, fileName?: string): void;
@@ -16,29 +14,57 @@ export default function Recording() {
   const { activeItemId, activeItemType } = useAppSelector((state) => state.workspace);
 
   const { currentWorkspaceId, accessToken } = useAppSelector((state) => state.auth);
-  const { getRecording } = useAppSelector((state) => state.workspace);
-
-  // const { data } = getUploadAttatchment({ id: activeItemId as string, type: activeItemType });
-  const queryClient = useQueryClient();
   const dispatch = useAppDispatch();
 
-  const { status, startRecording, stopRecording } = useReactMediaRecorder({
-    screen: true,
-    audio: true,
-    mediaRecorderOptions: { mimeType: 'video/webm;codecs=vp9' },
-    onStop: (blobUrl, blob) => {
-      uploadRecording(blob, currentWorkspaceId, accessToken, getRecording.id, queryClient, getRecording.type);
-      dispatch(
-        setRecording({
-          id: null,
-          type: null
-        })
-      );
+  const [recorder, setRecorder] = useState<RecordRTC | null>();
+  const [stream, setStream] = useState<MediaStream | null>();
+  const [recorderState, setRecorderState] = useState<'idle' | 'recording'>('idle');
+
+  const startRecording = async () => {
+    const mediaDevices = navigator.mediaDevices;
+    const audioConstraints: MediaTrackConstraints = {
+      echoCancellation: true,
+      noiseSuppression: true,
+      sampleRate: 44100
+    };
+
+    const options = {
+      mimeType: 'video/webm', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
+      audioBitsPerSecond: 128000,
+      videoBitsPerSecond: 128000
+    };
+
+    const stream: MediaStream = await mediaDevices.getDisplayMedia({
+      audio: audioConstraints,
+      video: true
+    });
+    const recorder = new RecordRTC(stream, options);
+    await recorder.startRecording();
+    setRecorderState('recording');
+    setRecorder(recorder as RecordRTC);
+    setStream(stream);
+  };
+
+  const { mutate } = useUploadRecording();
+
+  const stopRecording = async () => {
+    recorder?.stopRecording();
+    const blob = recorder?.getBlob();
+    if (blob && currentWorkspaceId && accessToken && activeItemId && activeItemType) {
+      await mutate({
+        blob,
+        currentWorkspaceId,
+        accessToken,
+        activeItemId,
+        activeItemType
+      });
+      stream?.getTracks().map((track) => track.stop());
+      setRecorderState('idle');
     }
-  });
+  };
 
   useEffect(() => {
-    if (status !== 'recording') {
+    if (recorderState !== 'recording') {
       dispatch(
         setRecording({
           id: activeItemId as string,
@@ -46,11 +72,11 @@ export default function Recording() {
         })
       );
     }
-  }, []);
+  }, [recorderState]);
 
   return (
     <div>
-      {status == 'recording' ? (
+      {recorderState == 'recording' ? (
         <>
           <button onClick={stopRecording} className="screenRecording">
             Stop Recording
@@ -63,24 +89,6 @@ export default function Recording() {
           </div>
         </>
       )}
-      {/* <table className="w-full mx-auto p-1">
-        <thead>
-          <tr className="flex mx-2 border-b-2 py-2 space-x-12">
-            <th className="capitalize font-bold">user</th>
-            <th className="capitalize font-bold">recording</th>
-            <th className="capitalize font-bold">duration</th>
-          </tr>
-        </thead>
-        {data?.data.attachments.map((video) => {
-          //  Leave for reference purposes
-          // console.log(data.data.attachments);
-          return (
-            <tbody key={video.id}>
-              <VideoEntries videoFile={video} />
-            </tbody>
-          );
-        })}
-      </table> */}
     </div>
   );
 }
