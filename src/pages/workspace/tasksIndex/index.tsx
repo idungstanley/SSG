23a37b60bdd2c659268d666f83b342d59ee9d150
Omachi John <omachi.john@yahoo.com>
2023-05-15 15:58:58 +0100

    update tasks index component

--- src/pages/workspace/tasksIndex/index.tsx
+++ src/pages/workspace/tasksIndex/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function TasksIndex() {
   return (
-    <div>
+    <div className="flex h-full justify-center items-center">
       <h1>Index for tasks under construction</h1>
     </div>
   );
