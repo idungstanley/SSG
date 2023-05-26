29aa9853539697d0faf9e3f6e8121af970fbba28
aceDavon <d.onate@simpsgroup.co.uk>
2023-05-26 08:50:06 +0100

    Extract the Header Modal to a Standalone component

--- src/layout/components/MainLayout/Header/AdditionHeader.tsx
+++ src/layout/components/MainLayout/Header/AdditionHeader.tsx
@@ -2,14 +2,15 @@ import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
 import { HiOutlineUpload } from 'react-icons/hi';
 import { BsFillGrid3X3GapFill } from 'react-icons/bs';
 import { MdHelpOutline, MdTab } from 'react-icons/md';
-import { GetTimeEntriesService, useMediaStream } from '../../../../features/task/taskService';
-import { IRecorderLastMemory } from '../../../../features/workspace/workspace.interfaces';
+import { GetTimeEntriesService } from '../../../../features/task/taskService';
 import { resetWorkSpace } from '../../../../features/workspace/workspaceSlice';
-import { useNavigate, useParams } from 'react-router-dom';
-import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { IoAlarmSharp } from 'react-icons/io5';
 import moment from 'moment';
 import { ITimeEntriesRes } from '../../../../features/task/interface.tasks';
+import BlinkerModal from './HeaderModal';
+
 export const handleEntity = ({
   workSpaceId,
   hubId,
@@ -73,46 +74,6 @@ export default function AdditionalHeader() {
         <BsFillGrid3X3GapFill className="w-5 h-5" />
         <MdHelpOutline className="w-5 h-5" />
       </div>
-    </div>
-  );
-}
-
-interface BlinkerProps {
-  toggleFn: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-function BlinkerModal({ toggleFn }: BlinkerProps) {
-  const { recorder, stream } = useAppSelector((state) => state.task);
-  const { recorderLastMemory } = useAppSelector((state) => state.workspace);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const { activeTabId, workSpaceId, hubId, listId } = recorderLastMemory as IRecorderLastMemory;
-
-  const { handleStopStream } = useMediaStream();
-
-  const stopRecording = () => {
-    handleStopStream({
-      recorder,
-      stream
-    });
-    dispatch(resetWorkSpace({ activeTabId, workSpaceId, hubId, listId }));
-    navigate(handleEntity({ workSpaceId, hubId, listId }), { replace: true });
-  };
-  return (
-    <div
-      className="flex flex-col w-64 rounded-lg shadow-2xl bg-warmGray-50 absolute right-1/2 z-50"
-      tabIndex={0}
-      onMouseLeave={() => toggleFn(false)}
-    >
-      <span className="capitalize px-2 py-3 hover:bg-gray-300 cursor-pointer" onClick={() => stopRecording()}>
-        Stop Recording
-      </span>
-      <span
-        className="capitalize px-2 py-3 hover:bg-gray-300 cursor-pointer"
-        onClick={() => dispatch(resetWorkSpace({ activeTabId, workSpaceId, hubId, listId }))}
-      >
-        Return to Recording tab
-      </span>
     </div>
   );
 }
---
+++ src/layout/components/MainLayout/Header/HeaderModal.tsx
@@ -0,0 +1,46 @@
+import { useNavigate } from 'react-router-dom';
+import { useMediaStream } from '../../../../features/task/taskService';
+import { IRecorderLastMemory } from '../../../../features/workspace/workspace.interfaces';
+import { resetWorkSpace } from '../../../../features/workspace/workspaceSlice';
+import { handleEntity } from './AdditionHeader';
+import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
+
+interface BlinkerProps {
+  toggleFn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function BlinkerModal({ toggleFn }: BlinkerProps) {
+  const { recorder, stream } = useAppSelector((state) => state.task);
+  const { recorderLastMemory } = useAppSelector((state) => state.workspace);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { activeTabId, workSpaceId, hubId, listId } = recorderLastMemory as IRecorderLastMemory;
+
+  const { handleStopStream } = useMediaStream();
+
+  const stopRecording = () => {
+    handleStopStream({
+      recorder,
+      stream
+    });
+    dispatch(resetWorkSpace({ activeTabId, workSpaceId, hubId, listId }));
+    navigate(handleEntity({ workSpaceId, hubId, listId }), { replace: true });
+  };
+  return (
+    <div
+      className="flex flex-col w-64 rounded-lg shadow-2xl bg-warmGray-50 absolute right-1/2 z-50"
+      tabIndex={0}
+      onMouseLeave={() => toggleFn(false)}
+    >
+      <span className="capitalize px-2 py-3 hover:bg-gray-300 cursor-pointer" onClick={() => stopRecording()}>
+        Stop Recording
+      </span>
+      <span
+        className="capitalize px-2 py-3 hover:bg-gray-300 cursor-pointer"
+        onClick={() => dispatch(resetWorkSpace({ activeTabId, workSpaceId, hubId, listId }))}
+      >
+        Return to Recording tab
+      </span>
+    </div>
+  );
+}
