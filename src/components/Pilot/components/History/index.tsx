4744a616e03ec5aa16c5fcf255ffee65fa7fd26b
aceDavon <d.onate@simpsgroup.co.uk>
2023-05-31 13:41:03 +0100

    Refactor the HistoryLogs component
    - Change the implementation of the history logs component on the pilot
      to match the figma design,
    - Destructure all needed variables from request data and reduce font
      sizes on all text on the component,
    - Modify the interface implementation for the history logs, adding new
      fields as now available on request data.

--- src/components/Pilot/components/History/index.tsx
+++ src/components/Pilot/components/History/index.tsx
@@ -6,22 +6,38 @@ import { useGetItemHistory } from '../../../../features/general/history/historyS
 import { OutputDateTime } from '../../../../app/helpers';
 import SectionArea from '../SectionArea';
 import { DocumentTextIcon } from '@heroicons/react/24/outline';
+import { BsFilter, BsThreeDots } from 'react-icons/bs';
+import { BiSearch } from 'react-icons/bi';
+import { AiFillPlusCircle } from 'react-icons/ai';
+import moment from 'moment';
 
 export default function History() {
   const { pilotSideOver } = useAppSelector((state) => state.slideOver);
 
   const id = pilotSideOver.id;
   const type = pilotSideOver.type;
-  console.log(type);
 
   // ! implement pagination
   const { data: logs, status } = useGetItemHistory({ type, id });
+  console.log(logs);
 
   return (
     <>
-      <SectionArea label="Logs" icon={<DocumentTextIcon className="w-4 h-4" />} />
-
-      <div className="relative h-full w-full">
+      <div className="flex items-center space-x-4">
+        <div className="w-1/5">
+          <SectionArea label="Logs" icon={<DocumentTextIcon className="w-4 h-4" />} />
+        </div>
+        <div className="flex items-center relative">
+          <input type="text" className="rounded-2xl w-64 h-7 border-purple-500 px-6 text-xs" placeholder="Search..." />
+          <BiSearch className="w-4 h-4 absolute left-2" />
+        </div>
+        <div className="flex space-x-2 items-center bg-gray-200 h-5 border-purple-500 rounded-lg px-1">
+          <BsFilter />
+          <span className="capitalize text-xs">filter</span>
+        </div>
+        <BsThreeDots />
+      </div>
+      <div className="relative h-full w-full mt-2">
         {/* status checking */}
         {status === 'error' ? (
           <FullScreenMessage title="Oops, an error occurred :(" description="Please try again later." />
@@ -33,27 +49,45 @@ export default function History() {
           !logs.length ? (
             <FullScreenMessage title="No logs yet" description="Do any action." />
           ) : (
-            <ul className="absolute top-0 left-0 flex w-full h-full overflow-y-scroll flex-col divide-y divide-gray-200">
-              {/* logs list */}
-              {logs.map((activityLog) => (
-                <li key={activityLog.id} className="py-2 flex justify-between items-center">
-                  <div className="flex items-center">
-                    <InitialsAvatar
-                      size={10}
-                      colour={activityLog.team_member.colour}
-                      initials={activityLog.team_member.initials}
-                    />
-
-                    <div className="ml-3">
-                      <p className="text-indigo-600">{activityLog.team_member.name}</p>
-                      <p className="text-gray-400">{activityLog.category}</p>
-                    </div>
+            <table className="absolute top-2 left-0 flex w-full h-full overflow-y-scroll flex-col divide-y divide-gray-200">
+              <thead className="w-full">
+                <tr className="w-full flex justify-between items-center">
+                  <div className="flex w-2/3 space-x-10 capitalize">
+                    <th>user</th>
+                    <th>date</th>
+                    <th>time</th>
+                    <th>description</th>
                   </div>
+                  <th>
+                    <AiFillPlusCircle className="w-4 h-4" />
+                  </th>
+                </tr>
+              </thead>
+              {/* logs list */}
+              {logs.map((activityLog) => {
+                const { created_at, category, model, created_by } = activityLog;
+                const { user } = created_by;
+                const startDate = moment(created_at);
+                const currentDate = moment(new Date().toISOString());
+                const duration = moment.duration(currentDate.diff(startDate));
+                const time = moment(created_at.substring(11, 19), 'HH:mm:ss').format('h:mm A');
+                return (
+                  <li key={activityLog.id} className="py-2 flex space-x-6 w-full items-center">
+                    <InitialsAvatar size={6} colour={user.color} initials={user.initials} />
+                    <div className="text-xs">
+                      <span>{duration.humanize()} ago</span>
+                    </div>
+                    <div className="">
+                      <span className="text-xs">{time}</span>
+                    </div>
 
-                  <p className="text-gray-400 text-sm">{OutputDateTime(activityLog.created_at)}</p>
-                </li>
-              ))}
-            </ul>
+                    <p className="text-gray-400 text-xs capitalize">
+                      {user.name} {category} {model}
+                    </p>
+                  </li>
+                );
+              })}
+            </table>
           )
         ) : null}
       </div>
