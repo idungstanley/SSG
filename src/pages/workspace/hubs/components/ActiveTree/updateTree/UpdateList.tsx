0549c2c36e6e3e6bcf425a183978fb767cb7dd40
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-06-01 09:33:49 +0100

    Fix issue

--- src/pages/workspace/hubs/components/ActiveTree/updateTree/UpdateList.tsx
+++ src/pages/workspace/hubs/components/ActiveTree/updateTree/UpdateList.tsx
@@ -1,6 +1,10 @@
 import { List } from '../activetree.interfaces';
 
-export default function UpdateList(existingTree: List[], updateFn: (i: List) => List, idToUpdate: string): List[] {
+export default function UpdateList(
+  existingTree: List[],
+  updateFn: (i: List) => List,
+  idToUpdate?: string | null
+): List[] {
   return existingTree.map((list) => {
     if (list.id === idToUpdate) {
       return updateFn(list);
