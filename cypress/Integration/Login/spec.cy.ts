e6a12694d0b9174d4421f2db1d10531e20f5f518
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-06-21 20:21:13 +0100

    Add updated folder pattern

--- cypress/Integration/Login/spec.cy.ts
+++ cypress/Integration/Login/spec.cy.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress"/>
 import { testData } from '../../Fixtures/testData';
-import Login from '../../PageObject/login.cy';
+import Login from '../../PageObject/Login/login.cy';
 
 describe('template spec', () => {
   const login = new Login();
