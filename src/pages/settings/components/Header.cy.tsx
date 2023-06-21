c8b299c9835e577a1cd3d146ad4521bb33a8d06f
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-06-21 08:51:09 +0100

    Add config fix

--- src/pages/settings/components/Header.cy.tsx
+++ src/pages/settings/components/Header.cy.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import Header from './Header'
+/// <reference types="cypress" />
+import React from 'react';
+import Header from './Header';
 
 describe('<Header />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Header />)
-  })
-})
+    cy.mount(<Header />);
+  });
+});
