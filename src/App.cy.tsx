c58a7e34d75dd30a648cd9e5c5c3bb16e0868587
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-06-21 09:48:34 +0100

    Add cypress configuration

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -4,6 +4,6 @@ import App from './App';
 describe('<App />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<App />);
+    cy.get('strong');
   });
 });
