1df6e7cc616b534917035ead6940cfe9c291631f
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-06-22 15:41:35 +0100

    Remove subs from variable declaration

--- src/utils/EntityTypes/EntityType.ts
+++ src/utils/EntityTypes/EntityType.ts
@@ -1,10 +1,6 @@
 const EntityType = {
   hub: 'hub',
-  subhub: 'subhub',
   wallet: 'wallet',
-  subwallet: 'subwallet',
-  subwalletTwo: 'subwallet2',
-  subwalletThree: 'subwallet3',
   list: 'list'
 };
 export { EntityType };
