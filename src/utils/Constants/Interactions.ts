34d0219219a839de77679855ed459f306be8f265
alhajideen <salifunurudeen834@gmail.com>
2023-06-22 12:40:55 +0100

    create interactions for buttons

---
+++ src/utils/Constants/Interactions.ts
@@ -0,0 +1,15 @@
+const Defaultbutton = {
+  backgroundColor: '#f4f4f4',
+  borderRadius: '5px',
+  textColor: '#424242',
+  padding: '0 2px',
+  marginRight: '10px'
+};
+
+const activeButton = {
+  backgroundColor: '#f9e6ff',
+  borderRadius: '5px',
+  textColor: '#bf01fe',
+  padding: '0 2px',
+  marginRight: '10px'
+};
