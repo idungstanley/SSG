8cf9d573c3150b48310756f2d391316355f8846e
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-06-27 14:09:00 +0300

    fixed wrong key prop

--- src/components/TasksHeader/ui/Filter/ui/ListBox/ui/SelectedValues/AssigneeValues.tsx
+++ src/components/TasksHeader/ui/Filter/ui/ListBox/ui/SelectedValues/AssigneeValues.tsx
@@ -13,7 +13,7 @@ interface AssigneeValuesProps {
 
 export function AssigneeValues({ values }: AssigneeValuesProps) {
   return (
-    <div className="flex items-center max-w-5">
+    <div className="flex items-center">
       {values.length === 0 ? (
         <span className="block">{SELECT_VALUE}</span>
       ) : (
@@ -22,7 +22,7 @@ export function AssigneeValues({ values }: AssigneeValuesProps) {
             title={i.value}
             key={i.id}
             style={{ backgroundColor: i.color }}
-            className="flex -ml-2 h-6 w-6 items-center justify-center rounded-full text-xs uppercase text-white"
+            className="flex -ml-2 h-5 w-5 items-center justify-center rounded-full text-xs uppercase text-white"
           >
             {i.initials}
           </span>
