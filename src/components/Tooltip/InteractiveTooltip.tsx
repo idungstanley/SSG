2a1610bcf26f858a2122ead134649ec6c66cd916
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-06-29 17:00:37 +0100

    Updated tooltip color

--- src/components/Tooltip/InteractiveTooltip.tsx
+++ src/components/Tooltip/InteractiveTooltip.tsx
@@ -23,8 +23,13 @@ export default function InteractiveTooltip({ children, content }: TooltipProps) 
     <div className="relative inline-block" onMouseEnter={handleOpenTooltip} onMouseLeave={handleCloseTooltip}>
       {isTooltipOpen && (
         <div className="absolute z-10 -top-36 -right-7" onClick={(e) => handleTooltipClick(e)}>
-          <div className="w-auto p-2 text-white bg-black rounded shadow-lg">{content}</div>
-          <div className="absolute left-0 right-0 w-2 h-2 ml-auto mr-auto -mt-1 transform rotate-45 bg-black"></div>
+          <div className="w-auto p-2 text-white rounded shadow-lg" style={{ backgroundColor: '#424242' }}>
+            {content}
+          </div>
+          <div
+            className="absolute left-0 right-0 w-2 h-2 ml-auto mr-auto -mt-1 transform rotate-45"
+            style={{ backgroundColor: '#424242' }}
+          ></div>
         </div>
       )}
       <div className="inline-block rounded cursor-pointer">{children}</div>
