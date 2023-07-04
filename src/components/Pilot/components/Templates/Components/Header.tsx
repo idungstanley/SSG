4dc9fa7ba0dabcb24e82f34463b560c316d66afe
alhajideen <salifunurudeen834@gmail.com>
2023-07-04 16:07:02 +0100

    add padding to templates icon

--- src/components/Pilot/components/Templates/Components/Header.tsx
+++ src/components/Pilot/components/Templates/Components/Header.tsx
@@ -4,10 +4,10 @@ import TemplatesIcon from '../../../../../assets/icons/Templates';
 function Header() {
   return (
     <div className="w-full">
-      <div className="border-2 flex items-center text-center py-1 bg-white">
+      <div className="border-2 flex items-center text-center py-1 px-2 bg-white">
         <TemplatesIcon />
-        <h1 className="text-base-xl mx-4">Templates</h1>
-        <button className="p-0.5 border-blue-400 border-solid border-2 rounded-lg text-xs">ADD</button>
+        <h1 className="text-base-xl mx-2">Templates</h1>
+        <button className="w-14  border-blue-400 border-solid border-2 rounded-lg text-xs">ADD</button>
       </div>
     </div>
   );
