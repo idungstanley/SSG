baffb20f66cd6261d13d6fca64ab56cd10d0195e
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-07-04 17:52:48 +0100

    Add functionality for adding colors on list level

--- src/pages/workspace/pilot/components/createEntity/createList/CreateList.tsx
+++ src/pages/workspace/pilot/components/createEntity/createList/CreateList.tsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
-import { Button, Input } from '../../../../../../components';
+import { Button, Checkbox, Input } from '../../../../../../components';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useAppDispatch, useAppSelector } from '../../../../../../app/hooks';
 import { setCreateListSlideOverVisibility } from '../../../../../../features/general/slideOver/slideOverSlice';
 import { setSubDropdownMenu, setshowMenuDropdown } from '../../../../../../features/hubs/hubSlice';
-import { setCreateWlLink } from '../../../../../../features/workspace/workspaceSlice';
+import { setCreateEntityType, setCreateWlLink } from '../../../../../../features/workspace/workspaceSlice';
 import { createListService } from '../../../../../../features/list/listService';
 import { EntityType } from '../../../../../../utils/EntityTypes/EntityType';
+import Palette from '../../../../../../components/ColorPalette';
+import Assignee from '../../../../tasks/assignTask/Assignee';
+import ArrowDown from '../../../../../../assets/icons/ArrowDown';
+import Wand from '../../../../../../assets/icons/Wand';
+import { ListColourProps } from '../../../../../../components/tasks/ListItem';
 
 export default function CreateList() {
   const queryClient = useQueryClient();
   const dispatch = useAppDispatch();
   // const { currentItemId } = useAppSelector((state) => state.workspace);
   const { selectedTreeDetails, createWLID } = useAppSelector((state) => state.hub);
+  const [paletteColor, setPaletteColor] = useState<string | ListColourProps | undefined>('');
+  const [showPalette, setShowPalette] = useState<boolean>(false);
   const { type, id } = selectedTreeDetails;
   const { createWlLink } = useAppSelector((state) => state.workspace);
 
@@ -34,6 +41,9 @@ export default function CreateList() {
   const defaultListFormState = {
     name: ''
   };
+  const onClose = () => {
+    dispatch(setCreateEntityType(null));
+  };
 
   const [formState, setFormState] = useState(defaultListFormState);
 
@@ -44,6 +54,10 @@ export default function CreateList() {
     });
   };
 
+  const handleShowPalette = (e: React.MouseEvent<HTMLDivElement | HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
+    setShowPalette((prev) => !prev);
+  };
   const { name } = formState;
   const onSubmit = async () => {
     await createList.mutateAsync({
@@ -54,23 +68,54 @@ export default function CreateList() {
   };
 
   return (
-    <div className="p-2">
-      <Input
-        label="Enter New List Name:"
-        placeholder="Enter List Name"
-        name="name"
-        value={name}
-        type="text"
-        onChange={handleListChange}
-      />
-      <div className="flex justify-end space-x-3 pt-2">
+    <div className="p-2 overflow-y-auto h-auto" style={{ maxHeight: '420px' }}>
+      <div className="flex flex-col mb-2">
+        <span className="font-bold">Create A Hub</span>
+        <span className="font-medium">Allows you manage all entities within the workspace</span>
+      </div>
+      <div className="flex flex-col border border-gray-200 alsoit-gray-bg p-4 rounded space-y-2">
+        <div className="flex relative">
+          <Input placeholder="Hub Name" name="name" value={name} type="text" onChange={handleListChange} />
+          <div
+            className="absolute cursor-pointer flex items-center right-2 top-3"
+            onClick={(e) => handleShowPalette(e)}
+          >
+            <Wand />
+          </div>
+        </div>
+        <div className="flex h-10 p-1 w-full border bg-white rounded justify-between items-center">
+          <span>Manage this Hub with other application</span>
+          <ArrowDown />
+        </div>
+        <div className="flex h-10 p-1 w-full border bg-white rounded justify-between items-center">
+          <span>Share with public</span>
+          <Assignee option="share" />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <span className="font-bold">Entity Description</span>
+          <Checkbox
+            checked={true}
+            onChange={() => ({})}
+            description="Host other entities list wallets and lists"
+            height="5"
+            width="5"
+          />
+          <Checkbox checked={false} onChange={() => ({})} description="Host other entities" height="5" width="5" />
+          <Checkbox checked={false} onChange={() => ({})} description="Host other entities" height="5" width="5" />
+        </div>
+        <div className="relative ml-24 mt-32">
+          {showPalette ? <Palette title="Hub Colour" setPaletteColor={setPaletteColor} /> : null}
+        </div>
+      </div>
+      <div className="flex justify-between pt-2 space-x-3">
+        <Button buttonStyle="white" onClick={onClose} loading={false} label="Close" width={20} height="h-7" />
         <Button
           buttonStyle="primary"
           onClick={onSubmit}
-          label="Create List"
-          padding="py-2 px-4"
-          height="h-10"
-          width="w-20"
+          label="Create Hub"
+          padding="py-2 px-2"
+          height="h-7"
+          width="w-24"
         />
       </div>
     </div>
