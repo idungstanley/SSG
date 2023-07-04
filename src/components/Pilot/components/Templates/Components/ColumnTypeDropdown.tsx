25d4567554b68545b184519b0f5bdbc45bb44429
alhajideen <salifunurudeen834@gmail.com>
2023-07-04 18:00:28 +0100

    put custom types in an array

--- src/components/Pilot/components/Templates/Components/ColumnTypeDropdown.tsx
+++ src/components/Pilot/components/Templates/Components/ColumnTypeDropdown.tsx
@@ -2,7 +2,102 @@ import * as React from 'react';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import DropdownIcon from '../../../../../assets/icons/dropdownIcon';
-import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineNumber, AiOutlineSearch } from 'react-icons/ai';
+import { BiCaretDownSquare, BiText } from 'react-icons/bi';
+import { BsTag, BsTextareaT } from 'react-icons/bs';
+import { MdDateRange, MdOutlineAttachFile, MdPersonOutline } from 'react-icons/md';
+import { TbBatteryEco } from 'react-icons/tb';
+import { IoMdCheckboxOutline } from 'react-icons/io';
+import { HiOutlineMail } from 'react-icons/hi';
+import { ImClearFormatting } from 'react-icons/im';
+import { TfiLocationPin, TfiMoney } from 'react-icons/tfi';
+
+const columnTypes = [
+  {
+    id: 'Dropdown',
+    title: 'Dropdown',
+    description: '',
+    icon: <BiCaretDownSquare />
+  },
+  {
+    id: 'Text',
+    title: 'Text',
+    description: '',
+    icon: <BiText />
+  },
+  {
+    id: 'Text Area',
+    title: 'Text Area',
+    description: '',
+    icon: <BsTextareaT />
+  },
+  {
+    id: 'Date',
+    title: 'Date',
+    description: '',
+    icon: <MdDateRange />
+  },
+  {
+    id: 'Progress',
+    title: 'Progress',
+    description: '',
+    icon: <TbBatteryEco />
+  },
+  {
+    id: 'Number',
+    title: 'Number',
+    description: '',
+    icon: <AiOutlineNumber />
+  },
+  {
+    id: 'Checkbox',
+    title: 'Checkbox',
+    description: '',
+    icon: <IoMdCheckboxOutline />
+  },
+  {
+    id: 'Email',
+    title: 'Email',
+    description: '',
+    icon: <HiOutlineMail />
+  },
+  {
+    id: 'Files',
+    title: 'Files',
+    description: '',
+    icon: <MdOutlineAttachFile />
+  },
+  {
+    id: 'Formula',
+    title: 'Formula',
+    description: '',
+    icon: <ImClearFormatting />
+  },
+  {
+    id: 'Labels',
+    title: 'Labels',
+    description: '',
+    icon: <BsTag />
+  },
+  {
+    id: 'Location',
+    title: 'Location',
+    description: '',
+    icon: <TfiLocationPin />
+  },
+  {
+    id: 'Money',
+    title: 'Money',
+    description: '',
+    icon: <TfiMoney />
+  },
+  {
+    id: 'People',
+    title: 'People',
+    description: '',
+    icon: <MdPersonOutline />
+  }
+];
 
 export default function ColumnTypeDropdown() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -48,49 +143,14 @@ export default function ColumnTypeDropdown() {
           />
         </section>
         <hr />
-        <MenuItem onClick={handleClose}>
-          {/* <DropdownIcon /> */}
-          Dropdown
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Text
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Text Area
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Date
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Progress
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Number
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Checkbox
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Email
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Files
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Formula
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Labels
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Location
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          Money
-        </MenuItem>
-        <MenuItem className="text-alsoit-text-md" onClick={handleClose}>
-          People
-        </MenuItem>
+        {columnTypes.map((item) => {
+          return (
+            <MenuItem key={item.id} onClick={handleClose}>
+              <span className="mx-1"> {item.icon}</span>
+              <p className="text-alsoit-text-lg">{item.title}</p>
+            </MenuItem>
+          );
+        })}
       </Menu>
     </div>
   );
