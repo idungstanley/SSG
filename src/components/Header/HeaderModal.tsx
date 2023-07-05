110f1253224c2303240254f6714a87b24500dda6
aceDavon <d.onate@simpsgroup.co.uk>
2023-07-05 17:32:53 +0100

    Add Ability to control timeclock from header\
    - Refactor Header Modal to a component that takes ReactNode as child
      and renders as required,
    - Refactor the RecordBlinker modal to use the Header Modal wrapper,
    - Create the TimerModal for the header timer and pass as child to the
      header Modal.

---
+++ src/components/Header/HeaderModal.tsx
@@ -0,0 +1,21 @@
+import { Dispatch, ReactNode, SetStateAction } from 'react';
+
+interface HeaderModalprops {
+  toggleFn: Dispatch<SetStateAction<boolean>>;
+  children: ReactNode;
+  styles?: string;
+}
+
+function HeaderModal({ toggleFn, children, styles }: HeaderModalprops) {
+  return (
+    <div
+      className={`w-64 rounded-lg shadow-2xl bg-warmGray-50 absolute -right-5 z-50 ${styles}`}
+      tabIndex={0}
+      onMouseLeave={() => toggleFn(false)}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default HeaderModal;
