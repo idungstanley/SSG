80aa5263c9b3e854739f6465f476250ee629b127
aceDavon <d.onate@simpsgroup.co.uk>
2023-07-13 08:06:35 +0100

    Extract the HeaderTimeModal Component
    - Extract the time modal component to a standalone component, reducing the
      the contents of the additionalHeader component for readability sake,
    - Implement the headerClock UI with moment instead of dayjs due to
      inconcictencies.

--- src/layout/components/MainLayout/Header/AdditionHeader.tsx
+++ src/layout/components/MainLayout/Header/AdditionHeader.tsx
@@ -11,6 +11,8 @@ import dayjs from 'dayjs';
 import HeaderModal from '../../../../components/Header/HeaderModal';
 import TimerModal from './TimerOptions';
 import { useParams } from 'react-router-dom';
+import moment from 'moment-timezone';
+import HeaderTimeModal from './HeaderTimeModal';
 
 export const handleEntity = ({
   workSpaceId,
@@ -41,6 +43,7 @@ export default function AdditionalHeader() {
   const { workSpaceId: workspaceId } = useParams();
   const { activeEntityName } = useAppSelector((state) => state.workspace);
   const { refetch } = useCurrentTime({ workspaceId });
+  // console.log(moment('13-07-2023 07:52', 'DD-MM-YYYY HH:mm').format('hh:mm'));
 
   const headerClockFn = () =>
     window.setInterval(() => {
@@ -128,10 +131,10 @@ export default function AdditionalHeader() {
           onMouseLeave={() => setShowClock((prev) => ({ ...prev, showMinimal: false }))}
         >
           <span className="text-center text-alsoit-text-md">
-            {dayjs(HeaderClock, 'DD-MM-YYYY hh:mm').format('hh:mm')}
+            {moment(HeaderClock, 'DD-MM-YYYY hh:mm').format('hh:mm')}
           </span>
           <span className="text-center text-alsoit-text-md">
-            {dayjs(HeaderClock, 'DD-MM-YYYY hh:mm').format('DD-MM-YYYY')}
+            {moment(HeaderClock, 'DD-MM-YYYY hh:mm').format('DD-MM-YYYY')}
           </span>
           {clockModal && (
             <HeaderModal toggleFn={setClockModal} styles="top-7 right-28">
@@ -147,38 +150,6 @@ export default function AdditionalHeader() {
           )}
         </div>
         <MdHelpOutline className="w-5 h-5" />
-      </div>
-    </div>
-  );
-}
-
-function HeaderTimeModal() {
-  const [time, setTime] = useState<string>(dayjs().format('hh:mm:ss a'));
-
-  const timeUpdateFn = () => window.setInterval(() => setTime(dayjs().format('hh:mm:ss a')), 1000);
-
-  useEffect(() => {
-    timeUpdateFn();
-
-    return () => document.addEventListener('visibilitychange', timeUpdateFn);
-  }, []);
-  return (
-    <div className="flex flex-col space-y-4 w-134 z-50 bg-alsoit-gray-50 h-screen transition-transform opacity-100 transform translate-y-0 delay-700">
-      <div className="flex justify-start flex-col space-y-2 w-full border-b border-alsoit-gray-300 px-4 py-6">
-        <span style={{ fontSize: '35px', padding: '0 0 8px 0' }}>{time}</span>
-        {dayjs().format('dddd MMMM D, YYYY')}
-      </div>
-      <div className="border-b border-alsoit-gray-300 px-4 py-6">
-        <p className="text-center">Calendar Goes here</p>
-      </div>
-      <div className="w-full flex flex-col space-y-2 px-4 py-6">
-        <span className="font-semibold text-alsoit-text-lg">Schedule</span>
-        <input
-          type="text"
-          className="w-72 h-6 text-alsoit-text-md rounded border-alsoit-border-base px-1"
-          placeholder="search..."
-        />
-        <span className="italic text-alsoit-text-md font-semibold">No activity found for the selected time</span>
       </div>
     </div>
   );
