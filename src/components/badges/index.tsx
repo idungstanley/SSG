6d316e85ec7fe926b1557e197b17c6d7f84fcf6c
Omachi John <omachi.john@yahoo.com>
2023-07-14 09:11:52 +0100

    make attachment component

--- src/components/badges/index.tsx
+++ src/components/badges/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Description from './Description';
+import AttachFile from './AttachFile';
 
 export default function Badges() {
   return (
-    <div>
+    <div className="flex items-center space-x-1">
       <Description />
+      <AttachFile />
     </div>
   );
 }
