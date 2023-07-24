614cb1196d609f9091ffe4e44693127c677d73ec
aceDavon <d.onate@simpsgroup.co.uk>
2023-07-24 09:01:12 +0100

    Implement the Agenda for the toolbar Calendar
    - Call the extracted Agenda component on the toolbar Calendar

--- src/components/Pilot/components/Calendar/index.tsx
+++ src/components/Pilot/components/Calendar/index.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import CalendarIcon from '../../../../assets/icons/CalendarIcon';
 import SectionArea from '../SectionArea';
 import MiniDatePicker from '../../../DatePicker/MiniCalendar';
+import { useParams } from 'react-router-dom';
+import { useAppSelector } from '../../../../app/hooks';
+import { getTaskListService } from '../../../../features/task/taskService';
 
 export default function Calendar() {
   const [iconToggle, setIconToggle] = useState(false);
+  const { listId } = useParams();
+  const { filterTaskByAssigneeIds } = useAppSelector((state) => state.task);
+
+  const { data } = getTaskListService({ listId, assigneeUserId: filterTaskByAssigneeIds });
+
+  const entityTaskData = useMemo(() => data?.pages.flatMap((page) => page.data.tasks), [data]);
+
   return (
     <div className="flex flex-col space-y-2">
       <div className="flex items-center w-full border-b-alsoit-border-base">
