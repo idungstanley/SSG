9a2ac681b3fa642f3779c02cc66224f2ae2e9b2b
aceDavon <d.onate@simpsgroup.co.uk>
2023-07-24 08:14:56 +0100

    Extract the Agenda Component
    - Extract the Agendar component to a standalone component to be used in
      multiple places across the application.

--- src/components/Pilot/components/Calendar/index.tsx
+++ src/components/Pilot/components/Calendar/index.tsx
@@ -5,6 +5,7 @@ import MiniDatePicker from '../../../DatePicker/MiniCalendar';
 import { useParams } from 'react-router-dom';
 import { getTaskListService } from '../../../../features/task/taskService';
 import { useAppSelector } from '../../../../app/hooks';
+import Agenda from './Agenda';
 
 export default function Calendar() {
   const [iconToggle, setIconToggle] = useState(false);
@@ -12,15 +13,12 @@ export default function Calendar() {
   const { filterTaskByAssigneeIds } = useAppSelector((state) => state.task);
   const { selectedDate } = useAppSelector((state) => state.workspace);
 
-  console.log(selectedDate?.date.format('YYYY-MM-DD'));
-
   const { data } = getTaskListService({ listId, assigneeUserId: filterTaskByAssigneeIds });
 
   const entityTaskData = useMemo(() => data?.pages.flatMap((page) => page.data.tasks), [data]);
 
   const checkSchedule = () =>
     entityTaskData?.find((task) => selectedDate?.date.format('YYYY-MM-DD') === task.start_date);
-  // console.log(entityTaskData);
   return (
     <div className="flex flex-col space-y-2">
       <div className="flex items-center w-full border-b-alsoit-border-base">
@@ -34,28 +32,7 @@ export default function Calendar() {
       <div className="flex justify-center w-full px-2 pt-6">
         <MiniDatePicker />
       </div>
-      {entityTaskData?.length && (
-        <div className="flex flex-col space-y-2">
-          <p className="text-center font-semibold text-alsoit-text-lg border-b-2">Schedule for Today</p>
-          {checkSchedule() ? (
-            entityTaskData.map(
-              (task) =>
-                selectedDate?.date.format('YYYY-MM-DD') === task.start_date && (
-                  <div key={task.id} className="flex space-x-2 border-b py-2 hover:bg-alsoit-gray-75 cursor-pointer">
-                    <span className="w-1/5 px-2 text-alsoit-text-lg font-semibold capitalize">{task.name}</span>
-                    <span className="w-2/5 px-2 text-alsoit-text-lg font-semibold capitalize">{task.description}</span>
-                    <span className="w-1/5 px-2 text-alsoit-text-lg font-semibold capitalize">
-                      {task.priority as string}
-                    </span>
-                    <span className="w-1/5 px-2 text-alsoit-text-lg font-semibold capitalize">{task.status.name}</span>
-                  </div>
-                )
-            )
-          ) : (
-            <span className="text-center text-alsoit-text-lg font-semibold py-4">No Agenda for the selected Date</span>
-          )}
-        </div>
-      )}
+      {entityTaskData?.length && <Agenda entityTaskData={entityTaskData} />}
     </div>
   );
 }
