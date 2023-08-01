ea9fa658cf602b49b4a2bcb917a87df0b75d366b
Omachi John <omachi.john@yahoo.com>
2023-08-01 10:46:20 +0100

    check item completed

--- src/components/badges/index.tsx
+++ src/components/badges/index.tsx
@@ -62,7 +62,7 @@ export default function Badges({ task }: BadgeTask) {
             <button className="p-1 border rounded-md " onClick={(e) => onShowAddSubtaskField(e, task.id)}>
               <Checklist />
               <p className="alsoit-text-sm h-2 w-2 absolute left-5" style={{ fontSize: '9px', bottom: '12.2px' }}>
-                {task.checklist_items_count}/{task.checklist_items_count}
+                {task.checklist_done_items_count}/{task.checklist_items_count}
               </p>
             </button>
           </ToolTip>
