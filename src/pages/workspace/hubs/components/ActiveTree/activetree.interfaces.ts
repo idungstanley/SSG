c8d6b87d4d8ac170165e01de459a7bb1605c9fd3
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-08-12 10:38:19 +0100

    Add status props

--- src/pages/workspace/hubs/components/ActiveTree/activetree.interfaces.ts
+++ src/pages/workspace/hubs/components/ActiveTree/activetree.interfaces.ts
@@ -9,6 +9,16 @@ export interface List extends IList {
   children: List[];
 }
 
+export interface StatusProps {
+  color: string | null;
+  id: string | null;
+  model_id: string | null;
+  model_type: string | null;
+  type: string | null;
+  position: number | null;
+  name: string | null;
+}
+
 export interface Hub extends IHub {
   children: Hub[];
   wallets: Wallet[];
