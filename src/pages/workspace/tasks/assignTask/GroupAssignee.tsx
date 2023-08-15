fc0f1e6da2b6719a001382f2740bb93eba573e50
Omachi John <omachi.john@yahoo.com>
2023-08-15 08:55:05 +0100

    compact view wrap text and assignee icons resize

--- src/pages/workspace/tasks/assignTask/GroupAssignee.tsx
+++ src/pages/workspace/tasks/assignTask/GroupAssignee.tsx
@@ -131,6 +131,8 @@ function GroupAssignee({
                                 : (newData.color as string)
                             }
                             badge={true}
+                            height={CompactView ? 'h-5' : 'h-7'}
+                            width={CompactView ? 'w-5' : 'w-7'}
                           />
                         </div>
                       )}
