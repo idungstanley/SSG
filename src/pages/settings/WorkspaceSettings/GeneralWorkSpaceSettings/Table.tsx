75deb27aa0a23d0381ba0cc56875738cc6a2fe17
aceDavon <d.onate@simpsgroup.co.uk>
2023-08-22 08:54:34 +0100

    Add the extra workspace settings
    - Add user ability to control their clock and reminders to the workspace
      extra settings.

--- src/pages/settings/WorkspaceSettings/GeneralWorkSpaceSettings/Table.tsx
+++ src/pages/settings/WorkspaceSettings/GeneralWorkSpaceSettings/Table.tsx
@@ -4,17 +4,29 @@ import { cl } from '../../../../utils';
 import { useEffect, useState } from 'react';
 import { WorkSpaceSettingsRes } from '../../../../features/workspace/workspace.interfaces';
 import { getWorkSpaceSettings } from '../../../../features/workspace/workspaceService';
-import { useAppSelector } from '../../../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
 import { SimpleSectionHeading } from '../../../../components';
 import notificationFrame from '../../../../assets/branding/notificationFrame.png';
 import { Spinner } from '../../../../common';
+import {
+  setClockLimit,
+  setClockStopReminder,
+  setClockType,
+  setClocktime
+} from '../../../../features/settings/user/userSettingsSlice';
+
+const minutesToMilliseconds = 60 * 1000;
+const hoursToMilliseconds = 60 * 60 * 1000;
 
 export default function WorkSpaceTable() {
   const { workSpaceSettings } = useAppSelector((state) => state.workspace);
-
-  const [getSettings, setGetSettings] = useState<WorkSpaceSettingsRes[] | undefined>([]);
+  const { is_clock_time, clock_limit, clock_stop_reminder, clock_type } = useAppSelector((state) => state.userSetting);
 
   const { data: fetchedData, status } = getWorkSpaceSettings();
+
+  const dispatch = useAppDispatch();
+
+  const [getSettings, setGetSettings] = useState<WorkSpaceSettingsRes[] | undefined>([]);
 
   useEffect(() => {
     if (workSpaceSettings) {
@@ -91,6 +103,97 @@ export default function WorkSpaceTable() {
                 </div>
               </Disclosure.Panel>
             )}
+          </div>
+        )}
+      </Disclosure>
+      <Disclosure defaultOpen={false}>
+        {({ open }) => (
+          <div className="w-full">
+            <Disclosure.Button className="w-full">
+              <div className="bg-gray-200">
+                <div className="flex">
+                  <div className="py-6 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 w-2/4">
+                    <div className="flex items-center space-x-2">
+                      <ChevronRightIcon className={cl(open ? 'rotate-90 transform' : '', 'w-6 h-6')} />
+                      <p className="uppercase" style={{ fontSize: '15px' }}>
+                        Workspace Extra Settings
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Disclosure.Button>
+            <Disclosure.Panel>
+              {' '}
+              <div className="bg-white p-4 w-full">
+                <div className="my-3 flex justify-between ">
+                  <div style={{ width: '48%' }}>
+                    <h5 className="font-semibold" style={{ fontSize: '15px' }}>
+                      Clocking Limit
+                    </h5>
+                    <select
+                      name="Day"
+                      className="h-10 rounded my w-full"
+                      value={clock_limit}
+                      onChange={(e) => dispatch(setClockLimit(Number(e.target.value)))}
+                      style={{ fontSize: '15px' }}
+                    >
+                      <option value={1 * hoursToMilliseconds}>1 hour</option>
+                      <option value={2 * hoursToMilliseconds}>2 hours</option>
+                      <option value={3 * hoursToMilliseconds}>3 hour</option>
+                      <option value={24 * hoursToMilliseconds}>1 day</option>
+                    </select>
+                  </div>
+                  <div style={{ width: '48%' }}>
+                    <h5 className="font-semibold" style={{ fontSize: '15px' }}>
+                      Reminder Time
+                    </h5>
+                    <select
+                      name="Time-format"
+                      className="h-10 rounded my w-full"
+                      value={clock_stop_reminder}
+                      onChange={(e) => dispatch(setClockStopReminder(Number(e.target.value)))}
+                      style={{ fontSize: '15px' }}
+                    >
+                      <option value={5 * minutesToMilliseconds}>5 minutes</option>
+                      <option value={15 * minutesToMilliseconds}>15 minutes</option>
+                      <option value={25 * minutesToMilliseconds}>25 minutes</option>
+                      <option value={35 * minutesToMilliseconds}>35 minutes</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="my-3 flex justify-between w-full">
+                  <div style={{ width: '48%' }}>
+                    <h5 className="font-semibold" style={{ fontSize: '15px' }}>
+                      Clock Type
+                    </h5>
+                    <select
+                      name="clock_time"
+                      className="h-10 rounded my w-full"
+                      value={clock_type}
+                      onChange={(e) => dispatch(setClockType(e.target.value))}
+                      style={{ fontSize: '15px' }}
+                    >
+                      <option value="d">Digital</option>
+                      <option value="a">Analog</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="my-3 flex space-x-1 items-center w-80">
+                  <div style={{ width: '20%' }} className="flex items-center">
+                    <label className="switch w-full">
+                      <input
+                        type="checkbox"
+                        checked={is_clock_time ? true : false}
+                        onClick={() => dispatch(setClocktime(is_clock_time === 1 ? 0 : 1))}
+                      />
+                      <div className={`slider ${is_clock_time === 1 ? 'checked' : ''}`}></div>
+                    </label>
+                  </div>
+                  <span className="w-full">Show Toolbar Clock?</span>
+                </div>
+              </div>
+            </Disclosure.Panel>
           </div>
         )}
       </Disclosure>
