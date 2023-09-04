2b7fa4a494c6fb9fca42dc8b191ed3845acaa889
alhajideen <salifunurudeen834@gmail.com>
2023-09-04 23:58:53 +0100

    add website field on list view

--- src/components/Views/ui/Table/CustomField/EmailWebsiteField/EmailWebsiteField.tsx
+++ src/components/Views/ui/Table/CustomField/EmailWebsiteField/EmailWebsiteField.tsx
@@ -12,14 +12,14 @@ interface EmailFieldProps {
   taskCustomFields?: ICustomField;
   taskId: string;
   fieldId: string;
+  fieldType: string;
 }
 
 const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const isValidEmail = (email: string) => {
-  return emailPattern.test(email);
-};
 
-function EmailField({ taskCustomFields, taskId, fieldId }: EmailFieldProps) {
+const websitePattern = /^(?:(https?:\/\/|www\.)[\w.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})|[\w.-]+\.[a-zA-Z]{2,})$/;
+
+function EmailWebsiteField({ taskCustomFields, taskId, fieldId, fieldType }: EmailFieldProps) {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const activeValue = taskCustomFields?.values[0].value ? taskCustomFields?.values[0].value : '-';
   const [currentValue, setCurrentValue] = useState<string>(activeValue);
@@ -28,9 +28,14 @@ function EmailField({ taskCustomFields, taskId, fieldId }: EmailFieldProps) {
 
   const { mutate: onUpdate } = useUpdateEntityCustomFieldValue(taskId);
 
+  const isValidEntry = (entry: string) => {
+    const validateParams = fieldType === 'email' ? emailPattern.test(entry) : websitePattern.test(entry);
+    return validateParams;
+  };
+
   const handleInputBlur = () => {
     setEditMode(false);
-    if (currentValue !== activeValue && isValidEmail(currentValue)) {
+    if (currentValue !== activeValue && isValidEntry(currentValue)) {
       onUpdate({
         taskId,
         value: [{ value: currentValue }],
@@ -42,7 +47,7 @@ function EmailField({ taskCustomFields, taskId, fieldId }: EmailFieldProps) {
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      if (currentValue !== activeValue && isValidEmail(currentValue)) {
+      if (currentValue !== activeValue && isValidEntry(currentValue)) {
         onUpdate({
           taskId,
           value: [{ value: currentValue }],
@@ -93,25 +98,39 @@ function EmailField({ taskCustomFields, taskId, fieldId }: EmailFieldProps) {
   };
 
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex justify-center items-center p-4">
       {!editMode ? (
-        <div className="w-full h-full group/parent p-1 relative">
+        <div className="w-full h-full group/parent">
           <span
             className={cl(
-              'h-full flex items-center  cursor-pointer',
+              'h-full flex items-center  cursor-pointer w-full',
               currentValue !== '-' ? 'justify-between' : 'justify-center'
             )}
           >
             {currentValue === '-' ? (
               <h1 onClick={() => setEditMode(true)}>-</h1>
             ) : (
-              <a
-                href={`mailto:${currentValue}`}
-                className="hover:text-alsoit-purple-300 w-full break-words text-center"
-              >
-                {currentValue}
-              </a>
+              <>
+                {fieldType === 'website' ? (
+                  <a
+                    href={currentValue}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-alsoit-purple-300 w-full break-words text-center"
+                  >
+                    {currentValue}
+                  </a>
+                ) : (
+                  <a
+                    href={`mailto:${currentValue}`}
+                    className="hover:text-alsoit-purple-300 w-full break-words text-center"
+                  >
+                    {currentValue}
+                  </a>
+                )}
+              </>
             )}
+
             {currentValue !== '-' && (
               <button
                 className="flex items-center justify-end opacity-0 group-hover/parent:opacity-100 w-5 h-5 absolute bg-white right-0"
@@ -123,15 +142,17 @@ function EmailField({ taskCustomFields, taskId, fieldId }: EmailFieldProps) {
           </span>
         </div>
       ) : (
-        <input
-          type="text"
-          autoFocus={true}
-          value={currentValue === '-' ? '' : currentValue}
-          onChange={(e) => setCurrentValue(e.target.value)}
-          onKeyDown={handleKeyDown}
-          onBlur={handleInputBlur}
-          className="w-full h-fit border-0 ring-0 outline-0 text-alsoit-text-lg font-semibold focus:ring-0 focus:outline-0 focus:border-0"
-        />
+        <div className="w-full">
+          <input
+            type="text"
+            autoFocus={true}
+            value={currentValue === '-' ? '' : currentValue}
+            onChange={(e) => setCurrentValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+            onBlur={handleInputBlur}
+            className="max-w-full h-fit border-0 outline-0 ring-0 focus:border-0 focus:outline-0 focus:ring-0"
+          />
+        </div>
       )}
       <AlsoitMenuDropdown handleClose={handleClose} anchorEl={anchorEl}>
         <div className="block" style={{ width: '150px' }}>
@@ -161,4 +182,4 @@ function EmailField({ taskCustomFields, taskId, fieldId }: EmailFieldProps) {
   );
 }
 
-export default EmailField;
+export default EmailWebsiteField;
