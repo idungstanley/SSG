a635e2c09f121e7e3feb58d744811cbff6d73689
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-09-18 10:26:49 +0100

    Update create button width for all entity types

--- src/pages/workspace/pilot/components/createEntity/createList/CreateList.tsx
+++ src/pages/workspace/pilot/components/createEntity/createList/CreateList.tsx
@@ -143,7 +143,7 @@ export default function CreateList() {
           label="Create List"
           padding="py-2 px-2"
           height="h-7"
-          width="w-28"
+          width="w-fit"
         />
       </div>
     </div>
