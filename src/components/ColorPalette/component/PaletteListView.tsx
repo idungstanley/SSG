59d8cecd1e05c2c8f73e6c325b62b27a5da0fdbc
Stanley_idung <s.sunday@simpsgroup.co.uk>
2023-09-22 13:24:42 +0100

    Add update

--- src/components/ColorPalette/component/PaletteListView.tsx
+++ src/components/ColorPalette/component/PaletteListView.tsx
@@ -54,8 +54,8 @@ function Row({ item, key }: { item: string; key: number }) {
   }, []);
 
   return (
-    <tr className={`divide-gray-300 divide-y contents ${isChecked ? 'bg-gray-100' : ''}`} key={key}>
-      <td className={`p-2 ${isChecked ? 'bg-alsoit-gray-75' : ''}`}>
+    <tr className="divide-y divide-gray-300 contents" key={key}>
+      <td className={`p-2 ${isChecked ? 'bg-primary-100' : ''}`}>
         <div className="flex items-center justify-between gap-1">
           <RoundedCheckbox
             onChange={onChange}
@@ -63,15 +63,15 @@ function Row({ item, key }: { item: string; key: number }) {
             styles="w-2 h-2 rounded-full cursor-pointer focus:outline-1 focus:ring-transparent  focus:border-2 focus:opacity-100 group-hover:opacity-100 text-alsoit-purple-300"
           />
           <div
-            className={`w-5 h-5 p-2 rounded ${isChecked ? 'bg-alsoit-gray-75' : ''}`}
+            className={`w-5 h-5 p-2 rounded ${isChecked ? 'bg-primary-100' : ''}`}
             style={{ backgroundColor: `${item}` }}
           ></div>
         </div>
       </td>
-      <td className={`p-2 ${isChecked ? 'bg-alsoit-gray-75' : ''}`}>
+      <td className={`p-2 ${isChecked ? 'bg-primary-100' : ''}`}>
         <div>{item}</div>
       </td>
-      <td className={`p-2 text-xs truncate ${isChecked ? 'bg-alsoit-gray-75' : ''}`}>
+      <td className={`p-2 text-xs truncate ${isChecked ? 'bg-primary-100' : ''}`}>
         <div>{colorName}</div>
       </td>
     </tr>
