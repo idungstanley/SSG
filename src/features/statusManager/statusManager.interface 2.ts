5161387c799027e31f88b9010f13b9fea15037bf
Omachi John <omachi.john@yahoo.com>
2023-10-03 09:21:50 +0100

    make new template fpor task and subtask

---
+++ src/features/statusManager/statusManager.interface 2.ts
@@ -0,0 +1,26 @@
+import { StatusType } from '../../utils/StatusManagement/Types';
+
+export interface statusItemsProps {
+  color: string;
+  position: number;
+  name: string;
+  type: string;
+}
+export interface taskStatusProps {
+  checksum: string;
+  id: string;
+  items: StatusType[];
+  name: string;
+}
+export interface getStatusTemplateRes {
+  data: {
+    task_status_templates: taskStatusProps[];
+  };
+}
+
+export const COLLECTION_TYPES = {
+  BESPOKE_TO_ENTITY: 'Bespoke to Entity',
+  PARENT_ENTITY: 'Parent Entity',
+  SHARED_ENTITY: 'Shared Entity',
+  TEMPLATE_COLLECTION: 'Template Collection'
+};
