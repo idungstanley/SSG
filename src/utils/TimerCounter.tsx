95b23204b2412f69feabc73a01e5e26219e3d374
Dmitri Stasenko <d.stasenko@alsoit.io>
2023-10-03 16:19:27 +0300

    Fix filters by users in list view

--- src/utils/TimerCounter.tsx
+++ src/utils/TimerCounter.tsx
@@ -10,8 +10,8 @@ interface TimerProps {
 
 export function runTimer({ isRunning, setTime }: TimerProps) {
   const dispatch = useAppDispatch();
+
   const { duration } = useAppSelector((state) => state.task);
-  console.log(isRunning);
 
   useEffect(() => {
     let updateH = duration.h;
