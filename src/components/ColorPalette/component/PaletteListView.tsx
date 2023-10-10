2d8b2922403888c5363c14f60a7a725f77d77d1a
Stanley.sunday <s.sunday@alsoit.io>
2023-10-10 14:14:58 +0100

    Update List View UI

--- src/components/ColorPalette/component/PaletteListView.tsx
+++ src/components/ColorPalette/component/PaletteListView.tsx
@@ -3,31 +3,30 @@ import { palette } from '../../../utils/Colors';
 import { VerticalScroll } from '../../ScrollableContainer/VerticalScroll';
 import RoundedCheckbox from '../../Checkbox/RoundedCheckbox';
 import { getColorName, initColors, ORIGINAL_COLORS } from 'ntc-ts';
+import DefaultColour from '../../../assets/icons/DefaultColour';
 
 export default function PaletteListView() {
   return (
     <VerticalScroll>
-      <div className="ml-5 table-container">
-        <table className="h-32" style={{ width: '250px', display: 'grid', gridTemplateColumns: '40px 90px auto' }}>
-          <thead className="w-full contents">
-            <tr className="w-full h-6 text-xs text-left bg-gray-200 contents">
-              <th className="ml-2.5 text-center p-2">
-                <span className="relative w-5 h-5 px-2 bg-white border border-gray-300 rounded">
-                  <div
-                    className="absolute left-0 origin-top-left transform rotate-45 bg-gray-300"
-                    style={{ top: '0px', height: '1px', width: '20px' }}
-                  ></div>
-                </span>
-              </th>
-              <th className="p-2 bg-gray-200">HEX CODE</th>
-              <th className="p-2 bg-gray-200">LIBRARY NAME</th>
-            </tr>
-          </thead>
-          <tbody className="contents">
-            {palette.map((item, index) => (
-              <Row item={item} key={index} />
-            ))}
-          </tbody>
+      <div className="w-full table-container">
+        <table className="w-full h-36" style={{ display: 'grid', gridTemplateColumns: '20px 44px 110px auto' }}>
+          <tr className="w-full h-6 text-xs text-left contents">
+            <th className="p-2 text-center">
+              <RoundedCheckbox
+                onChange={() => ({})}
+                isChecked={false}
+                styles="w-2 h-2 rounded-full cursor-pointer focus:outline-1 focus:ring-transparent focus:border-2 focus:opacity-100 group-hover:opacity-100 text-alsoit-purple-300"
+              />
+            </th>
+            <th className="p-2 text-center border-b border-gray-300">
+              <span className="flex items-center justify-between gap-1">
+                <DefaultColour dimensions={{ width: 20, height: 20 }} />
+              </span>
+            </th>
+            <th className="p-2 border-b border-gray-300">HEX CODE</th>
+            <th className="p-2 border-b border-gray-300">LIBRARY NAME</th>
+          </tr>
+          {palette.map((item, index) => item !== null && <Row item={item} key={index} />)}
         </table>
       </div>
     </VerticalScroll>
@@ -56,24 +55,26 @@ function Row({ item, key }: { item: string | null; key: number }) {
   }, []);
 
   return (
-    <tr className="divide-y divide-gray-300 contents" key={key}>
-      <td className={`p-2 ${isChecked ? 'bg-primary-100' : ''}`}>
+    <tr className="w-full bg-white contents" key={key}>
+      <td className="p-2">
+        <RoundedCheckbox
+          onChange={onChange}
+          isChecked={isChecked}
+          styles="w-2 h-2 rounded-full cursor-pointer focus:outline-1 focus:ring-transparent  focus:border-2 focus:opacity-100 group-hover:opacity-100 text-alsoit-purple-300"
+        />
+      </td>
+      <td className={`p-2 bg-white ${isChecked ? 'border-primary-400 border-y border-l' : 'border-b border-gray-300'}`}>
         <div className="flex items-center justify-between gap-1">
-          <RoundedCheckbox
-            onChange={onChange}
-            isChecked={isChecked}
-            styles="w-2 h-2 rounded-full cursor-pointer focus:outline-1 focus:ring-transparent  focus:border-2 focus:opacity-100 group-hover:opacity-100 text-alsoit-purple-300"
-          />
           <div
             className={`w-5 h-5 p-2 rounded ${isChecked ? 'bg-primary-100' : ''}`}
             style={{ backgroundColor: `${item}` }}
           ></div>
         </div>
       </td>
-      <td className={`p-2 ${isChecked ? 'bg-primary-100' : ''}`}>
+      <td className={`p-2 bg-white ${isChecked ? 'border-primary-400 border-y' : 'border-b border-gray-300'}`}>
         <div>{item}</div>
       </td>
-      <td className={`p-2 text-xs truncate ${isChecked ? 'bg-primary-100' : ''}`}>
+      <td className={`p-2 bg-white ${isChecked ? 'border-primary-400 border-y border-r' : 'border-b border-gray-300'}`}>
         <div>{colorName}</div>
       </td>
     </tr>
---
+++ src/utils/mui.styles.ts
@@ -0,0 +1,11 @@
+// import { Theme, makeStyles } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles({
+  menuPaper: {
+    padding: 0,
+    paddingBottom: 0,
+    paddingTop: 0,
+    backgroundColor: 'lightblue'
+  }
+});
