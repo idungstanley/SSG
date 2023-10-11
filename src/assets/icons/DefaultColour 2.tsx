8c692090a0f49191f1eb8ef8dd745ac994ac17ca
Omachi John <omachi.john@yahoo.com>
2023-10-11 11:56:58 +0100

    Sectionalise hotkeys command

---
+++ src/assets/icons/DefaultColour 2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DefaultColour() {
+  return (
+    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="0.1" y="0.975" width="15.8" height="15.8" rx="1.9" fill="white" stroke="#626262" strokeWidth="0.2" />
+      <path d="M1.07031 1.80469L15 15.8755" stroke="#626262" strokeWidth="0.2" />
+    </svg>
+  );
+}
+
+export default DefaultColour;
