bfffc1923c96848ac3ed37012c9ae6233af176e2
Omachi John <omachi.john@yahoo.com>
2023-10-12 13:57:49 +0100

    Ability toggle hotkeys and save details

--- src/features/settings/user/userSettingsServices.ts
+++ src/features/settings/user/userSettingsServices.ts
@@ -4,6 +4,7 @@ import { IColourRes, IUserRes } from '../../workspace/workspace.interfaces';
 import { setShowConfirmationModal } from './userSettingsSlice';
 import { useAppDispatch } from '../../../app/hooks';
 import { setColourPaletteData } from '../../account/accountSlice';
+import { IPreferenceState } from '../../task/taskSlice';
 // import { useAppDispatch } from '../../../app/hooks';
 interface IUserSettings {
   name?: string | undefined;
@@ -16,6 +17,7 @@ interface IUserSettings {
   color?: string;
   is_clock_time?: number | null;
   clock_type?: string | null;
+  user_preferences?: IPreferenceState;
 }
 
 interface IPasswordprops {
@@ -53,12 +55,13 @@ const updateUserSettings = ({
   time_format,
   color,
   clock_type,
-  is_clock_time
+  is_clock_time,
+  user_preferences
 }: IUserSettings) => {
   const request = requestNew({
     url: '/auth/account',
     method: 'PUT',
-    params: {
+    data: {
       name,
       date_format,
       email,
@@ -68,7 +71,8 @@ const updateUserSettings = ({
       time_format,
       color,
       clock_type,
-      is_clock_time
+      is_clock_time,
+      user_preferences
     }
   });
   return request;
