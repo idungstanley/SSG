5c879248b3bd88eefcc5b3880bacdfaa4674554c
Deen <n.salifu@alsoit.io>
2023-10-16 14:46:10 +0100

    add verify email route

--- src/pages/workspace/createWorkspace/auth/VerifyEmail/Index.tsx
+++ src/pages/workspace/createWorkspace/auth/VerifyEmail/Index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Wrapper from '..';
 import ProgressBar from '../../../../../layout/components/MainLayout/ProgressBar';
 import { Button } from '../../../../../components';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -23,22 +22,20 @@ function VerifyEmail() {
   };
 
   return (
-    <Wrapper>
-      <main className="flex flex-col justify-center flex-grow w-full min-h-full px-4 mx-auto text-center bg-white max-w-7xl sm:px-6 lg:px-8">
-        <ProgressBar />
-        <p className="mt-2 text-base text-gray-500">Click on this button to verify your email</p>
-        <div className="mt-6">
-          <Button
-            buttonStyle="primary"
-            onClick={handleAcceptInvite}
-            label={'Accept'}
-            padding="py-2 px-4"
-            height="h-10"
-            width="w-40"
-          />
-        </div>
-      </main>
-    </Wrapper>
+    <main className="flex flex-col justify-center flex-grow w-full min-h-full px-4 mx-auto text-center bg-white max-w-7xl sm:px-6 lg:px-8">
+      <ProgressBar />
+      <p className="mt-2 text-base text-gray-500">Click on this button to verify your email</p>
+      <div className="mt-6">
+        <Button
+          buttonStyle="primary"
+          onClick={handleAcceptInvite}
+          label="Verfy Email"
+          padding="py-2 px-4"
+          height="h-10"
+          width="w-40"
+        />
+      </div>
+    </main>
   );
 }
 
