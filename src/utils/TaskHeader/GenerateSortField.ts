590ffd1f4dcc5ff86ee8b7fe7c1bfb05610f2e1f
Deen <n.salifu@alsoit.io>
2023-10-23 11:57:09 +0100

    use onstants

--- src/utils/TaskHeader/GenerateSortField.ts
+++ src/utils/TaskHeader/GenerateSortField.ts
@@ -1,10 +1,10 @@
 const HEADER_TITLE = {
   assignee: 'Assignees',
   task: 'Task',
-  'Created at': 'Created at',
-  'Updated at': 'Updated at',
-  'Start Date': 'Start Date',
-  'End Date': 'End Date'
+  created_at: 'Created at',
+  updated_at: 'Updated at',
+  start_date: 'Start Date',
+  end_date: 'End Date'
 };
 
 const HEADER_PARAMS = {
@@ -22,13 +22,13 @@ export const generateSortField = (title: string, isDefault: boolean, id?: string
       ? HEADER_PARAMS.assignee
       : title === HEADER_TITLE.task
       ? HEADER_PARAMS.task
-      : title === HEADER_TITLE['Created at']
+      : title === HEADER_TITLE.created_at
       ? HEADER_PARAMS.created_at
-      : title === HEADER_TITLE['Updated at']
+      : title === HEADER_TITLE.updated_at
       ? HEADER_PARAMS.updated_at
-      : title === HEADER_TITLE['Start Date']
+      : title === HEADER_TITLE.start_date
       ? HEADER_PARAMS.start_date
-      : title === HEADER_TITLE['End Date']
+      : title === HEADER_TITLE.end_date
       ? HEADER_PARAMS.end_date
       : title?.toLowerCase();
   } else {
