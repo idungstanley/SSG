46a76dfaab6724d4101d89d00eb30072dcc6a54d
Deen <n.salifu@alsoit.io>
2023-10-25 11:37:08 +0100

    add is not option to assignee filter

--- src/components/TasksHeader/lib/generateFilters.ts
+++ src/components/TasksHeader/lib/generateFilters.ts
@@ -5,23 +5,24 @@ import { FilterKey, FilterValue, FilterWithId, FiltersOption } from '../ui/Filte
 
 const DATE_KEYS: Partial<FilterKey>[] = ['created_at', 'end_date', 'start_date'];
 
+const FILTER_KEYS = {
+  assignee: 'assignee',
+  tag: 'tag'
+};
+
 const SPECIAL_KEYS: Partial<FilterKey>[] = ['assignees', 'tags', ...DATE_KEYS];
 
 const getValues = (values: FilterValue[]) => values.map((i) => (isString(i) ? i : i.id));
 
-// function generateFilterValues(params: type) {}
-
 const getKey = (i: string) => i.split(SPECIAL_CHAR)[1] ?? i.split(SPECIAL_CHAR)[0];
-
-const EQ = 'eq';
 
 export const generateFilters = () => {
   const {
     filters: { fields: filters, option: op }
   } = useAppSelector((state) => state.task);
 
-  const assignee = filters.filter((i) => i.key === 'assignees');
-  const tags = filters.find((i) => i.key === 'tags');
+  const assignee = filters.filter((i) => i.key === SPECIAL_KEYS[0]);
+  const tags = filters.find((i) => i.key === SPECIAL_KEYS[1]);
 
   const dates = filters.filter((i) => DATE_KEYS.includes(i.key));
 
@@ -33,14 +34,14 @@ export const generateFilters = () => {
       fields: [
         tags
           ? {
-              field: 'tag',
+              field: FILTER_KEYS.tag,
               op: tags.operator.key,
               values: tags.values.length ? getValues(tags.values) : null
             }
           : null,
 
         ...assignee.map((i) => ({
-          field: 'assignee',
+          field: FILTER_KEYS.assignee,
           op: i.operator.key,
           values: i.values.length ? getValues(i.values) : null
         })),
@@ -65,8 +66,8 @@ export const generateFilters = () => {
 };
 
 export const generateFiltersSubtasks = (op: FiltersOption, filters: FilterWithId[]) => {
-  const assignee = filters.find((i) => i.key === 'assignees');
-  const tags = filters.find((i) => i.key === 'tags');
+  const assignee = filters.filter((i) => i.key === SPECIAL_KEYS[0]);
+  const tags = filters.find((i) => i.key === SPECIAL_KEYS[1]);
 
   const dates = filters.filter((i) => DATE_KEYS.includes(i.key));
 
@@ -75,20 +76,20 @@ export const generateFiltersSubtasks = (op: FiltersOption, filters: FilterWithId
   return {
     filters: {
       op,
-      assignees:
-        assignee && assignee.operator.key === EQ
-          ? assignee.values.length
-            ? getValues(assignee.values)
-            : assignee.operator.key
-          : null,
       fields: [
         tags
           ? {
-              field: 'tag',
+              field: FILTER_KEYS.tag,
               op: tags.operator.key,
               values: tags.values.length ? getValues(tags.values) : null
             }
           : null,
+
+        ...assignee.map((i) => ({
+          field: FILTER_KEYS.assignee,
+          op: i.operator.key,
+          values: i.values.length ? getValues(i.values) : null
+        })),
 
         ...dates.map((i) => ({
           field: i.key,
