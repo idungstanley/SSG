4ab096bd2ba6e30cffa202fcbd55cb4b6cf698d1
Dmitri Stasenko <d.stasenko@alsoit.io>
2023-11-13 09:53:50 +0100

    Update styles for chat/contacts tabs

--- src/components/Chat/components/Nav.tsx
+++ src/components/Chat/components/Nav.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ChatBubbleBottomCenterTextIcon, UsersIcon } from '@heroicons/react/24/outline';
 import ToolTip from '../../Tooltip/Tooltip';
-import { cl } from '../../../utils';
 
 const navItems = [
   {
@@ -23,20 +22,18 @@ interface NavProps {
 
 export default function Nav({ activeTabId, setActiveTabId }: NavProps) {
   return (
-    <div className="flex flex-col items-center w-20 h-full gap-10 p-2 bg-gray-400 border-r">
-      <div className="flex flex-col items-center gap-2">
+    <div className="w-full h-full p-2 border-r">
+      <div className="flex justify-between items-center gap-2">
         {navItems.map((tab) => (
           <ToolTip key={tab.id} title={tab.label}>
-            <button
+            <div
               onClick={() => setActiveTabId(tab.id)}
-              type="button"
-              className={cl(
-                'inline-flex items-center p-1 focus:outline-none ring-0 focus:ring-0',
-                activeTabId === tab.id ? 'text-indigo-500 hover:text-indigo-700' : 'text-gray-500 hover:text-gray-700'
-              )}
+              className={`relative w-full flex justify-center flex-grow p-1 font-medium transition cursor-pointer group hover:text-primary-700  ${
+                activeTabId === tab.id && 'bg-primary-200 rounded-t-md text-primary-700'
+              }`}
             >
               {tab.icon}
-            </button>
+            </div>
           </ToolTip>
         ))}
       </div>
