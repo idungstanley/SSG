11031b72d920ff6bbf34dabded2ac59565b9a970
aceDavon <d.onate@simpsgroup.co.uk>
2023-11-28 19:11:37 +0100

    Remove Modal when dashboard is active
    - Dispatch and remove the add file modal when the uppy dashboard is active,
    - Add the progress of upload property to the dashboard while file uploads.

--- src/components/Pilot/components/details/properties/attachments/AddTo.tsx
+++ src/components/Pilot/components/details/properties/attachments/AddTo.tsx
@@ -80,9 +80,12 @@ export default function AddTo({ locationn }: { locationn?: string }) {
       id: 'upload_file',
       title: 'Upload File',
       handleClick: () => {
-        locationn === 'list view'
-          ? dispatch(setOpenFileUploadModal({ ...fileUploadProps, openModal: true }))
-          : dispatch(setShowTaskUploadModal(true));
+        if (locationn === 'list view') {
+          dispatch(setOpenFileUploadModal({ ...fileUploadProps, openModal: true }));
+        } else {
+          dispatch(setShowTaskUploadModal(true));
+          setShowDropdown(null);
+        }
       },
       color: '#d3d3d3',
       bg: 'gray',
@@ -150,8 +153,6 @@ export default function AddTo({ locationn }: { locationn?: string }) {
       icon: <FormatListBullet color={selectedViews === paletteViews.LIST ? 'rgb(191, 0, 255)' : '#424242'} />
     }
   ];
-
-  // const selectedElement = views.find((items) => items.label === selectedViews)?.element;
 
   return (
     <div className="h-full pb-2 m-3 text-gray-500 rounded-md bg-alsoit-gray-50">
