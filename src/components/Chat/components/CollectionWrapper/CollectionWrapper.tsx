44239cba4ed4398e313475cc05f15d905e36f40b
Stanley.sunday <s.sunday@alsoit.io>
2023-11-29 14:50:01 +0100

    Add ui for add property section

--- src/components/Chat/components/CollectionWrapper/CollectionWrapper.tsx
+++ src/components/Chat/components/CollectionWrapper/CollectionWrapper.tsx
@@ -25,12 +25,12 @@ export default function CollectionWrapper({ collection }: ICollectionWrapperProp
     <div>
       {/* header */}
       <div
-        className="flex items-center justify-between border-b mb-2 rounded-md group"
+        className="flex items-center justify-between mb-2 border-b rounded-md group"
         style={{ background: isActiveCollection ? '#E6FAE9' : '#FEF6E6' }}
       >
-        <div className="group/header flex items-center gap-2">
+        <div className="flex items-center gap-2 group/header">
           <div
-            className="flex cursor-default gap-1 py-1 relative px-2 rounded-md rounded-tr-none flex items-center space-x-1 text-white dFlex"
+            className="relative flex items-center gap-1 px-2 py-1 space-x-1 text-white rounded-md rounded-tr-none cursor-default dFlex"
             style={{
               minWidth: '145px',
               minHeight: '32px',
@@ -49,13 +49,13 @@ export default function CollectionWrapper({ collection }: ICollectionWrapperProp
 
         <div className="flex items-center gap-1">
           <div
-            className="flex justify-center items-center p-1 rounded-sm bg-white"
+            className="flex items-center justify-center p-1 bg-white rounded-sm"
             style={{ minWidth: '16px', height: '16px', fontSize: '8px', color: 'orange' }}
           >
             5
           </div>
           <div
-            className="flex justify-center items-center rounded-sm bg-white"
+            className="flex items-center justify-center bg-white rounded-sm"
             style={{ minWidth: '16px', height: '16px', fontSize: '8px', padding: '4px 2px', color: 'orange' }}
           >
             <span className="pr-1">
@@ -64,7 +64,7 @@ export default function CollectionWrapper({ collection }: ICollectionWrapperProp
             Collection
           </div>
           <div
-            className="flex justify-center items-center rounded-sm bg-white"
+            className="flex items-center justify-center bg-white rounded-sm"
             style={{ minWidth: '16px', height: '16px', fontSize: '8px', padding: '4px 2px', color: 'orange' }}
           >
             <span className="pr-1">
@@ -73,32 +73,32 @@ export default function CollectionWrapper({ collection }: ICollectionWrapperProp
             Shared
           </div>
           <div
-            className="flex justify-center items-center bg-white"
+            className="flex items-center justify-center bg-white"
             style={{ minWidth: '16px', height: '16px', fontSize: '8px', borderRadius: '3px' }}
           >
             <NotificationIcon width="9" height="11" color="orange" />
           </div>
           <div
-            className="flex justify-center items-center bg-white"
+            className="flex items-center justify-center bg-white"
             style={{ minWidth: '16px', height: '16px', fontSize: '8px', borderRadius: '3px' }}
           >
             <EmailIcon width="10" height="10" color="orange" />
           </div>
           <div className="flex items-center gap-1 opacity-0 group-hover:opacity-100">
             <div
-              className="w-4 flex justify-center items-center rounded-sm bg-white"
+              className="flex items-center justify-center w-4 bg-white rounded-sm"
               style={{ minWidth: '16px', height: '16px', fontSize: '8px', padding: '4px 2px', color: 'orange' }}
             >
               <PermissionIcon color="orange" />
             </div>
             <div
-              className="w-4 flex justify-center items-center rounded-sm bg-white"
+              className="flex items-center justify-center w-4 bg-white rounded-sm"
               style={{ minWidth: '16px', height: '16px', fontSize: '8px', padding: '4px 2px', color: 'orange' }}
             >
               <EditIcon color="orange" />
             </div>
             <div
-              className="w-4 flex justify-center items-center rounded-sm bg-white"
+              className="flex items-center justify-center w-4 bg-white rounded-sm"
               style={{ minWidth: '16px', height: '16px', fontSize: '8px', padding: '4px 2px', color: 'orange' }}
             >
               <TrashIcon color="orange" />
--- src/components/Pilot/components/Templates/index.tsx
+++ src/components/Pilot/components/Templates/index.tsx
@@ -3,10 +3,12 @@ import CreateNewColumn from './Components/CreateNewColumn';
 import { useAppSelector } from '../../../../app/hooks';
 import EditDropdown from './Edit/EditDropdown';
 import Button from '../../../Button';
-import PlusCircle from '../../../../assets/icons/AddCircle';
+// import PlusCircle from '../../../../assets/icons/AddCircle';
 import ChatSearch from '../../../../assets/icons/ChatSearch';
 import ChatFilter from '../../../../assets/icons/ChatFilter';
 import CollectionWrapper, { ICollection } from '../../../Chat/components/CollectionWrapper/CollectionWrapper';
+import { IoIosAddCircleOutline } from 'react-icons/io';
+import AddProperty from './Properties/component/AddProperty';
 
 const mockChatsData = [
   {
@@ -65,6 +67,7 @@ function Templates() {
   const [isArchived, setArchived] = useState<boolean>(false);
   const [searchValue, setSearchValue] = useState<string>('');
   const [filteredCollections, setFilteredCollections] = useState<ICollection[]>(mockChatsData);
+  const [addProperties, setAddProperties] = useState<boolean>(false);
 
   const handleSearchChat = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -79,52 +82,56 @@ function Templates() {
   };
 
   return (
-    <div className="flex-col w-full h-full gap-3 p-2 overflow-scroll">
+    <div className="flex-col w-full h-full gap-3 p-2 pl-3 space-y-2 overflow-scroll">
       <span className="flex items-center justify-between">
         <Button
-          height="h-9 text-white"
-          icon={<PlusCircle active={false} color="white" dimensions={{ height: 22, width: 22 }} />}
+          height="h-8 text-white"
+          icon={<IoIosAddCircleOutline className="text-base text-white" />}
           label="ADD PROPERTY"
-          labelSize="text-sm"
+          labelSize="text-xs"
           padding="p-2"
+          onClick={() => setAddProperties((prev) => !prev)}
           bgColor="#B2B2B2"
         />
-        <div className="flex items-center gap-2">
-          <div className="flex items-center justify-center w-6 h-6 rounded-md bg-alsoit-gray-125">
-            <ChatFilter />
+        {mockChatsData.length && (
+          <div className="flex items-center gap-2">
+            <div className="flex items-center justify-center w-6 h-6 rounded-md bg-alsoit-gray-125">
+              <ChatFilter />
+            </div>
+            <div
+              className="flex justify-between px-1 py-0.5 bg-alsoit-gray-125 items-center rounded-md"
+              style={{ minHeight: '24px', color: 'orange', fontSize: '10px' }}
+            >
+              Archived
+              <span className="flex items-center pl-3">
+                <label className="switch small" onClick={(event) => event.stopPropagation()}>
+                  <input
+                    className="inputShow"
+                    type="checkbox"
+                    checked={isArchived}
+                    onChange={() => setArchived(!isArchived)}
+                  />
+                  <div className={`slider ${isArchived ? 'checked' : ''}`} />
+                </label>
+              </span>
+            </div>
+            <div className="flex items-center justify-center bg-white rounded-md grow chatSearch">
+              <span className="chatSearch_icon">
+                <ChatSearch color="#919191" />
+              </span>
+              <input
+                className="ring-0 focus:ring-0 focus:outline-0"
+                type="text"
+                value={searchValue}
+                placeholder="Search Property"
+                onChange={(e) => handleSearchChat(e)}
+              />
+            </div>
           </div>
-          <div
-            className="flex justify-between px-1 py-0.5 bg-alsoit-gray-125 items-center rounded-md"
-            style={{ minHeight: '24px', color: 'orange', fontSize: '10px' }}
-          >
-            Archived
-            <span className="flex items-center pl-3">
-              <label className="switch small" onClick={(event) => event.stopPropagation()}>
-                <input
-                  className="inputShow"
-                  type="checkbox"
-                  checked={isArchived}
-                  onChange={() => setArchived(!isArchived)}
-                />
-                <div className={`slider ${isArchived ? 'checked' : ''}`} />
-              </label>
-            </span>
-          </div>
-          <div className="flex items-center justify-center bg-white rounded-md grow chatSearch">
-            <span className="chatSearch_icon">
-              <ChatSearch color="#919191" />
-            </span>
-            <input
-              className="ring-0 focus:ring-0 focus:outline-0"
-              type="text"
-              value={searchValue}
-              placeholder="Search Property"
-              onChange={(e) => handleSearchChat(e)}
-            />
-          </div>
-        </div>
+        )}
         {/* {'designers'.startsWith(searchValue) || !searchValue.length ? <CreateNewColumn /> : null} */}
       </span>
+      {addProperties && <AddProperty />}
       <div className="mt-2">
         {filteredCollections.map((collection, index) => (
           <CollectionWrapper key={collection.name + index} collection={collection} />
