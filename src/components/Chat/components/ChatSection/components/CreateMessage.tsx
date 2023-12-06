704dfe88d84d5087a401e1737240f25f6cc42107
Dmitri Stasenko <d.stasenko@alsoit.io>
2023-12-06 10:53:32 +0300

    Fix pipeline

--- src/components/Chat/components/ChatSection/components/CreateMessage.tsx
+++ src/components/Chat/components/ChatSection/components/CreateMessage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DropdownForMention from './DropdownForMention';
 import { useSendMessageToChat } from '../../../../../features/chat/chatService';
 import ChatEmoticons from '../../../../../assets/icons/ChatEmoticons';
